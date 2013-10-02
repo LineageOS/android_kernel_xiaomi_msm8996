11239d18b47f29b47e312228c50ad9a7989499eb
Sundar Subramaniyam <subrams@qti.qualcomm.com>
2013-10-01 23:48:45 -0700

    cld: fwlog: add userspace utility to handle netlink logging
    This patch will add utility can be used for netlink
    logging.
    
    ./cld-fwlog-netlink -c will print the debug log message in
    console.
    ./cld-fwlog-netlink -f /tmp/cldlog will log into the file
    
    In x86 platform kernel buffer will be used, however
    in android platforms, logcat buffer will be used.
    
    Change-Id: I350df069ceeed2793d05e71c62fba170d09f3cb1
    CRs Fixed: 539076

--- tools/fwdebuglog/Android.mk
+++ tools/fwdebuglog/Android.mk
@@ -9,6 +9,18 @@ LOCAL_CFLAGS := $(CFLAGS)
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := cld-fwlog-netlink
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../CORE/SERVICES/COMMON
+LOCAL_SHARED_LIBRARIES := libc libcutils
+LOCAL_SRC_FILES := cld-fwlog-netlink.c parser.c
+LOCAL_CFLAGS := $(CFLAGS)
+LOCAL_CFLAGS += -DCONFIG_ANDROID_LOG
+LOCAL_LDLIBS += -llog
+LOCAL_LDLIBS := -landroid
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := cld-fwlog-parser
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../CORE/SERVICES/COMMON
