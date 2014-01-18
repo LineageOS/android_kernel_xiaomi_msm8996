866d959d6df4399820505991524b5aa5dab8618b
Prakash Dhavali <pdhavali@codeaurora.org>
2014-01-18 02:49:55 -0800

    Change LF copyright year to match others

--- CORE/MAC/src/pe/lim/limRMC.h
+++ CORE/MAC/src/pe/lim/limRMC.h
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
