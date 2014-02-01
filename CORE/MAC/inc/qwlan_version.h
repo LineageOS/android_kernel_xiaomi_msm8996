b43229de0632e181835cdb8b0bf15870d2ef1227
Sameer Thalappil <sameert@qca.qualcomm.com>
2014-01-31 17:52:12 -0800

    qcacld: Release 1.0.0.26 Caf Staging
    Change-Id: I6a0a322f633c7dc45bd5f597d44b4cdcd1c02be9

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            25
+#define QWLAN_VERSION_BUILD            26
 
-#define QWLAN_VERSIONSTR               "1.0.0.25"
+#define QWLAN_VERSIONSTR               "1.0.0.26"
 
 #ifdef QCA_WIFI_2_0
 
