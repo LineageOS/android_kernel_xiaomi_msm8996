fde23a49d70d78d7d76280d7e80061954720fb24
Pitani Venkata Rajesh Kumar <vpitan@codeaurora.org>
2014-02-26 17:37:07 +0530

    Cafstaging release 1.0.0.45
    Cafstaging release 1.0.0.45
    
    Change-Id: I70175f2173c66f8f3ae111b76a40399f5c8434b2

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            44
+#define QWLAN_VERSION_BUILD            45
 
-#define QWLAN_VERSIONSTR               "1.0.0.44"
+#define QWLAN_VERSIONSTR               "1.0.0.45"
 
 #ifdef QCA_WIFI_2_0
 
