2e1efaf1583c80bc70c3804ffb4bd511a831e72e
Leela Venkata Kiran Kumar Reddy Chirala <kchirala@qca.qualcomm.com>
2014-02-26 20:55:28 -0800

    Support to select a channel in the entire 5Ghz band
    In the current ACS algorithm to select channel for SAP operation
    there is no support to select any channel in the entire 5Ghz band
    this commit adds support for that.
    
    Change-Id: Iacb82800d3de5a041d12afa29947169e0f600eb3
    CRs-fixed: 616591

--- CORE/SAP/src/sapModule.c
+++ CORE/SAP/src/sapModule.c
@@ -1328,7 +1328,7 @@ WLANSAP_DeauthSta
 ============================================================================*/
 VOS_STATUS
 WLANSAP_SetChannelRange(tHalHandle hHal,v_U8_t startChannel, v_U8_t endChannel,
-                              v_U8_t operatingBand)
+                              eSapOperatingBand operatingBand)
 {
 
     v_U8_t    validChannelFlag =0;
@@ -1367,23 +1367,28 @@ WLANSAP_SetChannelRange(tHalHandle hHal,v_U8_t startChannel, v_U8_t endChannel,
     }
     switch(operatingBand)
     {
-       case RF_SUBBAND_2_4_GHZ:
+       case eSAP_RF_SUBBAND_2_4_GHZ:
           bandStartChannel = RF_CHAN_1;
           bandEndChannel = RF_CHAN_14;
           break;
 
-       case RF_SUBBAND_5_LOW_GHZ:
+       case eSAP_RF_SUBBAND_5_LOW_GHZ:
           bandStartChannel = RF_CHAN_36;
           bandEndChannel = RF_CHAN_64;
           break;
 
-       case RF_SUBBAND_5_MID_GHZ:
+       case eSAP_RF_SUBBAND_5_MID_GHZ:
           bandStartChannel = RF_CHAN_100;
           bandEndChannel = RF_CHAN_140;
           break;
 
-       case RF_SUBBAND_5_HIGH_GHZ:
+       case eSAP_RF_SUBBAND_5_HIGH_GHZ:
           bandStartChannel = RF_CHAN_149;
+          bandEndChannel = RF_CHAN_165;
+          break;
+
+       case eSAP_RF_SUBBAND_5_ALL_GHZ:
+          bandStartChannel = RF_CHAN_36;
           bandEndChannel = RF_CHAN_165;
           break;
 
