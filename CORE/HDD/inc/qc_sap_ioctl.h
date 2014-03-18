34998ca66ae4c262a1e4d1f26748be1609f1a8a6
Rajesh Chauhan <rajeshc@qca.qualcomm.com>
2014-03-18 12:15:55 -0700

    qcacld: Revert previous commit on QCSAP ioctl setAutoChannel
    Revert previous commit I7b1ef9e2240c3bc9249a97d0c345811f029e778e
    on QCSAP ioctl setAutoChannel.
    
    Change-Id: I5eebc02c3730bd0a4380b465b320f2c3864c5e32
    CRs-Fixed: 632376

--- CORE/HDD/inc/qc_sap_ioctl.h
+++ CORE/HDD/inc/qc_sap_ioctl.h
@@ -313,7 +313,6 @@ enum {
     QCSAP_PARAM_SET_TXRX_FW_STATS=11,
     QCSAP_PARAM_SET_MCC_CHANNEL_LATENCY = 12,
     QCSAP_PARAM_SET_MCC_CHANNEL_QUOTA = 13,
-    QCSAP_PARAM_SET_AUTO_CHANNEL = 14,
 };
 
 int iw_softap_get_channel_list(struct net_device *dev,
