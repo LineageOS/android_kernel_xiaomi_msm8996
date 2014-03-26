eef5f962728489f5037fb42ddd2056b4193707c8
Naresh Jayaram <c_njayar@qti.qualcomm.com>
2014-03-26 15:57:44 -0700

    TDLS: Enhance TDLS external control operations (gTDLSExternalControl=1)
    This commit enhances the external control operations to
    
    1.Allows gTDLSTxStatsPeriod to accept a minimal value of 10 msec.
    2.Allows gTDLSIdlePacketThreshold to accept a minimal value of 0 packets.
    3.Caters the requirement of establishing the TDLS link irrespective of
      the data traffic, the moment tdls_setup is issued by the framework.
    4.Only teardown based on non-zero idle packet threshold, to address a use
        case where this threshold does not get consider for TEAR DOWN
    
    Change-Id: I170afed07607440924e72515c23b2ddca8e83b62
    CRs-fixed: 647532

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -9574,6 +9574,7 @@ static int wlan_hdd_cfg80211_tdls_oper(struct wiphy *wiphy, struct net_device *d
             }
         case NL80211_TDLS_SETUP:
             {
+                hddTdlsPeer_t *pTdlsPeer;
                 VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
                           " %s : NL80211_TDLS_SETUP for " MAC_ADDRESS_STR,
                           __func__, MAC_ADDR_ARRAY(peer));
@@ -9587,7 +9588,27 @@ static int wlan_hdd_cfg80211_tdls_oper(struct wiphy *wiphy, struct net_device *d
                     return -ENOTSUPP;
                 }
 
-                return wlan_hdd_tdls_add_force_peer(pAdapter, peer);
+                if (0 != wlan_hdd_tdls_add_force_peer(pAdapter, peer)) {
+
+                    VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+                              "%s: NL80211_TDLS_SETUP: Add force peer failed",
+                               __func__);
+                    return -EINVAL;
+                }
+
+                /* To cater the requirement of establishing the TDLS link
+                 * irrespective of the data traffic , get an entry of TDLS peer.
+                 */
+                pTdlsPeer = wlan_hdd_tdls_get_peer(pAdapter, peer);
+
+                if (pTdlsPeer == NULL) {
+                    VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+                              "%s: peer "MAC_ADDRESS_STR
+                              " does not exist, NL80211_TDLS_SETUP failed",
+                               __func__, MAC_ADDR_ARRAY(peer));
+                    return -EINVAL;
+                }
+                break;
             }
         case NL80211_TDLS_DISCOVERY_REQ:
             /* We don't support in-driver setup/teardown/discovery */
