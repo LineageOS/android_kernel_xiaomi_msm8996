e3c04fd7ac4b1b60dfd1363901419c0352f815a7
Manikandaraja Venkatachalapathy <vmanikan@qca.qualcomm.com>
2014-03-30 18:22:41 -0700

    qcacld: CL 891944 - update fw common interface files
    Add tdls specific reason code in wmi header file.
    
    Change-Id: I61acb761063f71798324101a56ff9ec4e9271d56
    CRs-Fixed: 640860

--- CORE/SERVICES/COMMON/wmi_unified.h
+++ CORE/SERVICES/COMMON/wmi_unified.h
@@ -4591,6 +4591,7 @@ typedef enum wmi_peer_sta_kickout_reason {
     WMI_PEER_STA_KICKOUT_REASON_XRETRY = 1,
     WMI_PEER_STA_KICKOUT_REASON_INACTIVITY = 2,
     WMI_PEER_STA_KICKOUT_REASON_IBSS_DISCONNECT = 3,
+    WMI_PEER_STA_KICKOUT_REASON_TDLS_DISCONNECT = 4,    /* TDLS peer has disappeared. All tx is failing */
 } PEER_KICKOUT_REASON;
 
 typedef struct {
