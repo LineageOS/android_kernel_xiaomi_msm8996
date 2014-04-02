08c39be0467a4700ed1eb6164e80b743fee316ac
Pitani Venkata Rajesh Kumar <c_vpitan@qti.qualcomm.com>
2014-04-02 23:15:37 +0530

    Cafstaging Release 1.0.0.80
    Cafstaging Release 1.0.0.80
    
    Change-Id: I2214256fa6e11abc440afd1574a01cf1db843a56

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            79
+#define QWLAN_VERSION_BUILD            80
 
-#define QWLAN_VERSIONSTR               "1.0.0.79"
+#define QWLAN_VERSIONSTR               "1.0.0.80"
 
 #ifdef QCA_WIFI_2_0
 
