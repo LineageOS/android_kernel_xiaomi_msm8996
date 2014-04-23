13e26600132f89477bcbff210817b4d5d2e887ab
Rajeev Kumar <rajekuma@qca.qualcomm.com>
2014-04-22 20:09:46 -0700

    qcacld:record 8 bytes of WMI cmd in interface log buffers
    Record 8 bytes of WMI command buffer data excluding TLV and WMI
    headers in WMI interface log buffers
    
    Change-Id: Ie8f13c2e6765d48141d42fc16e35470b954d3c7d
    CRs-Fixed: 653548

--- CORE/SERVICES/WMI/wmi_unified.c
+++ CORE/SERVICES/WMI/wmi_unified.c
@@ -64,10 +64,11 @@ struct wmi_command_debug wmi_command_log_buffer[WMI_EVENT_DEBUG_MAX_ENTRY];
 u_int32_t g_wmi_event_buf_idx = 0;
 struct wmi_event_debug wmi_event_log_buffer[WMI_EVENT_DEBUG_MAX_ENTRY];
 
-#define WMI_COMMAND_RECORD(a) {						\
+#define WMI_COMMAND_RECORD(a, b) {					\
 	if (WMI_EVENT_DEBUG_MAX_ENTRY <= g_wmi_command_buf_idx)		\
 		g_wmi_command_buf_idx = 0;				\
 	wmi_command_log_buffer[g_wmi_command_buf_idx].command = a;	\
+	adf_os_mem_copy(wmi_command_log_buffer[g_wmi_command_buf_idx].data, b , 8);\
 	wmi_command_log_buffer[g_wmi_command_buf_idx].time =		\
 						 adf_os_ticks();	\
 	g_wmi_command_buf_idx++;					\
@@ -606,7 +607,8 @@ int wmi_unified_cmd_send(wmi_unified_t wmi_handle, wmi_buf_t buf, int len,
 
 #ifdef WMI_INTERFACE_EVENT_LOGGING
 	adf_os_spin_lock_bh(&wmi_handle->wmi_record_lock);
-	WMI_COMMAND_RECORD(cmd_id);
+        /*Record 8 bytes of WMI cmd data - exclude TLV and WMI headers*/
+        WMI_COMMAND_RECORD(cmd_id ,((u_int32_t *)adf_nbuf_data(buf) + 2));
 	adf_os_spin_unlock_bh(&wmi_handle->wmi_record_lock);
 #endif
 
