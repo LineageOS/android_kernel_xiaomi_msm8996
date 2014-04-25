301a18412cac845e5a1675f70d8d72a90ed34314
krunal soni <ksoni@qca.qualcomm.com>
2014-04-25 12:43:25 -0700

    wlan: Fix SA Query Timeout
    When in station mode, with a PMF protected connection to the AP,
    if an SA Query procedure is initiated with the AP then the procedure
    always times out. This is true even if the PMF connection with the
    AP is still active and both the station and the AP still have the
    correct security context.
    
    This is fixed by sending the SA Query Response to the supplicant
    when the device is in station mode instead of having the driver
    handle the SA Query Response.
    
    Change-Id: I838e75410e43e7899b77659f7549234263e17feb
    CRs-Fixed: 655522

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -1200,6 +1200,13 @@ void wlan_hdd_cfg80211_post_voss_start(hdd_adapter_t* pAdapter)
     sme_RegisterMgmtFrame(hHal, HDD_SESSION_ID_ANY, type,
                          (v_U8_t*)WNM_BSS_ACTION_FRAME,
                                   WNM_BSS_ACTION_FRAME_SIZE );
+#ifdef WLAN_FEATURE_11W
+    /* SA Query Response Action Frame */
+    sme_RegisterMgmtFrame(hHal, pAdapter->sessionId, type,
+                         (v_U8_t*)SA_QUERY_FRAME_RSP,
+                                  SA_QUERY_FRAME_RSP_SIZE );
+#endif /* WLAN_FEATURE_11W */
+
 }
 
 void wlan_hdd_cfg80211_pre_voss_stop(hdd_adapter_t* pAdapter)
