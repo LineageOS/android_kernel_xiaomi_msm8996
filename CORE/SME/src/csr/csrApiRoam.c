20c2a837c088dac0a539b349313bfa2809591c7d
Nirav Shah <nnshah@qti.qualcomm.com>
2014-05-01 16:03:01 +0530

    P2P: Add social channels in valid channel list for 5G Only Band
    Add social channels in valid channel list sent to FW for
    5G only band as well for P2P operation.
    
    Change-Id: I2bc3dcc758a943e5cab4f5a00c93ea675766080b
    CRs-Fixed: 657917

--- CORE/SME/src/csr/csrApiRoam.c
+++ CORE/SME/src/csr/csrApiRoam.c
@@ -97,6 +97,8 @@
 #define RSSI_HACK_BMPS (-40)
 #define MAX_CB_VALUE_IN_INI (2)
 
+#define MAX_SOCIAL_CHANNELS  3
+
 #ifdef WLAN_FEATURE_ROAM_SCAN_OFFLOAD
 static tANI_BOOLEAN bRoamScanOffloadStarted = VOS_FALSE;
 #endif
@@ -466,14 +468,27 @@ static tChannelPwrLimit csrFindChannelPwr(tChannelListWithPower * pdefaultPowerT
     return 0;
 }
 
-eHalStatus csrUpdateChannelList(tCsrScanStruct *pScan)
+eHalStatus csrUpdateChannelList(tpAniSirGlobal pMac)
 {
     tSirUpdateChanList *pChanList;
+    tCsrScanStruct *pScan = &pMac->scan;
     tANI_U8 numChan = pScan->base20MHzChannels.numChannels;
-    tANI_U32 bufLen = sizeof(tSirUpdateChanList) +
-        (sizeof(tSirUpdateChanParam) * (numChan - 1));
+    tANI_U32 bufLen;
     vos_msg_t msg;
-    tANI_U8 i;
+    tANI_U8 i, j, social_channel[MAX_SOCIAL_CHANNELS] = {1,6,11};
+
+    if (CSR_IS_5G_BAND_ONLY(pMac))
+    {
+        for (i = 0; i < MAX_SOCIAL_CHANNELS; i++)
+        {
+            if (vos_nv_getChannelEnabledState(social_channel[i])
+                         == NV_CHANNEL_ENABLE)
+                numChan++;
+        }
+    }
+
+    bufLen = sizeof(tSirUpdateChanList) +
+        (sizeof(tSirUpdateChanParam) * (numChan - 1));
 
     pChanList = (tSirUpdateChanList *) vos_mem_malloc(bufLen);
     if (!pChanList)
@@ -487,7 +502,7 @@ eHalStatus csrUpdateChannelList(tCsrScanStruct *pScan)
     msg.reserved = 0;
     msg.bodyptr = pChanList;
     pChanList->numChan = numChan;
-    for (i = 0; i < pChanList->numChan; i++)
+    for (i = 0; i < pScan->base20MHzChannels.numChannels; i++)
     {
         pChanList->chanParam[i].chanId =
                              pScan->base20MHzChannels.channelList[i];
@@ -500,6 +515,22 @@ eHalStatus csrUpdateChannelList(tCsrScanStruct *pScan)
             pChanList->chanParam[i].dfsSet = VOS_TRUE;
         else
             pChanList->chanParam[i].dfsSet = VOS_FALSE;
+    }
+
+    if (CSR_IS_5G_BAND_ONLY(pMac))
+    {
+        for (j = 0; j < MAX_SOCIAL_CHANNELS; j++)
+        {
+            if (vos_nv_getChannelEnabledState(social_channel[j])
+                         == NV_CHANNEL_ENABLE)
+            {
+                pChanList->chanParam[i].chanId = social_channel[j];
+                pChanList->chanParam[i].pwr =
+                 csrFindChannelPwr(pScan->defaultPowerTable, social_channel[j]);
+                pChanList->chanParam[i].dfsSet = VOS_FALSE;
+                i++;
+            }
+        }
     }
 
     if(VOS_STATUS_SUCCESS != vos_mq_post_message(VOS_MODULE_ID_WDA, &msg))
@@ -568,7 +599,7 @@ eHalStatus csrStart(tpAniSirGlobal pMac)
         {
             VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO,
                     "Scan offload is enabled, update default chan list");
-            status = csrUpdateChannelList(&pMac->scan);
+            status = csrUpdateChannelList(pMac);
         }
 
     }while(0);
