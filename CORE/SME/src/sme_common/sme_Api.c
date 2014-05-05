d5ec2b64b354b7ba094372a4183908d96a9041de
Rakesh Sunki <rsunki@qca.qualcomm.com>
2014-05-05 14:45:45 -0700

    qcacld: Fix sessionId mismatch for SAP DFS messages
    Fix sessionID mismatch caused when SAP ACS
    feature is turned on.  The SME sessionID
    is passed as part of CSA IE Tx completion
    message and Channel Change message from
    PE to SME.
    
    Change-Id: I2030a2b619a8d76269e4bed71f6271ca6f3e1d4d
    CRs-Fixed: 658283

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -2025,6 +2025,7 @@ eHalStatus dfsMsgProcessor(tpAniSirGlobal pMac, v_U16_t msgType, void *pMsgBuf)
     eHalStatus status = eHAL_STATUS_SUCCESS;
     tCsrRoamInfo roamInfo = {0};
     tSirSmeDfsEventInd *dfs_event;
+    tSirSmeCSAIeTxCompleteRsp *csaIeTxCompleteRsp;
     tANI_U32 sessionId = 0;
     eRoamCmdStatus roamStatus;
     eCsrRoamResult roamResult;
@@ -2055,6 +2056,15 @@ eHalStatus dfsMsgProcessor(tpAniSirGlobal pMac, v_U16_t msgType, void *pMsgBuf)
       }
       case eWNI_SME_DFS_CSAIE_TX_COMPLETE_IND:
       {
+         csaIeTxCompleteRsp = (tSirSmeCSAIeTxCompleteRsp *)pMsgBuf;
+         if (NULL == csaIeTxCompleteRsp)
+         {
+            smsLog(pMac, LOGE,
+                   "%s: pMsg is NULL for eWNI_SME_DFS_CSAIE_TX_COMPLETE_IND",
+                   __func__);
+            return eHAL_STATUS_FAILURE;
+         }
+         sessionId = csaIeTxCompleteRsp->sessionId;
          roamStatus = eCSR_ROAM_DFS_CHAN_SW_NOTIFY;
          roamResult = eCSR_ROAM_RESULT_DFS_CHANSW_UPDATE_SUCCESS;
          break;
