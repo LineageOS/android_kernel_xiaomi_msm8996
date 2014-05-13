157f6e241d338d8de9c51231c33dda66accd01b5
Naveen Rawat <nrawat@qca.qualcomm.com>
2014-05-13 09:46:41 -0700

    qcacld: Fix for SETBAND connecting to wrong AP
    When SETBAND is issued, flushing SME scan cache should,
    be followed by flushing lim scan cache, else it will contain
    inconsistent scan results due to old AP's beacon.
    
    Change-Id: I79b34a5ac11808cbc99b1b7a46fe8d90e003f0bd
    CRs-Fixed: 660917

--- CORE/SME/src/csr/csrApiScan.c
+++ CORE/SME/src/csr/csrApiScan.c
@@ -2403,11 +2403,27 @@ eHalStatus csrScanFlushResult(tpAniSirGlobal pMac)
 {
     tANI_U8 isFlushDenied = csrScanFlushDenied(pMac);
     eHalStatus status = eHAL_STATUS_SUCCESS;
+    tSirMbMsg *pMsg;
+    tANI_U16 msgLen;
+
     if (isFlushDenied) {
         smsLog(pMac, LOGW, "%s: scan flush denied in roam state %d",
                 __func__, isFlushDenied);
         return eHAL_STATUS_FAILURE;
     }
+
+    /* prepare and send clear cached scan results msg to lim */
+    msgLen = (tANI_U16)(sizeof( tSirMbMsg ));
+    pMsg = vos_mem_malloc(msgLen);
+    if ( NULL != pMsg ) {
+        vos_mem_set((void *)pMsg, msgLen, 0);
+        pMsg->type = pal_cpu_to_be16((tANI_U16)eWNI_SME_CLEAR_LIM_SCAN_CACHE);
+        pMsg->msgLen = pal_cpu_to_be16(msgLen);
+        palSendMBMessage(pMac->hHdd, pMsg);
+    } else {
+        status = eHAL_STATUS_FAILED_ALLOC;
+    }
+
     csrLLScanPurgeResult( pMac, &pMac->scan.tempScanResults );
     csrLLScanPurgeResult( pMac, &pMac->scan.scanResultList );
     return( status );
