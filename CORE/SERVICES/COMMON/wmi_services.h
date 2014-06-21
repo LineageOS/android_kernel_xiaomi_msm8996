76d433e1fab6e60270872ce2f505243659714779
Krishna Kumaar Natarajan <kknatara@qca.qualcomm.com>
2014-06-21 15:32:04 -0700

    qcacld: CL 992741 994298 994458 995875 - update fw common interface files
    This commit adds wmi interface changes which includes
    new WLAN module ID for HIF UART module, vdev_id to the ERE offload
    messages, LPI (Low Power Interface) module and add response for
    peer authorize.
    
    Change-Id: Ibabe86540453f38a8f39d1df7fc846c5fd4ad674
    CRs-Fixed: 682949

--- CORE/SERVICES/COMMON/wmi_services.h
+++ CORE/SERVICES/COMMON/wmi_services.h
@@ -105,6 +105,7 @@ typedef  enum  {
     WMI_SERVICE_TDLS_UAPSD_BUFFER_STA, /* TDLS UAPSD Buffer STA support */
     WMI_SERVICE_TDLS_UAPSD_SLEEP_STA,  /* TDLS UAPSD Sleep STA support */
     WMI_SERVICE_IBSS_PWRSAVE,         /* IBSS power save support */
+    WMI_SERVICE_LPASS,                /*Service to support LPASS*/
     WMI_MAX_SERVICE=128               /* max service */
 } WMI_SERVICE;
 
