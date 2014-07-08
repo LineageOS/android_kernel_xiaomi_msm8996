4471bae18267d24818122b93ad7d666a531867e8
Chandrasekaran, Manishekar <cmshekar@qti.qualcomm.com>
2014-07-08 20:08:31 +0530

    qcacld: Abort scan for SSID if deauth is received from supplicant.
    During connection if AP is not found in scan list, driver tries
    to scan for the ssid (eCsrScanForSsid) and if AP is found,
    the connect cmd is started.
    
    During this scan if Disconnect comes from supplicant, as there is
    no ongoing connect command and sme is not in connected state, the
    disconnect command is dropped in SME and hdd is indicated success.
    This leads to HDD waiting on the event disconnect_comp_var and
    connState to eConnectionState_Disconnecting.
    
    After this if supplicant send connect and as HDD is in
    disconnecting state it will again wait for disconnect_comp_var
    and return failure after timeout and this will continue and user
    will not able to connect.
    
    If disconnect is received during eCsrScanForSsid abort/dequeue
    the eCsrScanForSsid in SME and in HDD set the connState to
    eConnectionState_NotConnected and set disconnect_comp_var
    complete.
    
    Change-Id: I1da89065e1e59bcf08806179f85e3d9d00d5828e
    CRs-Fixed: 689890

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -9849,6 +9849,7 @@ int wlan_hdd_disconnect( hdd_adapter_t *pAdapter, u16 reason )
     int status;
     hdd_station_ctx_t *pHddStaCtx = WLAN_HDD_GET_STATION_CTX_PTR(pAdapter);
     hdd_context_t *pHddCtx = WLAN_HDD_GET_CTX(pAdapter);
+    long ret;
 
     status = wlan_hdd_validate_context(pHddCtx);
 
@@ -9873,8 +9874,13 @@ int wlan_hdd_disconnect( hdd_adapter_t *pAdapter, u16 reason )
 
     status = sme_RoamDisconnect( WLAN_HDD_GET_HAL_CTX(pAdapter),
                                  pAdapter->sessionId, reason);
-
-    if ( 0 != status )
+    if(eHAL_STATUS_CMD_NOT_QUEUED == status)
+    {
+        hddLog(VOS_TRACE_LEVEL_INFO,
+               FL("status = %d, already disconnected"),
+                      (int)status );
+    }
+    else if ( 0 != status )
     {
         hddLog(VOS_TRACE_LEVEL_ERROR,
                "%s csrRoamDisconnect failure, returned %d",
@@ -9882,23 +9888,25 @@ int wlan_hdd_disconnect( hdd_adapter_t *pAdapter, u16 reason )
         pHddStaCtx->staDebugState = status;
         return -EINVAL;
     }
-    status = wait_for_completion_interruptible_timeout(
+    ret = wait_for_completion_interruptible_timeout(
                 &pAdapter->disconnect_comp_var,
                 msecs_to_jiffies(WLAN_WAIT_TIME_DISCONNECT));
 
-    pHddStaCtx->conn_info.connState = eConnectionState_NotConnected;
-
-    if (!status)
+    if (!ret && ( eHAL_STATUS_CMD_NOT_QUEUED != status ))
     {
        hddLog(VOS_TRACE_LEVEL_ERROR,
               "%s: Failed to disconnect, timed out", __func__);
        return -ETIMEDOUT;
-    } else if (status == -ERESTARTSYS)
+    }
+    else if (ret == -ERESTARTSYS)
     {
         hddLog(VOS_TRACE_LEVEL_ERROR,
                "%s: Failed to disconnect, wait interrupted", __func__);
-        return status;
+        return ret;
     }
+    VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
+             FL("Set HDD connState to eConnectionState_NotConnected"));
+    pHddStaCtx->conn_info.connState = eConnectionState_NotConnected;
 
     return 0;
 }
