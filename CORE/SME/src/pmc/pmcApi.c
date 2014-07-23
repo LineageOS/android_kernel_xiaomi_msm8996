5b970c1388a808df27b9cf4873e63e8b303e5509
William Seto <wseto@qti.qualcomm.com>
2014-07-23 14:29:17 -0700

    wlan: Remove INTEGRATION_READY and GEN6_ONWARDS flags
    Removed conditional flags in qcacld driver so that conditional
    compilation is now unconditional. Also, code that is conditional
    on these 2 flags being NOT defined is now removed
    
    Change-Id: I5edff28ec3e643c678e9b856d1f602bffe1de686
    CRs-fixed: 698872

--- CORE/SME/src/pmc/pmcApi.c
+++ CORE/SME/src/pmc/pmcApi.c
@@ -405,67 +405,12 @@ eHalStatus pmcClose (tHalHandle hHal)
 eHalStatus pmcSignalPowerEvent (tHalHandle hHal, tPmcPowerEvent event)
 {
     tpAniSirGlobal pMac = PMAC_STRUCT(hHal);
-#ifndef GEN6_ONWARDS
-    tSirMacHTMIMOPowerSaveState  htMimoPowerSaveState;
-#endif
 
     pmcLog(pMac, LOG2, FL("Entering pmcSignalPowerEvent, event %d"), event);
 
     /* Take action based on the event being signaled. */
     switch (event)
     {
-#ifndef GEN6_ONWARDS
-    case ePMC_SYSTEM_HIBERNATE:
-        return pmcEnterLowPowerState(hHal);
-
-    case ePMC_SYSTEM_RESUME:
-        return pmcExitLowPowerState(hHal);
-
-    case ePMC_HW_WLAN_SWITCH_OFF:
-        pMac->pmc.hwWlanSwitchState = ePMC_SWITCH_OFF;
-        return pmcEnterLowPowerState(hHal);
-
-    case ePMC_HW_WLAN_SWITCH_ON:
-        pMac->pmc.hwWlanSwitchState = ePMC_SWITCH_ON;
-        return pmcExitLowPowerState(hHal);
-
-    case ePMC_SW_WLAN_SWITCH_OFF:
-        pMac->pmc.swWlanSwitchState = ePMC_SWITCH_OFF;
-        return pmcEnterLowPowerState(hHal);
-
-    case ePMC_SW_WLAN_SWITCH_ON:
-        pMac->pmc.swWlanSwitchState = ePMC_SWITCH_ON;
-        return pmcExitLowPowerState(hHal);
-
-    case ePMC_BATTERY_OPERATION:
-        pMac->pmc.powerSource = BATTERY_POWER;
-
-        /* Turn on SMPS. */
-        if (pMac->pmc.smpsEnabled)
-        {
-            if (pMac->pmc.smpsConfig.mode == ePMC_DYNAMIC_SMPS)
-                htMimoPowerSaveState = eSIR_HT_MIMO_PS_DYNAMIC;
-            if (pMac->pmc.smpsConfig.mode == ePMC_STATIC_SMPS)
-                htMimoPowerSaveState = eSIR_HT_MIMO_PS_STATIC;
-            if (pmcSendMessage(hHal, eWNI_PMC_SMPS_STATE_IND, &htMimoPowerSaveState,
-                               sizeof(tSirMacHTMIMOPowerSaveState)) != eHAL_STATUS_SUCCESS)
-                return eHAL_STATUS_FAILURE;
-        }
-        return eHAL_STATUS_SUCCESS;
-
-    case ePMC_AC_OPERATION:
-        pMac->pmc.powerSource = AC_POWER;
-
-        /* Turn off SMPS. */
-        if (!pMac->pmc.smpsConfig.enterOnAc)
-        {
-            htMimoPowerSaveState = eSIR_HT_MIMO_PS_NO_LIMIT;
-            if (pmcSendMessage(hHal, eWNI_PMC_SMPS_STATE_IND, &htMimoPowerSaveState,
-                               sizeof(tSirMacHTMIMOPowerSaveState)) != eHAL_STATUS_SUCCESS)
-                return eHAL_STATUS_FAILURE;
-        }
-        return eHAL_STATUS_SUCCESS;
-#endif //GEN6_ONWARDS
     default:
         pmcLog(pMac, LOGE, FL("Invalid event %d"), event);
         PMC_ABORT;
