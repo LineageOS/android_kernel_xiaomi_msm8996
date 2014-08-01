80a5941c16c67f77b899fde16abcdb8aaf6a3e01
Yun Park <yunp@qca.qualcomm.com>
2014-08-01 13:17:30 -0700

    qcacld: Remove SAP interface name "softap.0"
    This change is to completely remove the SAP interface name "softap.0".
    Also removed initAP/exitAP from iwpriv command.
    
    Change-Id: I4929e1f6dec43b60b891f4fd338e955e0fc48682
    CRs-fixed: 711036

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -11359,60 +11359,52 @@ int hdd_wlan_startup(struct device *dev, v_VOID_t *hif_sc)
       return VOS_STATUS_SUCCESS;
    }
 
-   if (VOS_STA_SAP_MODE == hdd_get_conparam())
-   {
-     pAdapter = hdd_open_adapter( pHddCtx, WLAN_HDD_SOFTAP, "softap.%d",
-         wlan_hdd_get_intf_addr(pHddCtx), FALSE );
-   }
-   else
-   {
-     pAdapter = hdd_open_adapter( pHddCtx, WLAN_HDD_INFRA_STATION, "wlan%d",
-         wlan_hdd_get_intf_addr(pHddCtx), FALSE );
+   pAdapter = hdd_open_adapter( pHddCtx, WLAN_HDD_INFRA_STATION, "wlan%d",
+       wlan_hdd_get_intf_addr(pHddCtx), FALSE );
 
 #ifdef WLAN_OPEN_P2P_INTERFACE
-     /* Open P2P device interface */
-     if (pAdapter != NULL)
-     {
-         if ( pHddCtx->cfg_ini->isP2pDeviceAddrAdministrated )
-         {
-               vos_mem_copy( pHddCtx->p2pDeviceAddress.bytes,
-                       pHddCtx->cfg_ini->intfMacAddr[0].bytes,
-                       sizeof(tSirMacAddr));
+   /* Open P2P device interface */
+   if (pAdapter != NULL)
+   {
+      if (pHddCtx->cfg_ini->isP2pDeviceAddrAdministrated)
+      {
+         vos_mem_copy( pHddCtx->p2pDeviceAddress.bytes,
+                     pHddCtx->cfg_ini->intfMacAddr[0].bytes,
+                     sizeof(tSirMacAddr));
 
-                /* Generate the P2P Device Address.  This consists of the device's
-                 * primary MAC address with the locally administered bit set.
-                */
-                pHddCtx->p2pDeviceAddress.bytes[0] |= 0x02;
+         /* Generate the P2P Device Address.  This consists of the device's
+          * primary MAC address with the locally administered bit set.
+          */
+         pHddCtx->p2pDeviceAddress.bytes[0] |= 0x02;
+      }
+      else
+      {
+         tANI_U8* p2p_dev_addr = wlan_hdd_get_intf_addr(pHddCtx);
+         if (p2p_dev_addr != NULL)
+         {
+            vos_mem_copy(&pHddCtx->p2pDeviceAddress.bytes[0],
+                         p2p_dev_addr, VOS_MAC_ADDR_SIZE);
          }
          else
          {
-             tANI_U8* p2p_dev_addr = wlan_hdd_get_intf_addr(pHddCtx);
-             if (p2p_dev_addr != NULL)
-             {
-                 vos_mem_copy(&pHddCtx->p2pDeviceAddress.bytes[0],
-                             p2p_dev_addr, VOS_MAC_ADDR_SIZE);
-             }
-             else
-             {
-                   hddLog(VOS_TRACE_LEVEL_FATAL,
-                           "%s: Failed to allocate mac_address for p2p_device",
+            hddLog(VOS_TRACE_LEVEL_FATAL,
+                   "%s: Failed to allocate mac_address for p2p_device",
                    __func__);
-                   goto err_close_adapter;
-             }
+            goto err_close_adapter;
          }
+      }
 
-         pP2pAdapter = hdd_open_adapter( pHddCtx, WLAN_HDD_P2P_DEVICE, "p2p%d",
-                           &pHddCtx->p2pDeviceAddress.bytes[0], FALSE );
-         if ( NULL == pP2pAdapter )
-         {
-             hddLog(VOS_TRACE_LEVEL_FATAL,
+      pP2pAdapter = hdd_open_adapter( pHddCtx, WLAN_HDD_P2P_DEVICE, "p2p%d",
+                        &pHddCtx->p2pDeviceAddress.bytes[0], FALSE );
+      if ( NULL == pP2pAdapter )
+      {
+         hddLog(VOS_TRACE_LEVEL_FATAL,
                 "%s: Failed to do hdd_open_adapter for P2P Device Interface",
                 __func__);
-             goto err_close_adapter;
-         }
-     }
-#endif
+         goto err_close_adapter;
+      }
    }
+#endif
 
    if( pAdapter == NULL )
    {
