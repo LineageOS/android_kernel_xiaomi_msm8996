b6d46629fa9fe78697db16e73446cbf0ed007a99
Manikandaraja Venkatachalapathy <vmanikan@qca.qualcomm.com>
2014-08-07 16:31:29 -0700

    qcacld: WMA: Fill up channel frequency
    Fill channel frequency field for WDA_TxPacket
    
    Change-Id: I568ad408ab60e557731d6f5d1658ee1da35bb34a
    CRs-Fixed: 706495

--- CORE/SERVICES/WMA/wma.c
+++ CORE/SERVICES/WMA/wma.c
@@ -24272,7 +24272,9 @@ VOS_STATUS WDA_TxPacket(void *wma_context, void *tx_frame, u_int16_t frmLen,
         if (wma_handle->roam_preauth_scan_state == WMA_ROAM_PREAUTH_ON_CHAN) {
                 chanfreq = wma_handle->roam_preauth_chanfreq;
                 WMA_LOGI("%s: Preauth frame on channel %d", __func__, chanfreq);
-        } else {
+        } else if(pFc->subType == SIR_MAC_MGMT_PROBE_RSP){
+		chanfreq = wma_handle->interfaces[vdev_id].mhz;
+	} else {
                 chanfreq = 0;
         }
         if (pMac->fEnableDebugLog & 0x1) {
