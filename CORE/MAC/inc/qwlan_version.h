c8c834f8b160532b432c0ba198d1df0d67641226
Akash Patel <c_akashp@qca.qualcomm.com>
2014-09-04 17:42:09 -0700

    Cafstaging Release 1.0.0.184A
    Cafstaging Release 1.0.0.184A
    
    Change-Id: I36adeda0c80bf74c3ff8f6a01fe9b7a7604e636d
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,7 +44,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_EXTRA            ""
 #define QWLAN_VERSION_BUILD            184
 
-#define QWLAN_VERSIONSTR               "1.0.0.184"
+#define QWLAN_VERSIONSTR               "1.0.0.184A"
 
 
 #define AR6320_REV1_VERSION             0x5000000
