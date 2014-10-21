2395502062eaa7a934fdfde1d326b30f249df902
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2014-10-21 12:16:47 +0530

    Cafstaging Release 1.0.0.216
    Cafstaging Release 1.0.0.216
    
    Change-Id: Ia9bb1050fbbbff56e5c431594520b8af06d72446
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            215
+#define QWLAN_VERSION_BUILD            216
 
-#define QWLAN_VERSIONSTR               "1.0.0.215"
+#define QWLAN_VERSIONSTR               "1.0.0.216"
 
 
 #define AR6320_REV1_VERSION             0x5000000
