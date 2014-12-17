a6401914e7188fce565a01a6bf9b4539b10d86eb
Srinivas Girigowda <sgirigow@qca.qualcomm.com>
2014-12-17 10:20:09 -0800

    qcacld: Do not clear tdls sta to adapter mapping
    This is prima to qcacld-2.0 propagation.
    
    Do not make the "sta to adapter" mapping to NULL in
    hdd_RoamTdlsStatusUpdateHandler() when the associated TDLS
    sessions on that station's context are deleted. This mapping
    is tied to the station adapter and has to be removed only when
    the station is disconnected to the AP. TL thread shall check
    for the existence of this mapping while fetching the packets
    queued for that adapter and would assert if this mapping is
    nullified. Since at the point of TDLS peer's session termination
    the station connection to the AP is not disturbed, there is no
    point in making this mapping NULL.
    
    Change-Id: Ic6e97d33fa7d7699bdf76cbf0cd24796977a6f49
    CRs-Fixed: 772537

--- CORE/HDD/src/wlan_hdd_assoc.c
+++ CORE/HDD/src/wlan_hdd_assoc.c
@@ -2846,7 +2846,6 @@ hdd_RoamTdlsStatusUpdateHandler(hdd_adapter_t *pAdapter,
                         wlan_hdd_tdls_decrement_peer_count(pAdapter);
                     }
                     wlan_hdd_tdls_reset_peer(pAdapter, pRoamInfo->peerMac);
-                    (WLAN_HDD_GET_CTX(pAdapter))->sta_to_adapter[pRoamInfo->staId] = NULL;
 
                     pHddCtx->tdlsConnInfo[staIdx].staId = 0 ;
                     pHddCtx->tdlsConnInfo[staIdx].sessionId = 255;
@@ -2912,7 +2911,6 @@ hdd_RoamTdlsStatusUpdateHandler(hdd_adapter_t *pAdapter,
                     }
                     wlan_hdd_tdls_decrement_peer_count(pAdapter);
 
-                    (WLAN_HDD_GET_CTX(pAdapter))->sta_to_adapter[staIdx] = NULL;
                     vos_mem_zero(&pHddCtx->tdlsConnInfo[staIdx].peerMac,
                                                sizeof(v_MACADDR_t)) ;
                     pHddCtx->tdlsConnInfo[staIdx].staId = 0 ;
