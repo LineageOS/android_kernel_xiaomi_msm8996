0e0c6c18168a32603527b1c9cf741b4873c41e0c
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2014-12-30 12:54:31 +0530

    Cafstaging Release 4.0.10.007A
    Cafstaging Release 4.0.10.007A
    
    Change-Id: I66d1dbc832cc64778ff030191753adbd3b7e5bcc
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,7 +44,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_EXTRA            ""
 #define QWLAN_VERSION_BUILD            007
 
-#define QWLAN_VERSIONSTR               "4.0.10.007"
+#define QWLAN_VERSIONSTR               "4.0.10.007A"
 
 
 #define AR6320_REV1_VERSION             0x5000000
