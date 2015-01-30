3fabc3c15cbacabb28b57c70096236487a1ac43d
Manikandan Mohan <manikand@qca.qualcomm.com>
2015-01-29 21:40:39 -0800

    HDD: Save WPA group key in SAP context for reuse
    WPA Group key need to be stored in SAP session context for reuse
    when SAP is restarted internally in driver. This key is then set
    in START_BSS event after SAP restart.
    
    Change-Id: I6cfb1d69c0a8011a433dccfcab14a93a551eaafa
    CRs-fixed: 767351

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -8745,6 +8745,7 @@ static int __wlan_hdd_cfg80211_add_key( struct wiphy *wiphy,
     hdd_hostapd_state_t *pHostapdState;
     eHalStatus halStatus;
     hdd_context_t *pHddCtx;
+    hdd_ap_ctx_t *ap_ctx = WLAN_HDD_GET_AP_CTX_PTR(pAdapter);
 
     ENTER();
 
@@ -8943,20 +8944,15 @@ static int __wlan_hdd_cfg80211_add_key( struct wiphy *wiphy,
             }
         }
 
-        /* Saving WEP keys */
-        else if( eCSR_ENCRYPT_TYPE_WEP40_STATICKEY  == setKey.encType ||
-                eCSR_ENCRYPT_TYPE_WEP104_STATICKEY  == setKey.encType  )
-        {
-            //Save the wep key in ap context. Issue setkey after the BSS is started.
-            hdd_ap_ctx_t *pAPCtx = WLAN_HDD_GET_AP_CTX_PTR(pAdapter);
-            vos_mem_copy(&pAPCtx->wepKey[key_index], &setKey, sizeof(tCsrRoamSetKey));
-        }
+        if (pairwise ||
+                eCSR_ENCRYPT_TYPE_WEP40_STATICKEY == setKey.encType ||
+                eCSR_ENCRYPT_TYPE_WEP104_STATICKEY  == setKey.encType)
+            vos_mem_copy(&ap_ctx->wepKey[key_index], &setKey,
+                                               sizeof(tCsrRoamSetKey));
         else
-        {
-            //Save the key in ap context. Issue setkey after the BSS is started.
-            hdd_ap_ctx_t *pAPCtx = WLAN_HDD_GET_AP_CTX_PTR(pAdapter);
-            vos_mem_copy(&pAPCtx->groupKey, &setKey, sizeof(tCsrRoamSetKey));
-        }
+            vos_mem_copy(&ap_ctx->groupKey, &setKey,
+                                               sizeof(tCsrRoamSetKey));
+
     }
     else if ( (pAdapter->device_mode == WLAN_HDD_INFRA_STATION) ||
               (pAdapter->device_mode == WLAN_HDD_P2P_CLIENT) )
