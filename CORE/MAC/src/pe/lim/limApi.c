c259a1378024b75903a25b9988099d75e2bc757b
Krishna Kumaar Natarajan <kknatara@qca.qualcomm.com>
2015-02-20 13:41:33 -0800

    wlan: Minimizing the retry log for Re/Assoc Rsp frame.
    This is prima to qcacld-2.0 propagation
    
    Due to some AP; Dut is receiving Re/assoc frame in wrong
    state. Log "received Re/Assoc rsp frame is not a retry frame"
    prints excessively when some buggy AP keep sending Re/Assoc frame.
    Minimizing the logs once in 15 prints.
    
    Change-Id: Ic9d6ed872127b7e87adc887b0afaf8674119420f
    CRs-Fixed: 798344

--- CORE/MAC/src/pe/lim/limApi.c
+++ CORE/MAC/src/pe/lim/limApi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2015 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -849,6 +849,7 @@ tSirRetStatus peOpen(tpAniSirGlobal pMac, tMacOpenParameters *pMacOpenParam)
         goto pe_open_lock_fail;
     }
     pMac->lim.deauthMsgCnt = 0;
+    pMac->lim.retry_packet_cnt = 0;
 
     /*
      * peOpen is successful by now, so it is right time to initialize
