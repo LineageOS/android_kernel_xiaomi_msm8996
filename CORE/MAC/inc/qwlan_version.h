4963fd68ed4a1bf0592448e1a2572cb5ac9552f1
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2015-03-06 15:09:06 +0530

    Cafstaging Release 4.0.10.49
    Cafstaging Release 4.0.10.49
    
    Change-Id: I45ee036f9a02933037b6fe4943cd22de83ba1839
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            10
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            48 
+#define QWLAN_VERSION_BUILD            49 
 
-#define QWLAN_VERSIONSTR               "4.0.10.48"
+#define QWLAN_VERSIONSTR               "4.0.10.49"
 
 
 #define AR6320_REV1_VERSION             0x5000000
