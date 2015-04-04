703e4d76dba4e715cc5fc4c58280b6c86f2cd41e
Varun Reddy Yeturu <vyeturu@qca.qualcomm.com>
2015-04-03 17:17:41 -0700

    qcacld-2.0: Enable HI_RSSI feature only when connected to 2.4GHz AP
    1) Enable the feature only when connected to 2.4 GHz AP.
    2) Modify the default values to reduce the scans and not impact power
    
    CRs-Fixed: 817919
    Change-Id: Idaff886cec558a06cf9341e47a8ce17b348ad72b

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2769,7 +2769,7 @@ enum dot11p_mode {
 #define CFG_ROAM_SCAN_HI_RSSI_MAXCOUNT_NAME         "gRoamScanHiRssiMaxCount"
 #define CFG_ROAM_SCAN_HI_RSSI_MAXCOUNT_MIN          (0)
 #define CFG_ROAM_SCAN_HI_RSSI_MAXCOUNT_MAX          (10)
-#define CFG_ROAM_SCAN_HI_RSSI_MAXCOUNT_DEFAULT      (5)
+#define CFG_ROAM_SCAN_HI_RSSI_MAXCOUNT_DEFAULT      (3)
 
 /* Change in RSSI at which scan is triggered */
 #define CFG_ROAM_SCAN_HI_RSSI_DELTA_NAME           "gRoamScanHiRssiDelta"
@@ -2780,14 +2780,14 @@ enum dot11p_mode {
 /* Delay between consecutive scans in milliseconds */
 #define CFG_ROAM_SCAN_HI_RSSI_DELAY_NAME            "gRoamScanHiRssiDelay"
 #define CFG_ROAM_SCAN_HI_RSSI_DELAY_MIN             (5000)
-#define CFG_ROAM_SCAN_HI_RSSI_DELAY_MAX             (15000)
-#define CFG_ROAM_SCAN_HI_RSSI_DELAY_DEFAULT         (5000)
+#define CFG_ROAM_SCAN_HI_RSSI_DELAY_MAX             (0x7fffffff)
+#define CFG_ROAM_SCAN_HI_RSSI_DELAY_DEFAULT         (15000)
 
 /* Upper bound after which scan will not be performed */
 #define CFG_ROAM_SCAN_HI_RSSI_UB_NAME              "gRoamScanHiRssiUpperBound"
-#define CFG_ROAM_SCAN_HI_RSSI_UB_MIN               (-76)
-#define CFG_ROAM_SCAN_HI_RSSI_UB_MAX               (-30)
-#define CFG_ROAM_SCAN_HI_RSSI_UB_DEFAULT           (-45)
+#define CFG_ROAM_SCAN_HI_RSSI_UB_MIN               (-66)
+#define CFG_ROAM_SCAN_HI_RSSI_UB_MAX               (0)
+#define CFG_ROAM_SCAN_HI_RSSI_UB_DEFAULT           (-30)
 
 /* Option to report rssi in cfg80211_inform_bss_frame()
  * 0 = use rssi value based on noise floor = -96 dBm
