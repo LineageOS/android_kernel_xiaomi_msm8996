cc646cab58f0584ea7af60c80a5698cd8dd96a4e
Kanchanapally, Vidyullatha <vkanchan@qti.qualcomm.com>
2015-04-15 15:06:06 +0530

    qcacld: Abort scan on P2P-GO when doing a stop ap
    This fix aborts scan on P2P-GO interface before doing a stop ap.
    Without this it is possible that the pending scan will not allow
    stop ap to complete within timeout.
    Also, this fix modifies the value of abort scan timeout in HDD to
    the value of timeout in WMA plus one second in order to ensure
    that HDD timeouts out only after a timeout in WMA and a proper
    cleanup is done.
    
    Change-Id: Ia4cb21e399ac2a6f2be2280ee5a5c511c739f4ac
    CRs-Fixed: 821856

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -9582,10 +9582,13 @@ static int wlan_hdd_cfg80211_stop_ap (struct wiphy *wiphy,
         staAdapter = pAdapterNode->pAdapter;
 
         if (WLAN_HDD_INFRA_STATION == staAdapter->device_mode ||
-           (WLAN_HDD_P2P_CLIENT == staAdapter->device_mode)) {
+           (WLAN_HDD_P2P_CLIENT == staAdapter->device_mode) ||
+           (WLAN_HDD_P2P_GO == staAdapter->device_mode)) {
             pScanInfo = &staAdapter->scan_info;
 
             if (pScanInfo && pScanInfo->mScanPending) {
+               hddLog(LOG1, FL("Aborting pending scan for device mode:%d"),
+                      staAdapter->device_mode);
                INIT_COMPLETION(pScanInfo->abortscan_event_var);
                hdd_abort_mac_scan(staAdapter->pHddCtx, staAdapter->sessionId,
                                   eCSR_SCAN_ABORT_DEFAULT);
