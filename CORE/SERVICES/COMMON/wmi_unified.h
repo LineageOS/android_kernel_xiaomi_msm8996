9cc17466b32149ca5f64d4aa02ab95ce252a4731
Rajeev Kumar <rajekuma@qca.qualcomm.com>
2015-04-15 17:54:07 -0700

    qcacld: CL 1279600 - update fw common interface files
    add GPIO num field in WMI set LED flashing cmd
    
    Change-Id: I2b0cf8b34777f7d16559991ced443e5da3ed348e
    CRs-Fixed: 819213

--- CORE/SERVICES/COMMON/wmi_unified.h
+++ CORE/SERVICES/COMMON/wmi_unified.h
@@ -9541,6 +9541,7 @@ typedef struct{
     A_UINT32    pattern_id; /* pattern identifier */
     A_UINT32    led_x0; /* led flashing parameter0 */
     A_UINT32    led_x1; /* led flashing parameter1 */
+    A_UINT32    gpio_num; /* GPIO number */
 } wmi_set_led_flashing_cmd_fixed_param;
 
 /**
