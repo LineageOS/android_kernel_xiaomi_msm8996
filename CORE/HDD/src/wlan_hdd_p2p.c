f6f7c04a83b4ec27f6a63d59a3f74a685f1b30e0
Tushnim Bhattacharyya <tushnimb@qca.qualcomm.com>
2015-05-08 15:29:22 -0700

    qcacld 2.0: Initialize the pRemainChanCtx to 0
    propagation from prima/pronto
    After allocating memory initilize it to 0.
    
    Change-Id: I8309e0395934c0f245359c577b1f07fe498f63d4
    CRs-Fixed: 807511

--- CORE/HDD/src/wlan_hdd_p2p.c
+++ CORE/HDD/src/wlan_hdd_p2p.c
@@ -830,6 +830,7 @@ static int wlan_hdd_request_remain_on_channel( struct wiphy *wiphy,
         return -ENOMEM;
     }
 
+    vos_mem_zero(pRemainChanCtx, sizeof(*pRemainChanCtx));
     vos_mem_copy(&pRemainChanCtx->chan, chan,
                    sizeof(struct ieee80211_channel));
 
