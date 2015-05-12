1e1a0962fc2ed2cee0c116459d7fc584ca3053d3
Rajeev Kumar <rajekuma@qca.qualcomm.com>
2015-05-12 12:54:02 -0700

    qcacld: Add sanity check for keep alive period
    Add sanity check for keep alive period in HDD to avoid
    target assert
    
    Change-Id: I4d369bb45482fb7faa311a1b593ab71ae657a874
    CRs-Fixed: 836818

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -8387,6 +8387,13 @@ static int __iw_set_keepalive_params(struct net_device *dev,
         return 0;
     }
 
+    if (pRequest->timePeriod > WNI_CFG_INFRA_STA_KEEP_ALIVE_PERIOD_STAMAX) {
+        hddLog(LOGE, FL("Value of timePeriod exceed Max limit %d"),
+               pRequest->timePeriod);
+        return -EINVAL;
+    }
+
+
     /* Debug display of request components. */
     hddLog(VOS_TRACE_LEVEL_INFO,
            "%s: Set Keep Alive Request : TimePeriod %d size %zu",
