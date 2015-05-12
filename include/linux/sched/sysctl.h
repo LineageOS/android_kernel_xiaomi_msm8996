3dd3ad7836ec94f346e86ed8396fef2e29a3d1d1
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2015-05-12 15:01:15 +0530

    sched: Aggregate for frequency
    Related threads in a group could execute on different CPUs and hence
    present a split-demand picture to cpufreq governor. IOW the governor
    fails to see the net cpu demand of all related threads in a given
    window if the threads's execution were to be split across CPUs. That
    could result in sub-optimal frequency chosen in comparison to the
    ideal frequency at which the aggregate work (taken up by related
    threads) needs to be run.
    
    This patch aggregates cpu execution stats in a window for all related
    threads in a group. This helps present cpu busy time to governor as if
    all related threads were part of the same thread and thus help select
    the right frequency required by related threads. This aggregation
    is done per-cluster.
    
    Change-Id: I71e6047620066323721c6d542034ddd4b2950e7f
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -80,6 +80,7 @@ extern unsigned int sysctl_sched_restrict_cluster_spill;
 #if defined(CONFIG_SCHED_FREQ_INPUT)
 extern unsigned int sysctl_sched_new_task_windows;
 extern unsigned int sysctl_sched_pred_alert_freq;
+extern unsigned int sysctl_sched_freq_aggregate;
 #endif
 #endif
 
