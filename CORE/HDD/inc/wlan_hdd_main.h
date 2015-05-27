5962cdcb562cec56e65643f41e3b5d6314a134c1
Amar Singhal <asinghal@qca.qualcomm.com>
2015-05-27 15:25:25 -0700

    qcacld-2.0: Fix timing issue in regulatory initialization
    Regulatory initializaton would not happen properly; if the
    regulatory callback is NOT serialized with call to
    csrInitGetChannels in csr_init_chan_list. Use linux waiting
    mechanism to serialize the two.
    
    Change-Id: If2484c80c70645b1e4154665a9eb4c828f3bbe81
    CRs-Fixed: 845672

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -1256,6 +1256,8 @@ struct hdd_context_s
    /* Completion  variable to indicate Mc Thread Suspended */
    struct completion mc_sus_event_var;
 
+   struct completion reg_init;
+
    v_BOOL_t isWlanSuspended;
 
    v_BOOL_t isTxThreadSuspended;
