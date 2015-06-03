9e8dcaa00882296c6e81e475e039bfcae148ca13
Rajeev Kumar <rajekuma@qca.qualcomm.com>
2015-06-03 13:14:20 -0700

    qcacld: Enable user space logging for all host modules
    - Increase kernel log buffer size to accomodate more logs
      to debug complex issues
    - Enable all log levels for all modules during driver load
    
    Change-Id: I0894242286c972fb0c7b15bc8d043ae590d4a62a
    CRs-Fixed: 847962

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2499,8 +2499,8 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 /* Number of buffers to be used for WLAN logging */
 #define CFG_WLAN_LOGGING_NUM_BUF_NAME               "wlanLoggingNumBuf"
 #define CFG_WLAN_LOGGING_NUM_BUF_MIN                ( 4 )
-#define CFG_WLAN_LOGGING_NUM_BUF_MAX                ( 64 )
-#define CFG_WLAN_LOGGING_NUM_BUF_DEFAULT            ( 32 )
+#define CFG_WLAN_LOGGING_NUM_BUF_MAX                ( 512 )
+#define CFG_WLAN_LOGGING_NUM_BUF_DEFAULT            ( 256 )
 #endif /* WLAN_LOGGING_SOCK_SVC_ENABLE */
 
 #define CFG_ENABLE_SIFS_BURST                      "gEnableSifsBurst"
