c5a249e82df0729eae0065f67a6f5ce13107956b
Komal Seelam <kseelam@qti.qualcomm.com>
2015-06-05 11:56:36 +0530

    qcacld: Skip Debug Log collection if fwlog is disabled in ini
    On Target Assert, Skip Debug Log collection if FwLog is disabled
    in ini.
    
    Change-Id: Iaf66e251635f74c4d7bac19423bade668765eb64
    CRs-Fixed: 849094

--- CORE/SERVICES/BMI/ol_fw.c
+++ CORE/SERVICES/BMI/ol_fw.c
@@ -1247,6 +1247,11 @@ void ol_target_failure(void *instance, A_STATUS status)
 		printk("[%02d]   :  0x%08X\n", i, reg_dump_values[i]);
 	}
 
+	if (scn->enablefwlog) {
+		printk("%s: FWLog is disabled in ini\n", __func__);
+		goto disable_fwlog;
+	}
+
 	if (HIFDiagReadMem(scn->hif_hdl,
 	            host_interest_item_address(scn->target_type, offsetof(struct host_interest_s, hi_dbglog_hdr)),
 	            (A_UCHAR *)&dbglog_hdr_address,
@@ -1294,6 +1299,8 @@ void ol_target_failure(void *instance, A_STATUS status)
 
 	    adf_os_mem_free(dbglog_data);
 	}
+
+disable_fwlog:
 #endif
 
 #if  defined(CONFIG_CNSS) || defined(HIF_SDIO)
