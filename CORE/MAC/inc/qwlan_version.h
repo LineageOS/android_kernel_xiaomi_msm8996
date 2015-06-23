950ea36c103ce553d2f4ab2fe0927678591cf8ae
Akash Patel <c_akashp@qca.qualcomm.com>
2015-06-23 15:05:19 -0700

    Release 4.0.10.124A
    Release 4.0.10.124A
    
    Change-Id: Ifb00f54be907b8137c5f0c062cd4827e018118a6
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -46,7 +46,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_EXTRA            ""
 #define QWLAN_VERSION_BUILD            124
 
-#define QWLAN_VERSIONSTR               "4.0.10.124"
+#define QWLAN_VERSIONSTR               "4.0.10.124A"
 
 
 #define AR6320_REV1_VERSION             0x5000000
