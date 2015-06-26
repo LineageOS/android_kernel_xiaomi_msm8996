7fd3f349c510ddc7f357ad59adadab5ea9ce7e03
Deepak Dhamdhere <ddhamdhe@qca.qualcomm.com>
2015-06-25 23:45:21 -0700

    qcacld-2.0: VO pkt is Txed before TSPEC handshake is completed
    prima to qcacld-2.0 propagation
    
    After roaming downgrading is not happening because while tl is fetching
    pkt from hdd_tx_fetch_packet_cbk, flag wmmAcTspecValid is TRUE because of
    (AddTs done for initial association). This needs to reset during roaming.
    So now hdd is making wmmAcTspecValid FALSE at the time of the connection
    complete a.k.a hdd_wmm_connect during roaming. Though hdd is not involved
    for Add TSpec during roaming still hdd_wmm_sme_callback needs to called
    to set this  flag.
    
    Change-Id: I235f6b61a9b682244da59845b50535a3d62c5a47
    CRs-Fixed: 697180

--- CORE/HDD/src/wlan_hdd_wmm.c
+++ CORE/HDD/src/wlan_hdd_wmm.c
@@ -63,6 +63,7 @@
 #include <wlan_hdd_hostapd.h>
 #include <wlan_hdd_softap_tx_rx.h>
 #include <vos_sched.h>
+#include "sme_Api.h"
 
 // change logging behavior based upon debug flag
 #ifdef HDD_WMM_DEBUG
@@ -2302,6 +2303,26 @@ VOS_STATUS hdd_wmm_connect( hdd_adapter_t* pAdapter,
                (pAdapter->hddWmmStatus.wmmAcStatus[ac].wmmAcTspecInfo.ts_info.direction !=
                 SME_QOS_WMM_TS_DIR_DOWNLINK)) {
             pAdapter->hddWmmStatus.wmmAcStatus[ac].wmmAcAccessAllowed = VOS_TRUE;
+
+            /*
+             * Making TSPEC invalid here so downgrading can happen while
+             * roaming. It is expected this will be SET in hdd_wmm_sme_callback,
+             * once sme is done with the AddTspec.
+             * Here we avoid 11r and ccx based association because Tspec will
+             * be part of assoc/reassoc request.
+             */
+#if defined (WLAN_FEATURE_VOWIFI_11R)
+            if (pRoamInfo->u.pConnectedProfile->AuthType != eCSR_AUTH_TYPE_FT_RSN
+                && pRoamInfo->u.pConnectedProfile->AuthType != eCSR_AUTH_TYPE_FT_RSN_PSK
+#if defined (FEATURE_WLAN_ESE)
+                && pRoamInfo->u.pConnectedProfile->AuthType != eCSR_AUTH_TYPE_CCKM_WPA
+                && pRoamInfo->u.pConnectedProfile->AuthType != eCSR_AUTH_TYPE_CCKM_RSN
+#endif
+               )
+#endif
+            {
+                pAdapter->hddWmmStatus.wmmAcStatus[ac].wmmAcTspecValid = VOS_FALSE;
+            }
          }
       }
       else
