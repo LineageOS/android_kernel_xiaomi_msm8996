bcacb1f630ac34a7c312f521f7b94b4e1e32764e
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2015-07-03 14:23:43 +0530

    Release 4.0.10.131
    Release 4.0.10.131
    
    Change-Id: I45a153fd56b08f501f5c3555f0874afdcc0bc95e
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,9 +44,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            10
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            130
+#define QWLAN_VERSION_BUILD            131
 
-#define QWLAN_VERSIONSTR               "4.0.10.130"
+#define QWLAN_VERSIONSTR               "4.0.10.131"
 
 
 #define AR6320_REV1_VERSION             0x5000000
