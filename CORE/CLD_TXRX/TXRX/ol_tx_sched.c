6597ebbea45449c7d50b627a3ce3c0a98b0514ed
Nirav Shah <nnshah@qti.qualcomm.com>
2015-07-06 14:17:58 +0530

    qcacld: Change credit threashold of MCAST_DATA to 17.
    Change credit threashold of MCAST_DATA of tx scheduler
    to 17 to make it same as other access catagory.
    
    CRs-Fixed: 866592
    Change-Id: I5464c75949d73b34bf2acb43552916de21d34143

--- CORE/CLD_TXRX/TXRX/ol_tx_sched.c
+++ CORE/CLD_TXRX/TXRX/ol_tx_sched.c
@@ -493,7 +493,7 @@ OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(BE,          10,     17,    16,     1,  8);
 OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(BK,          12,      6,     6,     1,  8);
 OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(NON_QOS_DATA,12,      6,     4,     1,  8);
 OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(UCAST_MGMT,   1,      1,     4,     0,  1);
-OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(MCAST_DATA,  10,     16,     4,     1,  4);
+OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(MCAST_DATA,  10,     17,     4,     1,  4);
 OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(MCAST_MGMT,   1,      1,     4,     0,  1);
 #else
 OL_TX_SCHED_WRR_ADV_CAT_CFG_SPEC(VO,           1,     16,    24,     0,  1);
