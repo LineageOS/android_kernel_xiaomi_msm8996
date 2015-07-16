84aa65d91c90e1a947e8b0aac2d6fabbfc46ccac
Paul Zhang <paulz@qti.qualcomm.com>
2015-07-16 15:51:09 +0800

    qcacld: Fix issue about signal jump
    The rssi_raw should not updated when the channel
    in probe response does not match the channel in BD.
    
    Change-Id: I517947c93bec32d9913b0ee85a1bc41e4d22d7d1
    CRs-Fixed: 873549

--- CORE/MAC/src/pe/lim/limScanResultUtils.c
+++ CORE/MAC/src/pe/lim/limScanResultUtils.c
@@ -737,6 +737,7 @@ limLookupNaddHashEntry(tpAniSirGlobal pMac,
     int idx, len;
     tANI_U8 *pbIe;
     tANI_S8  rssi = 0;
+    tANI_S8  rssi_raw = 0;
 
     index = limScanHashFunction(pBssDescr->bssDescription.bssId);
     ptemp = pMac->lim.gLimCachedScanHashTable[index];
@@ -785,6 +786,7 @@ limLookupNaddHashEntry(tpAniSirGlobal pMac,
                 if(dontUpdateAll)
                 {
                    rssi = ptemp->bssDescription.rssi;
+                   rssi_raw = ptemp->bssDescription.rssi_raw;
                 }
 
                 if(pBssDescr->bssDescription.fProbeRsp != ptemp->bssDescription.fProbeRsp)
@@ -841,9 +843,10 @@ limLookupNaddHashEntry(tpAniSirGlobal pMac,
     }
 
     //for now, only rssi, we can add more if needed
-    if ((action == LIM_HASH_UPDATE) && dontUpdateAll && rssi)
+    if ((action == LIM_HASH_UPDATE) && dontUpdateAll && rssi && rssi_raw)
     {
         pBssDescr->bssDescription.rssi = rssi;
+        pBssDescr->bssDescription.rssi_raw = rssi_raw;
     }
 
     // Add this BSS description at same index
@@ -972,6 +975,7 @@ limLookupNaddLfrHashEntry(tpAniSirGlobal pMac,
     int idx, len;
     tANI_U8 *pbIe;
     tANI_S8  rssi = 0;
+    tANI_S8  rssi_raw = 0;
 
     index = limScanHashFunction(pBssDescr->bssDescription.bssId);
     ptemp = pMac->lim.gLimCachedLfrScanHashTable[index];
@@ -1007,6 +1011,7 @@ limLookupNaddLfrHashEntry(tpAniSirGlobal pMac,
                 if(dontUpdateAll)
                 {
                    rssi = ptemp->bssDescription.rssi;
+                   rssi_raw = ptemp->bssDescription.rssi_raw;
                 }
 
                 if(pBssDescr->bssDescription.fProbeRsp != ptemp->bssDescription.fProbeRsp)
@@ -1063,9 +1068,10 @@ limLookupNaddLfrHashEntry(tpAniSirGlobal pMac,
     }
 
     //for now, only rssi, we can add more if needed
-    if ((action == LIM_HASH_UPDATE) && dontUpdateAll && rssi)
+    if ((action == LIM_HASH_UPDATE) && dontUpdateAll && rssi && rssi_raw)
     {
         pBssDescr->bssDescription.rssi = rssi;
+        pBssDescr->bssDescription.rssi_raw = rssi_raw;
     }
 
     // Add this BSS description at same index
