68a2be67e9f788d40773988ee6b2eba601891d93
Sultanxda <sultanxda@gmail.com>
2015-08-13 18:38:54 -0700

    msm: thermal: simple: Set minfreq to maxfreq if minfreq > maxfreq
    *Don't destroy CPU boosts during thermal throttle

--- drivers/thermal/msm_thermal_simple.c
+++ drivers/thermal/msm_thermal_simple.c
@@ -171,7 +171,7 @@ static int cpu_do_throttle(struct notifier_block *nb, unsigned long val, void *d
 	}
 
 	if (policy->min > policy->max)
-		policy->min = policy->cpuinfo.min_freq;
+		policy->min = policy->max;
 
 	return NOTIFY_OK;
 }
