ce6d35f9fa2ce0ecb969f7e32f547d1c09fc8c1c
Srinivas Dasari <dasaris@qti.qualcomm.com>
2015-08-18 16:10:38 -0700

    qcacld-2.0: Add vendor command support for setband
    This is a prima to qcacld-2.0 propagation.
    
    This commit allows the upper layers to send setband request to
    driver using vendor command.
    
    Change-Id: I3ce769e5426a5afb85979b4e19dbabd234f18e98
    CRs-Fixed: 879527

--- CORE/HDD/inc/wlan_hdd_cfg80211.h
+++ CORE/HDD/inc/wlan_hdd_cfg80211.h
@@ -247,6 +247,7 @@ enum qca_nl80211_vendor_subcmds {
 
 	/* subcommand to get link properties */
 	QCA_NL80211_VENDOR_SUBCMD_LINK_PROPERTIES = 101,
+	QCA_NL80211_VENDOR_SUBCMD_SETBAND = 105,
 };
 
 enum qca_nl80211_vendor_subcmds_index {
@@ -402,6 +403,10 @@ enum qca_wlan_vendor_attr {
 
     /* used by QCA_NL80211_VENDOR_SUBCMD_GET_FEATURES */
     QCA_WLAN_VENDOR_ATTR_FEATURE_FLAGS = 7,
+
+    /* Unsigned 32-bit value from enum qca_set_band */
+    QCA_WLAN_VENDOR_ATTR_SETBAND_VALUE = 12,
+
     /* keep last */
     QCA_WLAN_VENDOR_ATTR_AFTER_LAST,
     QCA_WLAN_VENDOR_ATTR_MAX =
