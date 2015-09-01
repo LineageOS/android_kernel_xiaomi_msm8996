36d81e78b3784a5239ea38af0541b0948ec5ff10
Jeff Johnson <jjohnson@qca.qualcomm.com>
2015-09-01 09:46:02 -0700

    qcacld-2.0: Remove obsolete SPY support
    The WLAN driver advertises support for the SPY wireless extension
    methods, but it doesn't actually implement the SPY monitoring logic.
    Hence it is pointless to advertise SPY support, so remove the method
    registrations.
    
    Change-Id: I4f60470812a9b14615d944233d03181721a0dd61
    CRs-Fixed: 893222

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -10987,17 +10987,10 @@ static const iw_handler      we_handler[] =
    (iw_handler) NULL,             /* SIOCGIWPRIV */
    (iw_handler) NULL,             /* SIOCSIWSTATS */
    (iw_handler) NULL,             /* SIOCGIWSTATS */
-#ifdef CONFIG_WEXT_SPY
-   iw_handler_set_spy,             /* SIOCSIWSPY */
-   iw_handler_get_spy,             /* SIOCGIWSPY */
-   iw_handler_set_thrspy,         /* SIOCSIWTHRSPY */
-   iw_handler_get_thrspy,         /* SIOCGIWTHRSPY */
-#else
    (iw_handler) NULL,             /* SIOCSIWSPY */
    (iw_handler) NULL,             /* SIOCGIWSPY */
    (iw_handler) NULL,             /* SIOCSIWTHRSPY */
    (iw_handler) NULL,             /* SIOCGIWTHRSPY */
-#endif
    (iw_handler) iw_set_ap_address,   /* SIOCSIWAP */
    (iw_handler) iw_get_ap_address,   /* SIOCGIWAP */
    (iw_handler) iw_set_mlme,              /* SIOCSIWMLME */
