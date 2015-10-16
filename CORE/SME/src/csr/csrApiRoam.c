0444eefe3da3d5a618d383d4da49107a1ba85754
Gupta, Kapil <kapgupta@qti.qualcomm.com>
2015-10-16 17:50:48 +0530

    qcacld-2.0: Add support for 2.4G VHT Interop in SAP
    In 2.4GHz some stations advertise VHT capability IE in Assoc Req frame.
    Add support to detect the VHT capability in vendor specific IE in
    Assoc Req frame and establish the connection with VHT mode to
    improve the performance.
    
    Change-Id: I08dbcb3ce1895aa2108924d7a672e5d9be514e3d
    CRs-Fixed: 924814

--- CORE/SME/src/csr/csrApiRoam.c
+++ CORE/SME/src/csr/csrApiRoam.c
@@ -2024,6 +2024,8 @@ eHalStatus csrChangeDefaultConfigParam(tpAniSirGlobal pMac, tCsrConfigParam *pPa
         pMac->roam.configParam.ignore_peer_ht_opmode =
                                     pParam->ignore_peer_ht_opmode;
         pMac->roam.configParam.obssEnabled = pParam->obssEnabled;
+        pMac->roam.configParam.vendor_vht_for_24ghz_sap =
+                               pParam->vendor_vht_for_24ghz_sap;
         pMac->roam.configParam.conc_custom_rule1 =
                                pParam->conc_custom_rule1;
         pMac->roam.configParam.conc_custom_rule2 =
@@ -2218,6 +2220,9 @@ eHalStatus csrGetConfigParam(tpAniSirGlobal pMac, tCsrConfigParam *pParam)
         csrSetChannels(pMac, pParam);
 
         pParam->obssEnabled = pMac->roam.configParam.obssEnabled;
+
+        pParam->vendor_vht_for_24ghz_sap =
+           pMac->roam.configParam.vendor_vht_for_24ghz_sap;
 
         pParam->conc_custom_rule1 =
                      pMac->roam.configParam.conc_custom_rule1;
@@ -15295,6 +15300,7 @@ eHalStatus csrSendMBStartBssReqMsg( tpAniSirGlobal pMac, tANI_U32 sessionId, eCs
 
         *pBuf++ = (tANI_U8)pMac->roam.configParam.obssEnabled;
         *pBuf++ = (tANI_U8)pParam->sap_dot11mc;
+        *pBuf++ = (tANI_U8)pMac->roam.configParam.vendor_vht_for_24ghz_sap;
 
         msgLen = (tANI_U16)(sizeof(tANI_U32 ) + (pBuf - wTmpBuf)); //msg_header + msg
         pMsg->length = pal_cpu_to_be16(msgLen);
