e20104421f5b50d9852c1874fbbda5eed51c8c8a
Edhar, Mahesh Kumar <c_medhar@qti.qualcomm.com>
2015-10-20 12:46:58 +0530

    qcacld-2.0: Properly update specturum channel list
    Currently for ACS,one to one correspondence between channel
    number and index of Sap specturum structure is desired.
    But the correspondence is broken in case of channel in NOL list
    or if the DFS channels are not allowed.
    
    In case of channel in NOL list or if DFS channel not allowed,
    channel number is incremented but index of structure doesn't change.
    
    As a part of fix, made sure that one to one correspondence is
    maintained.
    
    Change-Id: I05cbe92e2657d96e271b402b9fdbe41a32c7080d
    CRs-Fixed: 922865

--- CORE/SAP/src/sapChSelect.c
+++ CORE/SAP/src/sapChSelect.c
@@ -645,7 +645,7 @@ v_BOOL_t sapChanSelInit(tHalHandle halHandle,
     // Fill the channel number in the spectrum in the operating freq band
     for (channelnum = 0;
             channelnum < pSpectInfoParams->numSpectChans;
-                channelnum++, pChans++) {
+                channelnum++, pChans++, pSpectCh++) {
         chSafe = VOS_TRUE;
 
         /* check if the channel is in NOL blacklist */
@@ -686,7 +686,6 @@ v_BOOL_t sapChanSelInit(tHalHandle halHandle,
         if(*pChans == 14 &&
                eCSR_DOT11_MODE_11b != pSapCtx->csrRoamProfile.phyMode)
         {
-            pSpectCh++;
             continue;
         }
 
@@ -697,7 +696,6 @@ v_BOOL_t sapChanSelInit(tHalHandle halHandle,
             pSpectCh->rssiAgr = SOFTAP_MIN_RSSI;// Initialise for all channels
             pSpectCh->channelWidth = SOFTAP_HT20_CHANNELWIDTH; // Initialise 20MHz for all the Channels
         }
-        pSpectCh++;
     }
     return eSAP_TRUE;
 }
