d220a6ed53429e51f1166da3cb656cb85b6309ec
Masti, Narayanraddi <c_nmasti@qti.qualcomm.com>
2015-10-30 18:24:56 +0530

    qcacld-2.0: set channel width for TDLS link from ongoing session
    Channel width of TDLS link on base channel should not exceed
    channel width  of STA-AP link.
    pStaDs->vhtSupportedChannelWidthSet represent the channel width
    of TDLS link on base channel and psessionEntry->vhtTxChannelWidthSet
    represents channel width set for STA-AP link.
    Hence update pStaDs->vhtSupportedChannelWidthSet from
    psessionEntry->vhtTxChannelWidthSet.
    
    Change-Id: Ia0304ea4318a76259925c574946c299043398ebc
    CRs-Fixed: 932916

--- CORE/MAC/src/pe/lim/limProcessTdls.c
+++ CORE/MAC/src/pe/lim/limProcessTdls.c
@@ -2348,32 +2348,15 @@ static void limTdlsUpdateHashNodeInfo(tpAniSirGlobal pMac, tDphHashNode *pStaDs,
     pVhtCaps = &vhtCap;
     if (pVhtCaps->present)
     {
-       pStaDs->mlmStaContext.vhtCapability = 1 ;
+        pStaDs->mlmStaContext.vhtCapability = 1 ;
 
-       if (psessionEntry->currentOperChannel <= SIR_11B_CHANNEL_END)
-        {
-            /*
-             * if the channel is 2G then update the min channel widthset in
-             * pStaDs. These values are used when sending a AddSta request to
-             * firmware
-             * 11.21.1 General: The channel width of the TDLS direct link on the
-             * base channel shall not exceed the channel width of the BSS to
-             * which the TDLS peer STAs are associated.
-             */
-            pStaDs->vhtSupportedChannelWidthSet = WNI_CFG_VHT_CHANNEL_WIDTH_20_40MHZ;
-            pStaDs->htSupportedChannelWidthSet = eHT_CHANNEL_WIDTH_20MHZ;
-            limLog(pMac, LOG1,
-                   FL("vhtSupportedChannelWidthSet = %hu,"
-                      " htSupportedChannelWidthSet %hu"),
-                   pStaDs->htSupportedChannelWidthSet,
-                   pStaDs->htSupportedChannelWidthSet);
-        }
-        else
-        {
-            pStaDs->vhtSupportedChannelWidthSet =  WNI_CFG_VHT_CHANNEL_WIDTH_80MHZ;
-            pStaDs->htSupportedChannelWidthSet = eHT_CHANNEL_WIDTH_40MHZ ;
-        }
-
+        /*
+         * channel width of TDLS link on base channel should not exceed
+         * channel width of STA-AP link. Hence take this setting from the
+         * psessionEntry.
+         */
+        pStaDs->vhtSupportedChannelWidthSet =
+                psessionEntry->vhtTxChannelWidthSet;
         pStaDs->vhtLdpcCapable = pVhtCaps->ldpcCodingCap;
         pStaDs->vhtBeamFormerCapable = 0;
         pMac->lim.gLimTdlsLinkMode = TDLS_LINK_MODE_AC;
