ddcd230bd0df317e81f67c984cdc8e7484afbb6e
Yue Ma <yuem@qca.qualcomm.com>
2015-11-04 12:18:47 -0800

    qcacld: Reduce PM QOS latency tolerance time to minimum
    In order to improve driver load time, reduce PM QOS latency tolerance
    time to minimum to avoid CPU enters aggressive PS during loading driver.
    
    Change-Id: Ibacf933d379f37063d900d444de20493b94ebd90
    CRs-fixed: 935267

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -162,7 +162,7 @@ void hdd_ch_avoid_cb(void *hdd_context,void *indi_param);
 #define MEMORY_DEBUG_STR ""
 #endif
 
-#define DISABLE_KRAIT_IDLE_PS_VAL   200
+#define DISABLE_KRAIT_IDLE_PS_VAL   1
 #ifdef IPA_UC_OFFLOAD
 /* If IPA UC data path is enabled, target should reserve extra tx descriptors
  * for IPA WDI data path.
