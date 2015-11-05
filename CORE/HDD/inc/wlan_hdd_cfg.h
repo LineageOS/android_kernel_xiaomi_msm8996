f8d89925aa7d351e5154e95fd228e2ae1769e6d2
Manikandan Mohan <manikand@qca.qualcomm.com>
2015-11-05 14:04:56 -0800

    qcacld: Configure only valid TSF gpio pin
    Update qcacld to configure only valid GPIO setting if enabled in
    INI file. Default TSF Gpio value is set as invalid, So it will not
    be set in fw during driver load.
    
    Change-Id: I0414becbf83718155a9ca51d60f34d0e8ffcd4e3
    CRs-fixed: 935967

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -3181,8 +3181,9 @@ enum dot11p_mode {
 /* GPIO pin to toogle when capture tsf */
 #define CFG_SET_TSF_GPIO_PIN_NAME                  "gtsf_gpio_pin"
 #define CFG_SET_TSF_GPIO_PIN_MIN                   (0)
-#define CFG_SET_TSF_GPIO_PIN_MAX                   (255)
-#define CFG_SET_TSF_GPIO_PIN_DEFAULT               (34)
+#define CFG_SET_TSF_GPIO_PIN_MAX                   (254)
+#define TSF_GPIO_PIN_INVALID                       (255)
+#define CFG_SET_TSF_GPIO_PIN_DEFAULT               (TSF_GPIO_PIN_INVALID)
 
 #define CFG_MULTICAST_HOST_FW_MSGS          "gMulticastHostFwMsgs"
 #define CFG_MULTICAST_HOST_FW_MSGS_MIN      (0)
--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -12819,11 +12819,11 @@ int hdd_wlan_startup(struct device *dev, v_VOID_t *hif_sc)
                "%s: Error setting txlimit in sme", __func__);
    }
 
-   hal_status = sme_set_tsf_gpio(pHddCtx->hHal, pHddCtx->cfg_ini->tsf_gpio_pin);
-
-   if (eHAL_STATUS_SUCCESS != hal_status) {
-        hddLog(VOS_TRACE_LEVEL_ERROR,
-            FL("set tsf GPIO fail"));
+   if (pHddCtx->cfg_ini->tsf_gpio_pin != TSF_GPIO_PIN_INVALID) {
+       hal_status = sme_set_tsf_gpio(pHddCtx->hHal,
+                                     pHddCtx->cfg_ini->tsf_gpio_pin);
+       if (eHAL_STATUS_SUCCESS != hal_status)
+           hddLog(VOS_TRACE_LEVEL_ERROR, FL("set tsf GPIO failed"));
    }
 
 #ifdef MSM_PLATFORM
