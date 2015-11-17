f89b524d68b8e3a64ec259eb4f94667871899e1a
Kabilan Kannan <kabilank@qca.qualcomm.com>
2015-11-16 19:41:20 -0800

    qcacld-2.0: Change the vdev nss bit mask in correct order.
    Change the VDEV Nss value bit mask in correct order
    as per the firmware design.
    
    Change-Id: I4cc0ced042d3a31c021dde73050c0c48ced8fe9e
    CRs-Fixed: 940146

--- CORE/MAC/inc/aniGlobal.h
+++ CORE/MAC/inc/aniGlobal.h
@@ -158,9 +158,9 @@ typedef struct sAniSirGlobal *tpAniSirGlobal;
 #define CFG_SAP_NSS(_x)     ((((_x) >> 2) & 0x3) ? (((_x) >> 2) & 0x3) : 1)
 #define CFG_P2P_GO_NSS(_x)  ((((_x) >> 4) & 0x3) ? (((_x) >> 4) & 0x3) : 1)
 #define CFG_P2P_CLI_NSS(_x) ((((_x) >> 6) & 0x3) ? (((_x) >> 6) & 0x3) : 1)
-#define CFG_P2P_DEV_NSS(_x) ((((_x) >> 8) & 0x3) ? (((_x) >> 8) & 0x3) : 1)
-#define CFG_IBSS_NSS(_x)    ((((_x) >> 10) & 0x3) ? (((_x) >> 10) & 0x3) : 1)
-#define CFG_TDLS_NSS(_x)    ((((_x) >> 12) & 0x3) ? (((_x) >> 12) & 0x3) : 1)
+#define CFG_IBSS_NSS(_x)    ((((_x) >> 8) & 0x3) ? (((_x) >> 8) & 0x3) : 1)
+#define CFG_TDLS_NSS(_x)    ((((_x) >> 10) & 0x3) ? (((_x) >> 10) & 0x3) : 1)
+#define CFG_P2P_DEV_NSS(_x) ((((_x) >> 12) & 0x3) ? (((_x) >> 12) & 0x3) : 1)
 #define CFG_OCB_NSS(_x)     ((((_x) >> 14) & 0x3) ? (((_x) >> 14) & 0x3) : 1)
 
 /**
