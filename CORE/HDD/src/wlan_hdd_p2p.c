082c9206983269b53e1722d3d469aa56966d05d0
Naveen Rawat <nrawat@qca.qualcomm.com>
2015-11-17 18:05:46 -0800

    qcacld-2.0: Do not request ROC if already on correct channel
    If cfg80211_mgmt_tx_params.offchan is set, driver need not send ROC
    if device is already on correct channel. This patch check current home
    channel of associated adapter and if it is same as target channel
    requested by called, then skips ROC requests and directly sends frame
    over the air.
    
    Change-Id: Ib13a885437a4349702e1706b3a466af262813f8a
    CRs-Fixed: 921117

--- CORE/HDD/src/wlan_hdd_p2p.c
+++ CORE/HDD/src/wlan_hdd_p2p.c
@@ -1355,9 +1355,11 @@ int __wlan_hdd_mgmt_tx(struct wiphy *wiphy, struct net_device *dev,
     int status;
     unsigned long rc;
     hdd_adapter_t *goAdapter;
+    uint8_t home_ch = 0;
 
     ENTER();
 
+    hddLog(LOG1, FL("wait: %d, offchan: %d"), wait, offchan);
     MTRACE(vos_trace(VOS_MODULE_ID_HDD,
                       TRACE_CODE_HDD_ACTION, pAdapter->sessionId,
                       pAdapter->device_mode ));
@@ -1373,6 +1375,12 @@ int __wlan_hdd_mgmt_tx(struct wiphy *wiphy, struct net_device *dev,
     hddLog(LOG1, FL("Device_mode %s(%d) type: %d"),
            hdd_device_mode_to_string(pAdapter->device_mode),
            pAdapter->device_mode, type);
+
+    hddLog(LOG1, FL("category: %d, actionID: %d"),
+           buf[WLAN_HDD_PUBLIC_ACTION_FRAME_BODY_OFFSET +
+               WLAN_HDD_PUBLIC_ACTION_FRAME_CATEGORY_OFFSET],
+           buf[WLAN_HDD_PUBLIC_ACTION_FRAME_BODY_OFFSET +
+               WLAN_HDD_PUBLIC_ACTION_FRAME_ACTION_OFFSET]);
 
 #ifdef WLAN_FEATURE_P2P_DEBUG
     if ((type == SIR_MAC_MGMT_FRAME) &&
@@ -1497,16 +1505,21 @@ int __wlan_hdd_mgmt_tx(struct wiphy *wiphy, struct net_device *dev,
          hdd_getActionString(buf[WLAN_HDD_PUBLIC_ACTION_FRAME_OFFSET]));
      }
 
-    goAdapter = hdd_get_adapter( pAdapter->pHddCtx, WLAN_HDD_P2P_GO );
+    if (pAdapter->device_mode == WLAN_HDD_SOFTAP) {
+        home_ch = pAdapter->sessionCtx.ap.operatingChannel;
+    } else if (pAdapter->device_mode == WLAN_HDD_INFRA_STATION) {
+        home_ch = pAdapter->sessionCtx.station.conn_info.operationChannel;
+    } else {
+        goAdapter = hdd_get_adapter( pAdapter->pHddCtx, WLAN_HDD_P2P_GO );
+        if (goAdapter)
+            home_ch = goAdapter->sessionCtx.ap.operatingChannel;
+    }
 
-    //If GO adapter exists and operating on same frequency
-    //then we will not request remain on channel
-    if (goAdapter && (ieee80211_frequency_to_channel(chan->center_freq)
-                         == goAdapter->sessionCtx.ap.operatingChannel)) {
-        /* if GO exist and is not off channel
-         * wait time should be zero
-         */
+    if (ieee80211_frequency_to_channel(chan->center_freq) == home_ch) {
+        /* if adapter is already on requested ch, no need for ROC */
         wait = 0;
+        hddLog(LOGE,
+               FL("Adapter already on requested channel. No ROC requested"));
         goto send_frame;
     }
 
