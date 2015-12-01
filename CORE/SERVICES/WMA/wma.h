2489fdc1f117a97471813590ae7d0f6001584ead
Rajeev Kumar <rajekuma@qca.qualcomm.com>
2015-11-30 16:04:59 -0800

    qcacld-2.0: Remove support of power gating parameters
    - WLAN firmware does not support get/set operations for power
      gating VDEV parameters
    - Remove support for power gating VDEV parameters from WLAN host
      driver
    
    Change-Id: I5b262b71364c11f648b579a8bdb6fa993ecb4a81
    CRs-Fixed: 880180

--- CORE/SERVICES/WMA/wma.h
+++ CORE/SERVICES/WMA/wma.h
@@ -403,7 +403,6 @@ typedef struct {
 	u_int32_t rxchainmask;
 	u_int32_t txpow2g;
 	u_int32_t txpow5g;
-	u_int32_t pwrgating;
 	u_int32_t burst_enable;
 	u_int32_t burst_dur;
 	u_int32_t chainmask_2g;
