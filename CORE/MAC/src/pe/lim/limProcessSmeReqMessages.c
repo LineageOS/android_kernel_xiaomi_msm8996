37ff2e2dcc77a709a16fb04389ac58373ff69984
Archana Ramachandran <archanar@qca.qualcomm.com>
2016-01-25 16:57:02 -0800

    cld-2.0: Fix mode switch to 1x1 not updating HT SMPS in the reassoc request
    Dynamic Antenna mode switch from 2x2 to 1x1 in the non connected state
    is updating the association request HT SMPS to static but the reassociation
    request HT SMPS mode is still disabled. This will lead to station
    reassociating in 2x2 mode although the current antenna mode is 1x1.
    
    Change-Id: Iafbaf7f97ce56171b3fde469550a11ebfa20b0fc
    CRs-Fixed: 966939

--- CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
+++ CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
@@ -2658,6 +2658,12 @@ __limProcessSmeReassocReq(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
     psessionEntry->limPrevSmeState = psessionEntry->limSmeState;
     psessionEntry->limSmeState    = eLIM_SME_WT_REASSOC_STATE;
 
+    psessionEntry->enableHtSmps = pReassocReq->enableHtSmps;
+    psessionEntry->htSmpsvalue = pReassocReq->htSmps;
+    limLog(pMac, LOG1, FL("enableHtSmps: %d htSmps: %d"),
+           psessionEntry->enableHtSmps,
+           psessionEntry->htSmpsvalue);
+
     MTRACE(macTrace(pMac, TRACE_CODE_SME_STATE, psessionEntry->peSessionId, psessionEntry->limSmeState));
 
     limPostMlmMessage(pMac,
