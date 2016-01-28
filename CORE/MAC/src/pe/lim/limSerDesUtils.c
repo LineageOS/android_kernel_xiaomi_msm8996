13f81355f1d062095a63042038b4370b709973e0
Abhishek Singh <absingh@qti.qualcomm.com>
2016-01-28 17:28:19 +0530

    qcacld-2.0: Ignore security mismatch during WPS registartion
    prima to qcacld-2.0 propagation
    
    During WPS registration process the psession encryption type is 0.
    Now if AP's beacon have RSN IE, while processing beacons security
    mismatch is detected and DUT sends unicast probe request without
    WPS IE.
    
    To avoid this ignore security mismatch during WPS registartion.
    
    Change-Id: Ic1b4de116ec6a99749ec3d8ba28b29d52190c344
    CRs-Fixed: 967553

--- CORE/MAC/src/pe/lim/limSerDesUtils.c
+++ CORE/MAC/src/pe/lim/limSerDesUtils.c
@@ -935,6 +935,13 @@ limJoinReqSerDes(tpAniSirGlobal pMac, tpSirSmeJoinReq pJoinReq, tANI_U8 *pBuf)
         limLog(pMac, LOGE, FL("remaining len %d is too short"), len);
         return eSIR_FAILURE;
     }
+    pJoinReq->wps_registartion = *pBuf++;
+    len--;
+    if (limCheckRemainingLength(pMac, len) == eSIR_FAILURE)
+    {
+        limLog(pMac, LOGE, FL("remaining len %d is too short"), len);
+        return eSIR_FAILURE;
+    }
     // Extract cbMode
     pJoinReq->cbMode = (ePhyChanBondState)limGetU32(pBuf);
     pBuf += sizeof(ePhyChanBondState);
