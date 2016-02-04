ca01b976259f26edeecc9bfd9d3af69b56a1eff2
Mukul Sharma <mukul@qti.qualcomm.com>
2016-02-04 18:30:13 +0530

    qcacld-2.0: Wait for download complete only when OTA required
    Currently for low latency target, host is waiting for 1 sec for download
    complete, which is blocking MC thread. Due to this MC thread is not
    serving the SME queue and which triggers the active cmd time out.
    As a part of this fix, MC thread waits for download complete only
    for those frame which needs OTA.
    
    Change-Id: I25f45e969dd5bbf2341d586265d84919755f6ed3
    CRs-Fixed: 967842

--- CORE/SERVICES/WMA/wma.c
+++ CORE/SERVICES/WMA/wma.c
@@ -32524,7 +32524,8 @@ VOS_STATUS WDA_TxPacket(void *wma_context, void *tx_frame, u_int16_t frmLen,
 	is_high_latency = wdi_out_cfg_is_high_latency(
 				txrx_vdev->pdev->ctrl_pdev);
 
-	downld_comp_required = tx_frm_download_comp_cb && is_high_latency;
+	downld_comp_required = tx_frm_download_comp_cb && is_high_latency &&
+				tx_frm_ota_comp_cb;
 
 	/* Fill the frame index to send */
 	if(pFc->type == SIR_MAC_MGMT_FRAME) {
@@ -32680,15 +32681,6 @@ VOS_STATUS WDA_TxPacket(void *wma_context, void *tx_frame, u_int16_t frmLen,
 				WMA_LOGE("%s Tx Complete took %lu ms",__func__,
 				   vos_timer_get_system_time() - time_snapshot);
 		}
-	} else {
-		/*
-		 * For Low Latency Devices
-		 * Call the download complete
-		 * callback once the frame is successfully
-		 * given to txrx module
-		 */
-		tx_frm_download_comp_cb(wma_handle->mac_context, tx_frame,
-					WMA_TX_FRAME_BUFFER_NO_FREE);
 	}
 
 	return VOS_STATUS_SUCCESS;
