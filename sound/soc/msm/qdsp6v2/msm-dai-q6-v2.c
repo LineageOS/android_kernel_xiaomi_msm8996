5da6e2ddde8a7d40e70d0b0013386b43b3a15859
Derek Chen <chenche@codeaurora.org>
2016-02-05 18:33:22 -0500

    ASoC: msm: invert bit clk for afe tdm ports for msm8996
    Invert the bit clock for afe tdm ports for msm8996 to
    align the lpass and ext codec hw clock and avoid
    bit corruption.
    
    Change-Id: Ic36ab733b227a34ebb8401dd11e99f943b0c8839
    Signed-off-by: Derek Chen <chenche@codeaurora.org>
    CRs-fixed: 973480

--- sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -274,7 +274,7 @@ static struct afe_clk_set tdm_clk_set = {
 	AFE_API_VERSION_CLOCK_SET,
 	Q6AFE_LPASS_CLK_ID_QUAD_TDM_EBIT,
 	Q6AFE_LPASS_IBIT_CLK_DISABLE,
-	Q6AFE_LPASS_CLK_ATTRIBUTE_COUPLE_NO,
+	Q6AFE_LPASS_CLK_ATTRIBUTE_INVERT_COUPLE_NO,
 	Q6AFE_LPASS_CLK_ROOT_DEFAULT,
 	0,
 };
