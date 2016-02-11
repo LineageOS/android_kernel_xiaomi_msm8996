91755a4bd042193a400ec3e5ea949319e11fd7cc
mukul sharma <mukul@qti.qualcomm.com>
2016-02-11 18:19:30 +0530

    qcacld: Increase wait retry count in rmmod
    If SSR/ram dump collection is in progress, rmmod waits
    for it's completion for 75 times. But for high latency
    target SSR/ram dump collection is taking more then 75 sec.
    
    As part of this fix,For HL target set retry count to 200.
    
    Change-Id: I8cc052c748e6a6dfff45675f9c15ead00b002a11
    CRs-Fixed: 974023

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -212,7 +212,11 @@
 #define WLAN_HDD_QOS_MAP_CONFIGURE 4
 #define HDD_SAP_WAKE_LOCK_DURATION 10000 //in msecs
 
+#if defined(CONFIG_HL_SUPPORT)
+#define HDD_MOD_EXIT_SSR_MAX_RETRIES 200
+#else
 #define HDD_MOD_EXIT_SSR_MAX_RETRIES 75
+#endif
 
 /* Maximum number of interfaces allowed(STA, P2P Device, P2P Interfaces) */
 #ifndef WLAN_OPEN_P2P_INTERFACE
