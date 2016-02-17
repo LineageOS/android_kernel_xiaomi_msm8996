83463222ca5f24e378babec8fc217bd0c2999067
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2016-02-17 12:02:37 -0800

    msm: mdss: Update interface to support Qseed3.
    MSM cobalt mdss hardware block supports Qseed3 module which provides
    functionality of scaling, sharpening. Change adds support for exposing
    the block to user-space clients.
    
    CRs-Fixed: 982712
    Change-Id: I7a74566a527285aba313321a59bc17dd362e84ff
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>

--- drivers/video/msm/mdss/mdss.h
+++ drivers/video/msm/mdss/mdss.h
@@ -165,6 +165,9 @@ enum mdss_hw_capabilities {
 	MDSS_CAPS_YUV_CONFIG,
 	MDSS_CAPS_SCM_RESTORE_NOT_REQUIRED,
 	MDSS_CAPS_3D_MUX_UNDERRUN_RECOVERY_SUPPORTED,
+	MDSS_CAPS_MIXER_1_FOR_WB,
+	MDSS_CAPS_QSEED3,
+	MDSS_CAPS_DEST_SCALER,
 	MDSS_CAPS_MAX,
 };
 
@@ -194,6 +197,24 @@ struct mdss_smmu_client {
 	bool domain_attached;
 	bool handoff_pending;
 	char __iomem *mmu_base;
+};
+
+struct mdss_mdp_qseed3_lut_tbl {
+	bool valid;
+	u32 *dir_lut;
+	u32 *cir_lut;
+	u32 *sep_lut;
+};
+
+struct mdss_scaler_block {
+	u32 vig_scaler_off;
+	u32 vig_scaler_lut_off;
+	u32 has_dest_scaler;
+	char __iomem *dest_base;
+	u32 ndest_scalers;
+	u32 *dest_scaler_off;
+	u32 *dest_scaler_lut_off;
+	struct mdss_mdp_qseed3_lut_tbl lut_tbl;
 };
 
 struct mdss_data_type;
@@ -455,7 +476,9 @@ struct mdss_data_type {
 	u32 bcolor0;
 	u32 bcolor1;
 	u32 bcolor2;
+	struct mdss_scaler_block *scaler_off;
 };
+
 extern struct mdss_data_type *mdss_res;
 
 struct irq_info {
