2293e5250576ad6dc2a179689aa0e036498b8f30
Edhar, Mahesh Kumar <c_medhar@qti.qualcomm.com>
2016-03-01 17:57:25 +0530

    qcacld-2.0: Change to include VHT TX POWER ENVELOPE IE in SAP mode
    Extract VHT TX POWER ENVELOPE IE from hostapd beacon
    template passed through start/change cfg80211 API and include it
    in driver beacon template.
    
    Change-Id: I8ef00ebbd44e71219d47f5a8cec6496e78cb7c66
    CRs-Fixed: 984779

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -11815,6 +11815,8 @@ int wlan_hdd_cfg80211_update_apies(hdd_adapter_t* pHostapdAdapter)
     wlan_hdd_add_extra_ie(pHostapdAdapter, genie, &total_ielen,
                           WLAN_EID_INTERWORKING);
 
+    wlan_hdd_add_extra_ie(pHostapdAdapter, genie, &total_ielen,
+                          WLAN_EID_VHT_TX_POWER_ENVELOPE);
     if (0 != wlan_hdd_add_ie(pHostapdAdapter, genie,
                               &total_ielen, WPS_OUI_TYPE, WPS_OUI_TYPE_SIZE))
     {
