55f58c18e1f7c920d0870ef491582594889afce8
Yuanyuan Liu <yuanliu@qca.qualcomm.com>
2016-03-03 15:55:44 -0800

    qcacld-2.0: Send WLAN off event to WLS at the end of WLAN off steps
    send WLAN off event to WLS at the end of WLAN off steps
    to make sure WLAN is really off when WLS calls 'glink deinit'.
    
    Change-Id: I29e15d51ec306071fc9f5ff7e3565c75814944e8
    CRs-Fixed: 985226

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -12809,6 +12809,10 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
    if (VOS_FTM_MODE != hdd_get_conparam())
        wlan_hdd_logging_sock_deactivate_svc(pHddCtx);
 
+#ifdef WLAN_FEATURE_LPSS
+   wlan_hdd_send_status_pkg(NULL, NULL, 0, 0);
+#endif
+
 #ifdef WLAN_KD_READY_NOTIFIER
    cnss_diag_notify_wlan_close();
    nl_srv_exit(pHddCtx->ptt_pid);
@@ -12907,10 +12911,6 @@ void __hdd_wlan_exit(void)
 
    vos_set_load_unload_in_progress(VOS_MODULE_ID_VOSS, TRUE);
    vos_set_unload_in_progress(TRUE);
-
-#ifdef WLAN_FEATURE_LPSS
-   wlan_hdd_send_status_pkg(NULL, NULL, 0, 0);
-#endif
 
    //Do all the cleanup before deregistering the driver
    memdump_deinit();
