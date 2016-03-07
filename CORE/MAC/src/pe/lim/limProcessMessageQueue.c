9141bca8871acf737251e31ab220e19221bca72c
Deepak Dhamdhere <ddhamdhe@qca.qualcomm.com>
2016-03-07 00:14:47 -0800

    qcacld-2.0: Process NDP data end indication
    Firmware sends an array of:
        {ndp instance id, vdev id, active ndp num, peer mac address}
    for the NDP instances that are to be terminated.
    Host driver deletes the peers that have 0 active ndp instances and
    sends the entire list of ndp instance id's to the service layer.
    
    Change-Id: I325843ce7fb8198466cb66ce66710cef999d4581
    CRs-Fixed: 962367

--- CORE/MAC/src/pe/lim/limProcessMessageQueue.c
+++ CORE/MAC/src/pe/lim/limProcessMessageQueue.c
@@ -2211,6 +2211,7 @@ limProcessMessages(tpAniSirGlobal pMac, tpSirMsgQ  limMsg)
     case SIR_HAL_NDP_CONFIRM:
     case SIR_HAL_NDP_RESPONDER_RSP:
     case SIR_HAL_NDP_END_RSP:
+    case SIR_HAL_NDP_END_IND:
         lim_handle_ndp_event_message(pMac, limMsg);
         break;
     default:
