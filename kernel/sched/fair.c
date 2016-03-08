aea739a1c8a9ad05cd9c08078fc22fd9f8555d74
Joonwoo Park <joonwoop@codeaurora.org>
2016-03-08 13:46:04 -0800

    sched: add support for CPU frequency estimation with cycle counter
    At present scheduler calculates task's demand with the task's execution
    time weighted over CPU frequency.  The CPU frequency is given by
    governor's CPU frequency transition notification.  Such notification
    may not be available.
    
    Provide an API for CPU clock driver to register callback functions so
    in order for scheduler to access CPU's cycle counter to estimate CPU's
    frequency without notification.  At time point scheduler assumes the
    cycle counter increases always even when cluster is idle which might
    not be true.  This will be fixed by subsequent change for more accurate
    I/O wait time accounting.
    
    CRs-fixed: 1006303
    Change-Id: I93b187efd7bc225db80da0184683694f5ab99738
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4128,6 +4128,7 @@ void init_new_task_load(struct task_struct *p)
 
 	p->init_load_pct = 0;
 	memset(&p->ravg, 0, sizeof(struct ravg));
+	p->cpu_cycles = 0;
 	p->se.avg.decay_count	= 0;
 	rcu_assign_pointer(p->grp, NULL);
 	INIT_LIST_HEAD(&p->grp_list);
