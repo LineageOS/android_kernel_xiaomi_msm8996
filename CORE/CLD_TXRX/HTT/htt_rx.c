9684b3ccf1b7ff3a5247cf36597eb1737c8131d9
Himanshu Agarwal <himanaga@qti.qualcomm.com>
2016-03-09 22:36:40 +0530

    qcacld-2.0: Add support to mark first packet after wow wakeup
    Add support to mark the highest bit of the skb->mark for the first
    packet after wow wakeup event from firmware
    
    Change-Id: I877dd7af9a58ebc02d73461cc2a29e86a3945dec
    CRs-Fixed: 989984

--- CORE/CLD_TXRX/HTT/htt_rx.c
+++ CORE/CLD_TXRX/HTT/htt_rx.c
@@ -1907,6 +1907,12 @@ htt_rx_amsdu_rx_in_order_pop_ll(
         *((u_int8_t *) &rx_desc->fw_desc.u.val) =
              HTT_RX_IN_ORD_PADDR_IND_FW_DESC_GET(*(msg_word + 1));
 
+        if (HTT_RX_IN_ORD_PADDR_IND_MSDU_INFO_GET(*(msg_word + 1)) &
+                             FW_MSDU_INFO_FIRST_WAKEUP_M) {
+                adf_os_print("%s: first packet after WOW wakeup\n", __func__);
+                adf_nbuf_update_skb_mark(msdu, HTT_MARK_FIRST_WAKEUP_PACKET);
+        }
+
         msdu_count--;
 
         if (ghtt_packetdump_cb) {
