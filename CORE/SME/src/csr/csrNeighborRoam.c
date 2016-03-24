279f1657d3e74ed12c5cb54c9e268119fe3c9748
Abhishek Singh <absingh@qti.qualcomm.com>
2016-03-24 12:21:28 +0530

    qcacld-2.0: If MCC is disabled do not roam to an AP which cause MCC
    prima to qcacld-2.0 propagation
    
    Even though MCC is disabled by gEnableMCCMode ini, driver try to
    roam to APs which may cause MCC but as MCC is disable the roaming
    fails eventually in CSR while trying to connect to the new profile.
    
    Driver creates a preauth session which is not deleted upon failure.
    The session sme state is eLIM_SME_WT_REASSOC_STATE and thus fresh
    scan required is set to false and cached scan result are returned.
    
    Do not add the APs which cause MCC scenario, in preauth candidate
    list if MCC is disabled.
    
    Change-Id: Iae2a887e1fa34f89f340bd7392d757e1add97a16
    CRs-Fixed: 992672

--- CORE/SME/src/csr/csrNeighborRoam.c
+++ CORE/SME/src/csr/csrNeighborRoam.c
@@ -2051,6 +2051,14 @@ csrNeighborRoamProcessScanResults(tpAniSirGlobal pMac,
                            "SKIP-currently associated AP");
                 continue;
             }
+            if (vos_concurrent_open_sessions_running() &&
+               !pMac->roam.configParam.fenableMCCMode &&
+               (pScanResult->BssDescriptor.channelId !=
+               csrGetConcurrentOperationChannel(pMac))) {
+                smsLog(pMac, LOG1, FL("MCC not supported so Ignore AP on channel %d"),
+                    pScanResult->BssDescriptor.channelId);
+                continue;
+            }
 
 #ifdef FEATURE_WLAN_LFR
 #ifdef WLAN_FEATURE_ROAM_SCAN_OFFLOAD
