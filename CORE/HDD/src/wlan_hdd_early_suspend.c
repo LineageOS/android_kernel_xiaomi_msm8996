8f10b207b07e46fad974add41dcaa20262a1411d
Ganesh Kondabattini <ganeshk@qti.qualcomm.com>
2016-04-06 18:05:06 +0530

    qcacld-2.0: Pass radio index in svc events
    The smart connection manager expects the radio index in
    dual wifi case to take the proper action for received
    wlan svc events.
    
    Change-Id: Ie89b15dc62879f90e93e5ee8cf2ea4dc74f88542
    CRs-Fixed: 1000468

--- CORE/HDD/src/wlan_hdd_early_suspend.c
+++ CORE/HDD/src/wlan_hdd_early_suspend.c
@@ -2305,7 +2305,8 @@ VOS_STATUS hdd_wlan_re_init(void *hif_sc)
    hdd_register_mcast_bcast_filter(pHddCtx);
    hdd_ssr_timer_del();
 
-   wlan_hdd_send_svc_nlink_msg(WLAN_SVC_FW_CRASHED_IND, NULL, 0);
+   wlan_hdd_send_svc_nlink_msg(pHddCtx->radio_index,
+                               WLAN_SVC_FW_CRASHED_IND, NULL, 0);
 
    /* Allow the phone to go to sleep */
    hdd_allow_suspend(WIFI_POWER_EVENT_WAKELOCK_DRIVER_REINIT);
