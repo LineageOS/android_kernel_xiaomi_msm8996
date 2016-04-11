f895924a55d11e247a6fe50b78c1c2762e16cd64
Camus Wong <camusw@codeaurora.org>
2016-04-11 16:56:02 -0400

    msm: mdp: Skip overlay start from overlay on if handoff pending
    If handoff is pending, we should skip the overlay start and kickoff
    and return success for overlay on. This is to ensure that fb open
    succeeds for all displays despite handoff being pending.
    
    Change-Id: If75f3a4ea61c48ee6b3c97096d20c592723fa97c
    Signed-off-by: Camus Wong <camusw@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -5560,6 +5560,7 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 	int rc;
 	struct mdss_overlay_private *mdp5_data;
 	struct mdss_mdp_ctl *ctl = NULL;
+	struct mdss_data_type *mdata;
 
 	if (!mfd)
 		return -ENODEV;
@@ -5569,6 +5570,10 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 
 	mdp5_data = mfd_to_mdp5_data(mfd);
 	if (!mdp5_data)
+		return -EINVAL;
+
+	mdata = mfd_to_mdata(mfd);
+	if (!mdata)
 		return -EINVAL;
 
 	mdss_mdp_set_lm_flag(mfd);
@@ -5600,8 +5605,16 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 	if (rc)
 		goto panel_on;
 
+	/* Skip the overlay start and kickoff for all displays
+	if handoff is pending. Previously we skipped it for DTV
+	panel and pluggable panels (bridge chip hdmi case). But
+	it does not cover the case where there is a non pluggable
+	tertiary display. Using the flag handoff_pending to skip
+	overlay start and kickoff should cover all cases
+	TODO: In the long run, the overlay start and kickoff
+	should not be skipped, instead, the handoff can be done */
 	if (!mfd->panel_info->cont_splash_enabled &&
-		(mfd->panel_info->type != DTV_PANEL)) {
+		!mdata->handoff_pending) {
 		rc = mdss_mdp_overlay_start(mfd);
 		if (rc)
 			goto end;
