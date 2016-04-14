e1ff8880169cb5e428634f29500b76a37581aed0
Naveen Rawat <nrawat@qca.qualcomm.com>
2016-04-14 16:21:41 -0700

    qcacld-2.0: Update NAN HDD APIs as per vendor definitions changes
    Remove usage of following attributes from NAN HDD APIs:
    1) QCA_WLAN_VENDOR_ATTR_NDP_APP_INFO_LEN
    2) QCA_WLAN_VENDOR_ATTR_NDP_NUM_INSTANCE_ID
    3) QCA_WLAN_VENDOR_ATTR_NDP_SCHEDULE_STATUS_CODE
    4) ndp_cfg from NDP_CONFRIM
    5) Remove mandatory check for ndp_app_info in data initiator
    
    Change-Id: I17fe580661f836d45a518a330a63d2c6de8bcf28
    CRs-Fixed: CRs-Fixed: 962367

--- CORE/MAC/inc/sirApi.h
+++ CORE/MAC/inc/sirApi.h
@@ -7218,7 +7218,6 @@ struct ndp_responder_rsp_event {
  * @ndp_instance_id: ndp instance id for which confirm is being generated
  * @peer_ndi_mac_addr: peer NDI mac address
  * @rsp_code: ndp response code
- * @ndp_config: ndp configuration
  * @ndp_info: ndp application info
  *
  */
@@ -7227,7 +7226,6 @@ struct ndp_confirm_event {
 	uint32_t ndp_instance_id;
 	v_MACADDR_t peer_ndi_mac_addr;
 	enum ndp_response_code rsp_code;
-	struct ndp_cfg ndp_config;
 	struct ndp_app_info ndp_info;
 };
 
