abec792ffe767649b40e83dd4224fdad23092709
Ratnam Rachuri <c_rrachu@qti.qualcomm.com>
2016-04-15 17:33:21 +0530

    qcacld-2.0: Logging level changes on wlan HOST
    prima to qcacld-2.0 propagation
    
    As there are a lot of prints dropped on the console in
    short duration, it leads to interrupt disablement on
    the cpu.
    
    So reducing the logging level of log statement from
    ERROR to INFO
    
    Change-Id: I3eefac49378a7bfbcc235b1dd3362e1213fa1f42
    CRs-Fixed: 886644

--- CORE/SME/src/QoS/sme_Qos.c
+++ CORE/SME/src/QoS/sme_Qos.c
@@ -7782,7 +7782,7 @@ void sme_QosPmcDeviceStateUpdateInd(void *callbackContext, tPmcState pmcState)
       break;
    default:
       status = eHAL_STATUS_SUCCESS;
-      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR,
+      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO,
                 "%s: %d: nothing to process in PMC state %s (%d)",
                 __func__, __LINE__,
                 sme_PmcStatetoString(pmcState), pmcState);
