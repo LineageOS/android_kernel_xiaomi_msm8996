5ad6cc46eda9e6d7195c353730a22427b9937283
Selvaraj, Sridhar <sselvara@qti.qualcomm.com>
2016-04-22 16:32:42 +0530

    qcacld-2.0: Use BSSID for active scan if provided by supplicant
    Presently, supplicant does not send bssid parameter as part of scan
    command to driver. Since that support is added, use the bssid received
    from scan command for active scan ie add the bssid as part of scan
    offload command to firmware so as to send probe request with bssid
    set with the one received as part of the scan command.
    
    Change-Id: I97886b1dbd63fbca21fa410252df572f819df207
    CRs-Fixed: 1010596

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -17095,6 +17095,9 @@ int __wlan_hdd_cfg80211_scan( struct wiphy *wiphy,
     scanRequest.minChnTime = cfg_param->nActiveMinChnTime;
     scanRequest.maxChnTime = cfg_param->nActiveMaxChnTime;
 
+#ifdef CFG80211_SCAN_BSSID
+    vos_mem_copy(scanRequest.bssid, request->bssid, VOS_MAC_ADDR_SIZE);
+#endif
     /* set BSSType to default type */
     scanRequest.BSSType = eCSR_BSS_TYPE_ANY;
 
