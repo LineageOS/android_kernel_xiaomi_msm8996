0582320c3819c340333b8c6401234fb9b70eb90d
Joonwoo Park <joonwoop@codeaurora.org>
2016-05-25 11:27:35 -0700

    sched: eliminate sched_freq_account_wait_time knob
    Kill unused scheduler knob sched_freq_account_wait_time.
    
    Change-Id: Ib74123ebd69dfa3f86cf7335099f50c12a6e93c3
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1685,7 +1685,7 @@ int register_cpu_cycle_counter_cb(struct cpu_cycle_counter_cb *cb)
 
 /*
  * sched_window_stats_policy, sched_ravg_hist_size,
- * sched_migration_fixup, sched_freq_account_wait_time have a 'sysctl' copy
+ * sched_migration_fixup have a 'sysctl' copy
  * associated with them. This is required for atomic update of those variables
  * when being modifed via sysctl interface.
  *
@@ -1726,8 +1726,7 @@ __read_mostly unsigned int sysctl_sched_new_task_windows = 5;
 static __read_mostly unsigned int sched_migration_fixup = 1;
 __read_mostly unsigned int sysctl_sched_migration_fixup = 1;
 
-static __read_mostly unsigned int sched_freq_account_wait_time;
-__read_mostly unsigned int sysctl_sched_freq_account_wait_time;
+#define SCHED_FREQ_ACCOUNT_WAIT_TIME 0
 
 /*
  * For increase, send notification if
@@ -2089,11 +2088,11 @@ static int account_busy_for_cpu_time(struct rq *rq, struct task_struct *p,
 		if (rq->curr == p)
 			return 1;
 
-		return p->on_rq ? sched_freq_account_wait_time : 0;
+		return p->on_rq ? SCHED_FREQ_ACCOUNT_WAIT_TIME : 0;
 	}
 
 	/* TASK_MIGRATE, PICK_NEXT_TASK left */
-	return sched_freq_account_wait_time;
+	return SCHED_FREQ_ACCOUNT_WAIT_TIME;
 }
 
 static inline int
@@ -2314,7 +2313,7 @@ void update_task_pred_demand(struct rq *rq, struct task_struct *p, int event)
 		return;
 
 	if (event != PUT_PREV_TASK && event != TASK_UPDATE &&
-			(!sched_freq_account_wait_time ||
+			(!SCHED_FREQ_ACCOUNT_WAIT_TIME ||
 			 (event != TASK_MIGRATE &&
 			 event != PICK_NEXT_TASK)))
 		return;
@@ -2324,7 +2323,7 @@ void update_task_pred_demand(struct rq *rq, struct task_struct *p, int event)
 	 * related groups
 	 */
 	if (event == TASK_UPDATE) {
-		if (!p->on_rq && !sched_freq_account_wait_time)
+		if (!p->on_rq && !SCHED_FREQ_ACCOUNT_WAIT_TIME)
 			return;
 	}
 
@@ -3227,7 +3226,6 @@ enum reset_reason_code {
 	POLICY_CHANGE,
 	HIST_SIZE_CHANGE,
 	MIGRATION_FIXUP_CHANGE,
-	FREQ_ACCOUNT_WAIT_TIME_CHANGE,
 	FREQ_AGGREGATE_CHANGE,
 };
 
@@ -3236,7 +3234,7 @@ const char *sched_window_reset_reasons[] = {
 	"POLICY_CHANGE",
 	"HIST_SIZE_CHANGE",
 	"MIGRATION_FIXUP_CHANGE",
-	"FREQ_ACCOUNT_WAIT_TIME_CHANGE"};
+};
 
 /* Called with IRQs enabled */
 void reset_all_window_stats(u64 window_start, unsigned int window_size)
@@ -3310,13 +3308,6 @@ void reset_all_window_stats(u64 window_start, unsigned int window_size)
 		old = sched_migration_fixup;
 		new = sysctl_sched_migration_fixup;
 		sched_migration_fixup = sysctl_sched_migration_fixup;
-	} else if (sched_freq_account_wait_time !=
-					sysctl_sched_freq_account_wait_time) {
-		reason = FREQ_ACCOUNT_WAIT_TIME_CHANGE;
-		old = sched_freq_account_wait_time;
-		new = sysctl_sched_freq_account_wait_time;
-		sched_freq_account_wait_time =
-				 sysctl_sched_freq_account_wait_time;
 	} else if (sched_freq_aggregate !=
 					sysctl_sched_freq_aggregate) {
 		reason = FREQ_AGGREGATE_CHANGE;
