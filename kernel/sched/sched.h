40162ed550e197f42dacf4c5f9b2d2b06a7ca593
Pavankumar Kondeti <pkondeti@codeaurora.org>
2016-06-17 08:52:59 +0530

    sched: fix compilation errors with !SCHED_HMP
    Accessing p->grp in __migrate_task() and detach_task()
    breaks compilation when SCHED_HMP is not defined. Add a
    wrapper to check if a task belongs to related thread
    group or not.
    
    Change-Id: I558f9a6c7b5f9bd4a21b5021b8eb80bace69bc32
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1197,6 +1197,11 @@ struct related_thread_group *task_related_thread_group(struct task_struct *p)
 	return rcu_dereference(p->grp);
 }
 
+static inline int is_task_in_related_thread_group(struct task_struct *p)
+{
+	return rcu_access_pointer(p->grp) != NULL;
+}
+
 #else	/* CONFIG_SCHED_HMP */
 
 struct hmp_sched_stats;
@@ -1249,6 +1254,11 @@ static inline
 struct related_thread_group *task_related_thread_group(struct task_struct *p)
 {
 	return NULL;
+}
+
+static inline int is_task_in_related_thread_group(struct task_struct *p)
+{
+	return 0;
 }
 
 static inline u32 task_load(struct task_struct *p) { return 0; }
