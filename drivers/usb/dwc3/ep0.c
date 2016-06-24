4e92a135201f8f8071e2b656ca1dfe7241830d38
dianlujitao <dianlujitao@gmail.com>
2016-06-24 21:50:50 +0800

    Revert "usb: dwc3: Fix assignment of EP transfer resources"
     * QCOM fixed in db123d09eac41d84579edd15167cb9c89c34f964
    
    This reverts commit 174bf25de7d4777c7ee865985e079a04f18dc9e1.
    
    Change-Id: Idc6ed8d7b0e9dfc14ccdc74d300b0702918aa06c

--- drivers/usb/dwc3/ep0.c
+++ drivers/usb/dwc3/ep0.c
@@ -575,6 +575,7 @@ static int dwc3_ep0_set_config(struct dwc3 *dwc, struct usb_ctrlrequest *ctrl)
 	int ret;
 	u32 reg;
 
+	dwc->start_config_issued = false;
 	cfg = le16_to_cpu(ctrl->wValue);
 
 	switch (state) {
@@ -761,6 +762,10 @@ static int dwc3_ep0_std_request(struct dwc3 *dwc, struct usb_ctrlrequest *ctrl)
 		dwc3_trace(trace_dwc3_ep0, "USB_REQ_SET_ISOCH_DELAY\n");
 		ret = dwc3_ep0_set_isoch_delay(dwc, ctrl);
 		break;
+	case USB_REQ_SET_INTERFACE:
+		dev_vdbg(dwc->dev, "USB_REQ_SET_INTERFACE\n");
+		dwc->start_config_issued = false;
+		/* Fall through */
 	default:
 		dwc3_trace(trace_dwc3_ep0, "Forwarding to gadget driver\n");
 		ret = dwc3_ep0_delegate_req(dwc, ctrl);
