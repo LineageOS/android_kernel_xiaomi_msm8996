be45cbbcbf0cfdea72c35d007f30b61cf9690938
Archana Ramachandran <archanar@codeaurora.org>
2016-06-27 11:44:13 -0700

    qcacld-2.0: Fix NULL pointer dereferencing in p2p TX MGMT
    Fix dereferencing channel struct pointer without verifying the
    validity.
    
    CRs-Fixed: 1034743
    Change-Id: I7a49467c3fdbe85c7255fcce7f135e53658eef63

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_p2p.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_p2p.c
@@ -2171,7 +2171,7 @@ int __wlan_hdd_mgmt_tx(struct wiphy *wiphy, struct net_device *dev,
     }
 
 
-    if (ieee80211_frequency_to_channel(chan->center_freq) == home_ch) {
+    if (chan && ieee80211_frequency_to_channel(chan->center_freq) == home_ch) {
         /* if adapter is already on requested ch, no need for ROC */
         wait = 0;
         hddLog(LOGE,
@@ -2179,7 +2179,7 @@ int __wlan_hdd_mgmt_tx(struct wiphy *wiphy, struct net_device *dev,
         goto send_frame;
     }
 
-    if( offchan && wait)
+    if( offchan && wait && chan)
     {
         int status;
         rem_on_channel_request_type_t req_type = OFF_CHANNEL_ACTION_TX;
