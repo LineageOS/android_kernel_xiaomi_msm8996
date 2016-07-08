2628872609e3c429c673dbf1a437bc6cb3636a42
Alberto97 <albertop2197@gmail.com>
2016-07-08 08:14:01 +0200

    drivers: drv2604: Rename vtg_level_default to vtg_default
    Required to match hardware/cyanogen VibratorHW implementation needs.
    
    Change-Id: I4f74a958acfcae5d081b4b39ab6dc416a2d64c6e

--- drivers/misc/drv2604.c
+++ drivers/misc/drv2604.c
@@ -149,7 +149,7 @@ static ssize_t drv2604_vib_max_show(struct device *dev,
 	return scnprintf(buf, PAGE_SIZE, "%d\n", MAX_VIBE_STRENGTH);
 }
 
-static ssize_t drv2604_vib_level_default_show(struct device *dev,
+static ssize_t drv2604_vib_default_show(struct device *dev,
 		struct device_attribute *attr,
 		char *buf)
 {
@@ -194,13 +194,13 @@ static ssize_t drv2604_vib_level_store(struct device *dev,
 
 static DEVICE_ATTR(vtg_min, S_IRUGO, drv2604_vib_min_show, NULL);
 static DEVICE_ATTR(vtg_max, S_IRUGO, drv2604_vib_max_show, NULL);
-static DEVICE_ATTR(vtg_level_default, S_IRUGO, drv2604_vib_level_default_show, NULL);
+static DEVICE_ATTR(vtg_default, S_IRUGO, drv2604_vib_default_show, NULL);
 static DEVICE_ATTR(vtg_level, S_IRUGO | S_IWUSR, drv2604_vib_level_show, drv2604_vib_level_store);
 
 static struct attribute *timed_dev_attrs[] = {
 	&dev_attr_vtg_min.attr,
 	&dev_attr_vtg_max.attr,
-	&dev_attr_vtg_level_default.attr,
+	&dev_attr_vtg_default.attr,
 	&dev_attr_vtg_level.attr,
 	NULL,
 };
