20c0bd95a6a065a60ec6ec6be5a9abb7ba0c5378
Govind Singh <govinds@qti.qualcomm.com>
2016-07-14 18:53:17 +0530

    qcacld-2.0: Remove peer id check for per msdu count
    In rx data path, aggregated msdu are coming for single
    peer id. Hence, it is unnecessary to validate peer id
    per msdu count. This is resulting in excessive logging.
    Remove peer id check for per msdu count to avoid excessive
    logging in case of wrong peer id passed from the target.
    
    Change-Id: I57af2f5b067fa4e76f42379afa40a0f547221fac
    CRs-Fixed: 1041359

--- CORE/SERVICES/COMMON/ol_htt_api.h
+++ CORE/SERVICES/COMMON/ol_htt_api.h
@@ -326,12 +326,12 @@ htt_clear_bundle_stats(struct htt_pdev_t *pdev);
 
 #endif
 
-typedef void (*tp_rx_pkt_dump_cb)(adf_nbuf_t msdu, uint16_t peer_id,
+typedef void (*tp_rx_pkt_dump_cb)(adf_nbuf_t msdu, struct ol_txrx_peer_t *peer,
                                           uint8_t status);
 void htt_register_rx_pkt_dump_callback(struct htt_pdev_t *pdev,
               tp_rx_pkt_dump_cb ol_rx_pkt_dump_call);
 void htt_deregister_rx_pkt_dump_callback(struct htt_pdev_t *pdev);
-void ol_rx_pkt_dump_call(adf_nbuf_t msdu, uint16_t peer_id, uint8_t status);
+void ol_rx_pkt_dump_call(adf_nbuf_t msdu, struct ol_txrx_peer_t *peer, uint8_t status);
 void htt_mark_first_wakeup_packet(htt_pdev_handle pdev, uint8_t value);
 
 #endif /* _OL_HTT_API__H_ */
