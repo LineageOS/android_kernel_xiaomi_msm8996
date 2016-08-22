caacbde912df599d3a0ddf4df1200a932c03eeb8
Himanshu Agarwal <himanaga@qti.qualcomm.com>
2016-08-22 14:05:01 +0530

    qcacld-2.0: Add sanity check to avoid NULL pointer dereference
    Add sanity check to avoid NULL pointer dereferencing of tx
    completion handler callback in hif_send_buffer_cleanup_on_pipe.
    
    Change-Id: If38b4b7cdd1154d8272e5150c6653f90df8f4b5e
    CRs-Fixed: 1057490

--- CORE/SERVICES/HIF/PCIe/hif_pci.c
+++ CORE/SERVICES/HIF/PCIe/hif_pci.c
@@ -1810,8 +1810,9 @@ hif_send_buffer_cleanup_on_pipe(struct HIF_CE_pipe_info *pipe_info)
                 return;
             }
             /* Indicate the completion to higer layer to free the buffer */
-            hif_state->msg_callbacks_current.txCompletionHandler(
-                hif_state->msg_callbacks_current.Context, netbuf, id);
+            if (hif_state->msg_callbacks_current.txCompletionHandler)
+                hif_state->msg_callbacks_current.txCompletionHandler(
+                    hif_state->msg_callbacks_current.Context, netbuf, id);
         }
     }
 }
