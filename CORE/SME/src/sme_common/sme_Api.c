76beacf483eb0ec477333f60b73c2092c7c97ce0
Deepthi Gowri <deepthi@codeaurora.org>
2016-09-12 12:16:41 +0530

    qcacld-2.0: Fix for crash caused by buffer overflow
    prima to qcacld-2.0 propagation
    
    In sme_UpdateDSCPtoUPMapping() function, we try to write into
    the array dscpmapping in 255th location, if the location index
    happens to be 255. But the size of dscpmapping array is 64 only
    which is causing out of boundary access resulting in crash.
    
    To address this, avoid writing in out of boundary. If the DSCP
    range is 255, there is no mapping for such priority, so there is
    no need assign anything for dscpmapping.
    
    CRs-Fixed: 1027457
    Change-Id: Ic4299122b10a990d17816f864ef8415c1b75b230

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -15521,7 +15521,6 @@ VOS_STATUS sme_UpdateDSCPtoUPMapping( tHalHandle hHal,
                    if ((pSession->QosMapSet.dscp_range[i][0] == 255) &&
                                 (pSession->QosMapSet.dscp_range[i][1] == 255))
                    {
-                       dscpmapping[j]= 0;
                        VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR,
                        "%s: User Priority %d is not used in mapping",
                                                              __func__, i);
