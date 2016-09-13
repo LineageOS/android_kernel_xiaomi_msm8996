49482cec99561e87ccd7ad73c392b59797d6f267
flar2 <asegaert@gmail.com>
2016-09-13 00:44:20 -0400

    msm: mdss: KCAL: disable igc update
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>
    (cherry picked from commit 47eb5af2bf02d413fb8e1d6a872ae0bc4fea1340)
    (cherry picked from commit fc4dff3ee56e72b14ecae6b865457fee74a2ee26)

--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -408,10 +408,11 @@ static ssize_t kcal_invert_store(struct device *dev,
 		(lut_data->invert == kcal_invert))
 		return -EINVAL;
 
-	lut_data->invert = kcal_invert;
+	//disable
+	lut_data->invert = 0;
 
-	mdss_mdp_kcal_update_igc(lut_data);
-	mdss_mdp_kcal_display_commit();
+	//mdss_mdp_kcal_update_igc(lut_data);
+	//mdss_mdp_kcal_display_commit();
 
 	return count;
 }
