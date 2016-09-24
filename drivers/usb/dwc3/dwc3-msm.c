f6db818fb0c758b4b62898b9bc38d7b0c3a17214
Sultanxda <sultanxda@gmail.com>
2016-09-24 13:46:09 -0700

    usb: dwc3-msm: Remove indefinite wakeup
    Something is wrong with this wakeup; it isn't always relaxed, thus
    wreaking havoc on battery life. We already have a wakeup in smbcharger
    that is triggered on USB connection, so instead of fixing this one, just
    remove it since it's redundant.
    
    Change-Id: If81b223f37a4a5456940aea02b2e12121c4a39c5

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2163,8 +2163,10 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 		dev_dbg(mdwc->dev, "defer suspend with %d(msecs)\n",
 					mdwc->lpm_to_suspend_delay);
 		pm_wakeup_event(mdwc->dev, mdwc->lpm_to_suspend_delay);
+#ifndef CONFIG_MACH_XIAOMI
 	} else {
 		pm_relax(mdwc->dev);
+#endif
 	}
 
 	atomic_set(&dwc->in_lpm, 1);
@@ -2204,7 +2206,9 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		return 0;
 	}
 
+#ifndef CONFIG_MACH_XIAOMI
 	pm_stay_awake(mdwc->dev);
+#endif
 
 	/* Vote for TCXO while waking up USB HSPHY */
 	ret = clk_prepare_enable(mdwc->xo_clk);
@@ -3270,7 +3274,9 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 		register_cpu_notifier(&mdwc->dwc3_cpu_notifier);
 
 	device_init_wakeup(mdwc->dev, 1);
+#ifndef CONFIG_MACH_XIAOMI
 	pm_stay_awake(mdwc->dev);
+#endif
 
 	if (of_property_read_bool(node, "qcom,disable-dev-mode-pm"))
 		pm_runtime_get_noresume(mdwc->dev);
@@ -3961,7 +3967,9 @@ static void dwc3_msm_otg_sm_work(struct work_struct *w)
 						dcp_max_current);
 				atomic_set(&dwc->in_lpm, 1);
 				dbg_event(0xFF, "RelaxDCP", 0);
+#ifndef CONFIG_MACH_XIAOMI
 				pm_relax(mdwc->dev);
+#endif
 				break;
 			case DWC3_CDP_CHARGER:
 			case DWC3_SDP_CHARGER:
