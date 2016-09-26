ef0ee3e365beed13cb0244d6c699e15c7dca8633
andip71 <andreasp@gmx.de>
2016-09-26 17:45:56 +0200

    msm: mdss: kcal: KCAL driver is off by default
    Change-Id: Idc29f7eeab6f11f2a4c9bc9654a8eb47ff4d5812
    (cherry picked from commit fbc935d07d633b36660c9261f95cdd8c14208b2c)
    (cherry picked from commit 7d839d533869e07a710b0504c2cf8ad2066e3f4c)

--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -554,7 +554,7 @@ static int kcal_ctrl_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, lut_data);
 
-	lut_data->enable = 0x1;
+	lut_data->enable = 0x0; // KCAL driver is off by default
 	lut_data->red = DEF_PCC;
 	lut_data->green = DEF_PCC;
 	lut_data->blue = DEF_PCC;
@@ -565,10 +565,10 @@ static int kcal_ctrl_probe(struct platform_device *pdev)
 	lut_data->val = DEF_PA;
 	lut_data->cont = DEF_PA;
 
-	mdss_mdp_kcal_update_pcc(lut_data);
-	mdss_mdp_kcal_update_pa(lut_data);
-	mdss_mdp_kcal_update_igc(lut_data);
-	mdss_mdp_kcal_display_commit();
+	//mdss_mdp_kcal_update_pcc(lut_data);
+	//mdss_mdp_kcal_update_pa(lut_data);
+	//mdss_mdp_kcal_update_igc(lut_data);
+	//mdss_mdp_kcal_display_commit();
 
 	ret = device_create_file(&pdev->dev, &dev_attr_kcal);
 	ret |= device_create_file(&pdev->dev, &dev_attr_kcal_min);
