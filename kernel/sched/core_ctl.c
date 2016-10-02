e3588fb52e9b01c475d8af660a82d341d67ea7b0
Sultanxda <sultanxda@gmail.com>
2016-10-02 01:16:11 -0700

    core_ctl: Allow certain CPUs to always be kept online
    This allows specific CPUs to never be offlined. Useful for always keeping
    CPU0 online, for example.
    
    Change-Id: Ia91b22eab18cc3fb4bf086c9e737b0e20fdd75ba

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -61,6 +61,7 @@ struct cpu_data {
 	struct kobject kobj;
 	struct list_head pending_lru;
 	bool disabled;
+	bool always_online_cpu;
 };
 
 static DEFINE_PER_CPU(struct cpu_data, cpu_state);
@@ -371,6 +372,47 @@ static ssize_t show_disable(struct cpu_data *state, char *buf)
 	return snprintf(buf, PAGE_SIZE, "%u\n", state->disabled);
 }
 
+static ssize_t store_always_online_cpu(struct cpu_data *state,
+				const char *buf, size_t count)
+{
+	struct cpu_data *c;
+	unsigned int i, first_cpu;
+	unsigned int val[MAX_CPUS_PER_GROUP];
+	int ret;
+
+	ret = sscanf(buf, "%u %u %u %u\n", &val[0], &val[1], &val[2], &val[3]);
+	if (ret != 1 && ret != state->num_cpus)
+		return -EINVAL;
+
+	first_cpu = state->first_cpu;
+
+	for (i = 0; i < state->num_cpus; i++) {
+		c = &per_cpu(cpu_state, first_cpu);
+		c->always_online_cpu = val[i];
+		first_cpu++;
+	}
+
+	return count;
+}
+
+static ssize_t show_always_online_cpu(struct cpu_data *state, char *buf)
+{
+	struct cpu_data *c;
+	ssize_t count = 0;
+	unsigned int i, first_cpu;
+
+	first_cpu = state->first_cpu;
+
+	for (i = 0; i < state->num_cpus; i++) {
+		c = &per_cpu(cpu_state, first_cpu);
+		count += snprintf(buf + count, PAGE_SIZE - count,
+				"\tCPU:%d %u\n", first_cpu, c->always_online_cpu);
+		first_cpu++;
+	}
+
+	return count;
+}
+
 struct core_ctl_attr {
 	struct attribute attr;
 	ssize_t (*show)(struct cpu_data *, char *);
@@ -398,6 +440,7 @@ core_ctl_attr_ro(online_cpus);
 core_ctl_attr_ro(global_state);
 core_ctl_attr_rw(not_preferred);
 core_ctl_attr_rw(disable);
+core_ctl_attr_rw(always_online_cpu);
 
 static struct attribute *default_attrs[] = {
 	&min_cpus.attr,
@@ -413,6 +456,7 @@ static struct attribute *default_attrs[] = {
 	&global_state.attr,
 	&not_preferred.attr,
 	&disable.attr,
+	&always_online_cpu.attr,
 	NULL
 };
 
@@ -760,7 +804,7 @@ static void __ref do_hotplug(struct cpu_data *f)
 	mutex_lock(&lru_lock);
 	if (f->online_cpus > need) {
 		list_for_each_entry_safe(c, tmp, &f->lru, sib) {
-			if (!c->online)
+			if (!c->online || c->always_online_cpu)
 				continue;
 
 			if (f->online_cpus == need)
@@ -783,7 +827,7 @@ static void __ref do_hotplug(struct cpu_data *f)
 			goto done;
 
 		list_for_each_entry_safe(c, tmp, &f->lru, sib) {
-			if (!c->online)
+			if (!c->online || c->always_online_cpu)
 				continue;
 
 			if (f->online_cpus <= f->max_cpus)
