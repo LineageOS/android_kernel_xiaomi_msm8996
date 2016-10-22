2e481fe74323d23d8bd819e896fee92ed7e97190
Louis Popi <theh2o64@gmail.com>
2016-10-22 09:14:14 +0200

    fingerprint: fpc1020: Allow sending keycodes to userspace
    Change-Id: I464651a2560964180e0f840d6717a99e32ded2f7

--- drivers/input/fingerprint/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc1020_tee.c
@@ -45,6 +45,7 @@
 #include <linux/regulator/consumer.h>
 #include <linux/wakelock.h>
 #include <soc/qcom/scm.h>
+
 #ifdef CONFIG_FB
 #include <linux/notifier.h>
 #include <linux/fb.h>
@@ -62,6 +63,7 @@ struct fpc1020_data {
 	int  rst_gpio;
 	int  fp_id_gpio;
 	int  wakeup_enabled;
+	int  homebutton_enabled;
 
 	struct pinctrl         *ts_pinctrl;
 	struct pinctrl_state   *gpio_state_active;
@@ -72,7 +74,7 @@ struct fpc1020_data {
 	struct work_struct reset_work;
 	struct workqueue_struct *reset_workqueue;
 #endif
-
+	struct input_dev	*input_dev;
 };
 
 enum {
@@ -260,19 +262,90 @@ static ssize_t enable_wakeup_store(struct device *dev,
 		return -EINVAL;
 	}
 }
+
+static ssize_t homebutton_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+	char c;
+
+	c = fpc1020->homebutton_enabled ? '1' : '0';
+	return scnprintf(buf, PAGE_SIZE, "%c\n", c);
+}
+
+
+static ssize_t homebutton_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+	int i;
+
+	if (sscanf(buf, "%u", &i) == 1 && i < 2) {
+		fpc1020->homebutton_enabled = (i == 1);
+
+		dev_info(dev, "%s\n", i ? "homebutton enabled" : "homebutton disabled");
+		return count;
+	} else {
+		dev_info(dev, "homebutton write error\n");
+		return -EINVAL;
+	}
+}
+
 static DEVICE_ATTR(enable_wakeup, S_IWUSR | S_IRUSR, enable_wakeup_show,
 		   enable_wakeup_store);
 
+static DEVICE_ATTR(homebutton, S_IWUSR | S_IRUSR, homebutton_show,
+		   homebutton_store);
 
 static struct attribute *attributes[] = {
 	&dev_attr_irq.attr,
 	&dev_attr_enable_wakeup.attr,
+	&dev_attr_homebutton.attr,
 	NULL
 };
 
 static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
+
+int fpc1020_input_init(struct fpc1020_data *fpc1020)
+{
+	int error = 0;
+
+	dev_dbg(fpc1020->dev, "%s\n", __func__);
+
+	fpc1020->input_dev = input_allocate_device();
+
+	if (!fpc1020->input_dev) {
+		dev_err(fpc1020->dev, "%s failed to allocate input device\n", __func__);
+		error  = -ENOMEM;
+	}
+
+	if (!error) {
+		fpc1020->input_dev->name = "uinput-fpc";
+
+		/* Set event bits according to what events we are generating */
+		set_bit(EV_KEY, fpc1020->input_dev->evbit);
+		set_bit(KEY_HOME, fpc1020->input_dev->keybit);
+
+		/* Register the input device */
+		error = input_register_device(fpc1020->input_dev);
+		if (error) {
+			dev_err(fpc1020->dev, "%s failed to register input device\n", __func__);
+			input_free_device(fpc1020->input_dev);
+			fpc1020->input_dev = NULL;
+		}
+	}
+	return error;
+}
+
+void fpc1020_input_destroy(struct fpc1020_data *fpc1020)
+{
+	dev_dbg(fpc1020->dev, "%s\n", __func__);
+
+	if (fpc1020->input_dev != NULL)
+		input_free_device(fpc1020->input_dev);
+}
 
 static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
@@ -432,6 +505,10 @@ static int fpc1020_tee_probe(struct platform_device *pdev)
 		goto exit;
 
 	rc = fpc1020_pinctrl_select_tee(fpc1020, true);
+	if (rc)
+		goto exit;
+
+	rc = fpc1020_input_init(fpc1020);
 	if (rc)
 		goto exit;
 
