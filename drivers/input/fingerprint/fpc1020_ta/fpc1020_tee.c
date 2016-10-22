91d8ec285253cd60688c46a03630a7c282dff244
Louis Popi <theh2o64@gmail.com>
2016-10-22 09:14:14 +0200

    fingerprint: fpc1020_ta: Allow sending keycodes to userspace
    Change-Id: I464651a2560964180e0f840d6717a99e32ded2f7

--- drivers/input/fingerprint/fpc1020_ta/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc1020_ta/fpc1020_tee.c
@@ -45,6 +45,7 @@
 #include <linux/regulator/consumer.h>
 #include <linux/wakelock.h>
 #include <soc/qcom/scm.h>
+
 #ifdef CONFIG_FB
 #include <linux/notifier.h>
 #include <linux/fb.h>
@@ -72,7 +73,7 @@ struct fpc1020_data {
 	struct work_struct reset_work;
 	struct workqueue_struct *reset_workqueue;
 #endif
-
+	struct input_dev	*input_dev;
 };
 
 enum {
@@ -274,6 +275,47 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
+int fpc1020_input_init(struct fpc1020_data *fpc1020)
+{
+	int error = 0;
+
+
+	dev_dbg(fpc1020->dev, "%s\n", __func__);
+
+	fpc1020->input_dev = input_allocate_device();
+
+	if (!fpc1020->input_dev) {
+		dev_err(fpc1020->dev, "Input_allocate_device failed.\n");
+		error  = -ENOMEM;
+	}
+
+	if (!error) {
+		fpc1020->input_dev->name = "fpc1020";
+
+		/* Set event bits according to what events we are generating */
+		set_bit(EV_KEY, fpc1020->input_dev->evbit);
+		set_bit(KEY_F19, fpc1020->input_dev->keybit);
+
+		/* Register the input device */
+		error = input_register_device(fpc1020->input_dev);
+
+		if (error) {
+			dev_err(fpc1020->dev, "Input_register_device failed.\n");
+			input_free_device(fpc1020->input_dev);
+			fpc1020->input_dev = NULL;
+		}
+	}
+	return error;
+}
+
+void fpc1020_input_destroy(struct fpc1020_data *fpc1020)
+{
+	dev_dbg(fpc1020->dev, "%s\n", __func__);
+
+	if (fpc1020->input_dev != NULL)
+		input_free_device(fpc1020->input_dev);
+}
+
 static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
@@ -282,6 +324,12 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	/* Make sure 'wakeup_enabled' is updated before using it
 	** since this is interrupt context (other thread...) */
 	smp_rmb();
+
+       /* Report button input */
+       input_report_key(fpc1020->input_dev, KEY_F19, 1);
+       input_sync(fpc1020->input_dev);
+       input_report_key(fpc1020->input_dev, KEY_F19, 0);
+       input_sync(fpc1020->input_dev);
 
 	if (fpc1020->wakeup_enabled) {
 		wake_lock_timeout(&fpc1020->ttw_wl, msecs_to_jiffies(FPC_TTW_HOLD_TIME));
@@ -432,6 +480,10 @@ static int fpc1020_tee_probe(struct platform_device *pdev)
 		goto exit;
 
 	rc = fpc1020_pinctrl_select_tee(fpc1020, true);
+	if (rc)
+		goto exit;
+
+	rc = fpc1020_input_init(fpc1020);
 	if (rc)
 		goto exit;
 
