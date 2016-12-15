99012bc147503b5d760e2ffb1cfc58edc964d290
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-12-15 15:37:47 -0800

    rcu: Don't wake rcuc/X kthreads on NOCB CPUs
    Chris Friesen notice that rcuc/X kthreads were consuming CPU even on
    NOCB CPUs.  This makes no sense because the only purpose or these
    kthreads is to invoke normal (non-offloaded) callbacks, of which there
    will never be any on NOCB CPUs.  This problem was due to a bug in
    cpu_has_callbacks_ready_to_invoke(), which should have been checking
    ->nxttail[RCU_NEXT_TAIL] for NULL, but which was instead (incorrectly)
    checking ->nxttail[RCU_DONE_TAIL].  Because ->nxttail[RCU_DONE_TAIL] is
    never NULL, the only effect is to cause the rcuc/X kthread to execute
    when it should not do so.
    
    This commit therefore checks ->nxttail[RCU_NEXT_TAIL], which is NULL
    for NOCB CPUs.
    
    Change-Id: Iadf4ffdf390bdfaba0c0740a5fea677f3faee731
    Reported-by: Chris Friesen <chris.friesen@windriver.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -572,8 +572,8 @@ EXPORT_SYMBOL_GPL(rcutorture_record_progress);
 static int
 cpu_has_callbacks_ready_to_invoke(struct rcu_data *rdp)
 {
-    return &rdp->nxtlist != rdp->nxttail[RCU_DONE_TAIL] &&
-           rdp->nxttail[RCU_DONE_TAIL] != NULL;
+	return &rdp->nxtlist != rdp->nxttail[RCU_DONE_TAIL] &&
+	       rdp->nxttail[RCU_NEXT_TAIL] != NULL;
 }
 
 /*
