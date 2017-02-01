a180bedc7c09d72c0dd94b0497c0f570c04af75a
Andres Oportus <andresoportus@google.com>
2017-02-01 13:34:54 -0800

    ANDROID: cpufreq_stat: add per task/uid/freq stats
    Adds per process nodes in /proc/PID/time_in_state showing per
    frequency times and adds a global /proc/uid_time_in_state
    showing per frequency per uid times.
    
    Bug: 34133340
    Bug: 38320164
    
    Tests: boot sailfish and reading /proc/uid_time_in_state and
    /proc/$$/time_in_state
    
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Change-Id: Ideb22b608b9a5e7bd2200a3a6df0f110b635f96a

--- kernel/exit.c
+++ kernel/exit.c
@@ -54,6 +54,7 @@
 #include <linux/writeback.h>
 #include <linux/shm.h>
 #include <linux/kcov.h>
+#include <linux/cpufreq.h>
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
@@ -174,6 +175,9 @@ void release_task(struct task_struct *p)
 {
 	struct task_struct *leader;
 	int zap_leader;
+#ifdef CONFIG_CPU_FREQ_STAT
+	cpufreq_task_stats_exit(p);
+#endif
 repeat:
 	/* don't need to get the RCU readlock here - the process is dead and
 	 * can't be modifying its own credentials. But shut RCU-lockdep up */
