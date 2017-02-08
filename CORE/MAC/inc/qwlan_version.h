a02f2fd3a8e0b5f41ac929db70131492e86593c4
Ashish kumar goswami <agoswa@codeaurora.org>
2017-02-08 18:03:30 +0530

    Release 4.0.11.203A
    Release 4.0.11.203A
    
    Change-Id: I3b2ebc77a5ade07563a14b2fa3f1f071ffbf0974
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,7 +44,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_EXTRA            ""
 #define QWLAN_VERSION_BUILD            203
 
-#define QWLAN_VERSIONSTR               "4.0.11.203"
+#define QWLAN_VERSIONSTR               "4.0.11.203A"
 
 
 #define AR6320_REV1_VERSION             0x5000000
