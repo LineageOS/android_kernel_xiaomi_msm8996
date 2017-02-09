8fa62bff71d8754577882134af1fc1fc556d68a9
qcabuildsw <qcabuildsw@localhost>
2017-02-08 23:48:01 -0800

    Release 4.0.11.203A
    Release 4.0.11.203A
    
    Change-Id: Iea56be439a7366292e0e054fb6d63e908861a2f5
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -41,7 +41,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            4
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            11
-#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_EXTRA            "A"
 #define QWLAN_VERSION_BUILD            203
 
 #define QWLAN_VERSIONSTR               "4.0.11.203A"
