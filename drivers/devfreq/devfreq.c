7c841f6369cb2905065ee9a7c9083bc194d75c06
engstk <eng.stk@sapo.pt>
2017-02-14 12:53:33 +0000

    drivers: devfreq: add simple_ondemand
    Signed-off-by: engstk <eng.stk@sapo.pt>
    (cherry picked from commit 3eebf9b33dcaa5c8a398644359303dde49273226)
    (cherry picked from commit a733c9f35f804d974d5da930539410e146fc34fa)

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -821,6 +821,7 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 		strncmp(str_governor, "performance", DEVFREQ_NAME_LEN) &&
 		strncmp(str_governor, "powersave", DEVFREQ_NAME_LEN) &&
 		strncmp(str_governor, "mem_latency", DEVFREQ_NAME_LEN) &&
+		strncmp(str_governor, "simple_ondemand", DEVFREQ_NAME_LEN) &&
 		strncmp(str_governor, "msm-adreno-tz", DEVFREQ_NAME_LEN))
 		return -EINVAL;
 
