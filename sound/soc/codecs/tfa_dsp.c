61d647932c9f861c0f4c80a57309b758c125164c
Fatih Akca <fatihakca5@gmail.com>
2017-02-20 04:55:58 +0300

    ASoC: tfa98xx: Clean up trace_printk() calls
     * Use a ftrace wrapper to get rid of the message seen below on dmesg
       (remove DEBUG flag and lower log level while at it).
    
     **********************************************************
     **   NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE   **
     **                                                      **
     ** trace_printk() being used. Allocating extra memory.  **
     **                                                      **
     ** This means that this is a DEBUG kernel and it is     **
     ** unsafe for produciton use.                           **
     **                                                      **
     ** If you see this message and you are not debugging    **
     ** the kernel, report this immediately to your vendor!  **
     **                                                      **
     **   NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE   **
     **********************************************************
    
     The usage of trace_printk() causes remarkable CPU load and memory waste
     on production systems, as described here: https://lkml.org/lkml/2014/5/28/496
    
    Change-Id: I3b6ff56df5215610890ded8e7e1f5a7a6eb9c863

--- sound/soc/codecs/tfa_dsp.c
+++ sound/soc/codecs/tfa_dsp.c
@@ -320,7 +320,7 @@ tfa_probe(unsigned char slave_address, Tfa98xx_handle_t *pHandle)
 		*pHandle = idx;
 		error = Tfa98xx_Error_Ok;
 #ifdef __KERNEL__ /* don't spam userspace with information */
-		trace_printk("slave:0x%02x revid:0x%04x\n", slave_address, rev);
+		tfa98xx_trace_printk("slave:0x%02x revid:0x%04x\n", slave_address, rev);
 		pr_debug("slave:0x%02x revid:0x%04x\n", slave_address, rev);
 #endif
 		break;
@@ -2471,7 +2471,7 @@ enum Tfa98xx_Error tfaRunSpeakerBoost(Tfa98xx_handle_t handle, int force, int pr
 	value = TFA_GET_BF(handle, ACS);
 
 #ifdef __KERNEL__ /* TODO try to combine this with the pr_debug below */
-	trace_printk("%s %sstart\n",
+	tfa98xx_trace_printk("%s %sstart\n",
 			tfaContDeviceName(handle),
 			value ? "cold" : "warm");
 #endif
