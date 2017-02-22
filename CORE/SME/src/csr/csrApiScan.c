83b8b19bf665c30c789dbe5a16e0b81e6a1b17d4
Sreelakshmi Konamki <skonam@codeaurora.org>
2017-02-22 16:03:29 +0530

    qcacld-2.0: Update MDIE when PNO match event found
    prima to qcacld-2.0 propagation
    
    Current implementation does not update MDIE while processing of
    PNO match event due to this STA is unable to connect to 11r AP.
    
    Add changes to update MDIE to 'tSirBssDescription' in
    csrScanSavePreferredNetworkFound()
    
    Change-Id: Ia419c0f7284fc168434d91bcc3d7b29b99a06f3f
    CRs-Fixed: 2005101

--- CORE/SME/src/csr/csrApiScan.c
+++ CORE/SME/src/csr/csrApiScan.c
@@ -8866,6 +8866,23 @@ eHalStatus csrScanSavePreferredNetworkFound(tpAniSirGlobal pMac,
    pBssDescr->capabilityInfo = *((tANI_U16 *)&pParsedFrame->capabilityInfo);
    vos_mem_copy((tANI_U8 *) &pBssDescr->bssId, (tANI_U8 *) macHeader->bssId, sizeof(tSirMacAddr));
    pBssDescr->nReceivedTime = vos_timer_get_system_time();
+#ifdef WLAN_FEATURE_VOWIFI_11R
+   // MobilityDomain
+   pBssDescr->mdie[0] = 0;
+   pBssDescr->mdie[1] = 0;
+   pBssDescr->mdie[2] = 0;
+   pBssDescr->mdiePresent = FALSE;
+   // If mdie is present in the probe resp we fill it in the bss description
+   if (pParsedFrame->mdiePresent) {
+       pBssDescr->mdiePresent = TRUE;
+       pBssDescr->mdie[0] = pParsedFrame->mdie[0];
+       pBssDescr->mdie[1] = pParsedFrame->mdie[1];
+       pBssDescr->mdie[2] = pParsedFrame->mdie[2];
+   }
+   smsLog(pMac, LOG1, FL("mdie=%02x%02x%02x"),
+          (unsigned int)pBssDescr->mdie[0], (unsigned int)pBssDescr->mdie[1],
+          (unsigned int)pBssDescr->mdie[2]);
+#endif
 
    smsLog( pMac, LOG2, "(%s):Bssid= "MAC_ADDRESS_STR
                        " chan= %d, rssi = %d", __func__,
