960895ad7e04c315a2e048cec53a6173b2861188
Sandeep Puligilla <spuligil@codeaurora.org>
2017-03-12 23:50:14 -0700

    Release 4.0.11.205
    Release 4.0.11.205
    
    Change-Id: I321d428da1631cb009e6e2e030ebc5c6666812ff
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -41,10 +41,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            4
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            11
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            204
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            205
 
-#define QWLAN_VERSIONSTR               "4.0.11.204Z"
+#define QWLAN_VERSIONSTR               "4.0.11.205"
 
 
 #define AR6320_REV1_VERSION             0x5000000
