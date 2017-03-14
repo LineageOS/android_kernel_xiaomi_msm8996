370a73dabbf3128863de8ac04150c95bef192ddd
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-03-13 17:29:35 -0700

    qcacld-2.0: Fix memory leak issue
    Memory should be allocated to radar_event only when radar phy
    event needs to be sent to WMA layer.
    
    Add changes to fix memory leak.
    
    CRs-Fixed: 1065466
    Bug: 34514463
    Change-Id: Ia3e93ddd47913956c27487472b6a70eb68d63fd9
    Signed-off-by: Ecco Park <eccopark@google.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -42075,7 +42075,7 @@ int wma_dfs_indicate_radar(struct ieee80211com *ic,
 	    ( pmac->sap.SapDfsInfo.disable_dfs_ch_switch == VOS_TRUE) )
 	{
 		radar_event = (struct wma_dfs_radar_indication *)
-			vos_mem_malloc(sizeof(*radar_event));
+			vos_mem_malloc(sizeof(struct wma_dfs_radar_indication));
 		if (radar_event == NULL) {
 			WMA_LOGE(FL("Failed to allocate memory for radar_event"));
 			adf_os_spin_unlock_bh(&ic->chan_lock);
