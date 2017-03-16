5f1a0c126e0e80e913e6a3d0e3e4d69e525fad56
Ferry Zhou <tianguiz@codeaurora.org>
2017-03-16 14:39:21 +0800

    qcacld-2.0: Fix data loss of def_tx_param
    The copy_sir_ocb_config() did not allocate and copy the def_tx_param
    from SME layer. So, the memory data of def_tx_param was lost.
    
    Fix it by allocating and copying the memory of def_tx_param.
    
    Change-Id: I013366db4bb722010940ccb249b24de28d88f0d9
    CRs-Fixed: 2046334

--- CORE/SERVICES/WMA/wma_ocb.c
+++ CORE/SERVICES/WMA/wma_ocb.c
@@ -132,7 +132,8 @@ static struct sir_ocb_config *copy_sir_ocb_config(struct sir_ocb_config *src)
 		src->channel_count * sizeof(*src->channels) +
 		src->schedule_size * sizeof(*src->schedule) +
 		src->dcc_ndl_chan_list_len +
-		src->dcc_ndl_active_state_list_len;
+		src->dcc_ndl_active_state_list_len +
+		src->def_tx_param_size;
 
 	dst = vos_mem_malloc(length);
 	if (!dst)
@@ -159,6 +160,13 @@ static struct sir_ocb_config *copy_sir_ocb_config(struct sir_ocb_config *src)
 	vos_mem_copy(dst->dcc_ndl_active_state_list,
 		     src->dcc_ndl_active_state_list,
 		     src->dcc_ndl_active_state_list_len);
+
+	if (src->def_tx_param && src->def_tx_param_size) {
+		dst->def_tx_param = cursor;
+		vos_mem_copy(dst->def_tx_param, src->def_tx_param,
+			     src->def_tx_param_size);
+	}
+
 	return dst;
 }
 
