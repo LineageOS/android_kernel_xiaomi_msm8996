68dd0a5eb8ad5468213e797976a47aac0aae47e5
Luca Stefani <luca020400@lineageos.org>
2017-04-27 14:07:47 +0200

    arch: arm64: Import bootinfo driver and setup changes to use it
    Change-Id: Ie69782c0fa3f24dd6d364f45a939ffafb709bb3c

--- include/linux/qpnp/power-on.h
+++ include/linux/qpnp/power-on.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -58,6 +59,8 @@ enum pon_restart_reason {
 	PON_RESTART_REASON_DMVERITY_CORRUPTED	= 0x04,
 	PON_RESTART_REASON_DMVERITY_ENFORCE	= 0x05,
 	PON_RESTART_REASON_KEYS_CLEAR		= 0x06,
+	PON_RESTART_REASON_NORMAL		= 0x20,
+	PON_RESTART_REASON_PANIC		= 0x21,
 };
 
 #ifdef CONFIG_QPNP_POWER_ON
@@ -66,6 +69,8 @@ int qpnp_pon_is_warm_reset(void);
 int qpnp_pon_trigger_config(enum pon_trigger_source pon_src, bool enable);
 int qpnp_pon_wd_config(bool enable);
 int qpnp_pon_set_restart_reason(enum pon_restart_reason reason);
+int qpnp_pon_is_lpk(void);
+int qpnp_pon_is_ps_hold_reset(void);
 bool qpnp_pon_check_hard_reset_stored(void);
 
 #else
@@ -91,6 +96,8 @@ static inline bool qpnp_pon_check_hard_reset_stored(void)
 {
 	return false;
 }
+static inline int qpnp_pon_is_lpk(void) { return -ENODEV; }
+static inline int qpnp_pon_is_ps_hold_reset(void) { return -ENODEV; }
 #endif
 
 #endif
