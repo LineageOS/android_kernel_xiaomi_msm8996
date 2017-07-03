bb789a78c4001ebeca95d4e67ff72fc97b7e9fba
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-07-03 13:29:04 +0530

    cpufreq: cpufreq_stats: constify attribute_group structures
    attribute_groups are not supposed to change at runtime. All functions
    working with attribute_groups provided by <linux/sysfs.h> work with const
    attribute_group. So mark the non-const structs as const.
    
    File size before:
       text	   data	    bss	    dec	    hex	filename
       1655	    256	      4	   1915	    77b	drivers/cpufreq/cpufreq_stats.o
    
    File size After adding 'const':
       text	   data	    bss	    dec	    hex	filename
       1695	    192	      4	   1891	    763	drivers/cpufreq/cpufreq_stats.o
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -486,7 +486,7 @@ static struct attribute *default_attrs[] = {
 #endif
 	NULL
 };
-static struct attribute_group stats_attr_group = {
+static const struct attribute_group stats_attr_group = {
 	.attrs = default_attrs,
 	.name = "stats"
 };
