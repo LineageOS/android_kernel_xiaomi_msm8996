a3182a65fd15de98d5739f289dd65ccb410b982e
Andrea Arcangeli <andrea@cpushare.com>
2017-07-25 22:22:45 +0200

    fs/exec: fix use after free in execve
    "file" can be already freed if bprm->file is NULL after
    exec_binprm() return. binfmt_script will do exactly that for
    example. If the VM reuses the file after fput run(), this will result in
    a use ater free.
    
    So obtain d_is_su before exec_binprm() runs.
    
    This should explain this crash:
    
    [25333.009554] Unable to handle kernel NULL pointer dereference at virtual address 00000185
    [..]
    [25333.009918] [2:             am:21861] PC is at do_execve+0x354/0x474
    
    Change-Id: I2a8a814d1c0aa75625be83cb30432cf13f1a0681
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- fs/exec.c
+++ fs/exec.c
@@ -1478,6 +1478,7 @@ static int do_execve_common(struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+	bool is_su;
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1553,11 +1554,14 @@ static int do_execve_common(struct filename *filename,
 	if (retval < 0)
 		goto out;
 
+	/* exec_binprm can release file and it may be freed */
+	is_su = d_is_su(file->f_dentry);
+
 	retval = exec_binprm(bprm);
 	if (retval < 0)
 		goto out;
 
-	if (d_is_su(file->f_dentry) && capable(CAP_SYS_ADMIN)) {
+	if (is_su && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
 	}
