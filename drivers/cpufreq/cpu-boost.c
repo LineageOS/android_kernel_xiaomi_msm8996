15725708d7ad4ee957c5737096466fc447f2743e
Francisco Franco <franciscofranco.1990@gmail.com>
2017-08-16 04:33:50 +0000

    cpufreq: cpu-boost: don't boost the gold cluster unecessarily
    The gold cluster is power hungry, we shouldn't abuse it. There's
    no real need to boost it on input touches if there's absolutely no
    jobs running on those cores. If mid "boost" there's a job that migrates
    to the gold cluster the governor can take care of it.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -23,6 +23,8 @@
 #include <linux/input.h>
 #include <linux/time.h>
 
+#include "../../kernel/sched/sched.h"
+
 struct cpu_sync {
 	int cpu;
 	unsigned int input_boost_min;
@@ -207,6 +209,17 @@ static void do_input_boost(struct work_struct *work)
 	pr_debug("Setting input boost min for all CPUs\n");
 	for_each_possible_cpu(i) {
 		i_sync_info = &per_cpu(sync_info, i);
+
+		// cpu 0-1 -> silver cluster
+		// cpu 2-3 -> gold cluster
+		// to save power there's no point in boosting the
+		// gold cluster core if it doesn't have any runnable
+		// thread at this point in time
+		// since inputs are fairly common we might save some
+		// juice in the long run
+		if (i >= 2 && cpu_rq(i)->nr_running == 0)
+			continue;
+
 		i_sync_info->input_boost_min = i_sync_info->input_boost_freq;
 	}
 
