0484150dd17f12d9f6f371b15853256636075681
xNombre <kartapolska@gmail.com>
2017-11-05 16:04:43 +0100

    msm_thermal: add workaround for not initialized Freq Mitig Task

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -202,6 +202,8 @@ static LIST_HEAD(devices_list);
 static LIST_HEAD(thresholds_list);
 static int mitigation = 1;
 
+static void freq_mitigation_reinit(void);
+
 enum thermal_threshold {
 	HOTPLUG_THRESHOLD_HIGH,
 	HOTPLUG_THRESHOLD_LOW,
@@ -645,8 +647,10 @@ static void msm_thermal_update_freq(bool is_shutdown, bool mitigate)
 
 	if (freq_mitigation_task)
 		complete(&freq_mitigation_complete);
-	else
+	else {
 		pr_err("Freq mitigation task is not initialized\n");
+		freq_mitigation_reinit();
+	}
 notify_exit:
 	return;
 }
@@ -778,6 +782,7 @@ static int devmgr_client_cpufreq_update(struct device_manager_data *dev_mgr)
 		complete(&freq_mitigation_complete);
 	} else {
 		pr_err("Frequency mitigation task is not initialized\n");
+		freq_mitigation_reinit();
 		ret = -ESRCH;
 	}
 
@@ -3903,6 +3908,13 @@ init_freq_thread:
 	}
 }
 
+static void freq_mitigation_reinit(void)
+{
+	pr_warn("Trying to reinitialize Frequency mitigation task...\n");
+	pr_warn("Dumping mitg parameters: %d , %d , %d .\n", msm_thermal_info.freq_mitig_temp_degc, msm_thermal_info.freq_mitig_temp_hysteresis_degc, msm_thermal_info.freq_limit);
+ 	freq_mitigation_init();
+}
+
 int msm_thermal_get_freq_plan_size(uint32_t cluster, unsigned int *table_len)
 {
 	uint32_t i = 0;
@@ -4082,6 +4094,7 @@ int msm_thermal_set_cluster_freq(uint32_t cluster, uint32_t freq, bool is_max)
 			complete(&freq_mitigation_complete);
 	} else {
 		pr_err("Frequency mitigation task is not initialized\n");
+		freq_mitigation_reinit();
 		return -ESRCH;
 	}
 
@@ -4122,6 +4135,7 @@ int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq, bool is_max)
 	} else {
 		pr_err("Frequency mitigation task is not initialized\n");
 		ret = -ESRCH;
+		freq_mitigation_reinit();
 		goto set_freq_exit;
 	}
 
@@ -5851,8 +5865,10 @@ static void thermal_cpu_freq_mit_disable(void)
 	}
 	if (freq_mitigation_task)
 		complete(&freq_mitigation_complete);
-	else
+	else {
 		pr_err("Freq mit task is not initialized\n");
+		freq_mitigation_reinit();
+	}
 }
 
 static void thermal_cpu_hotplug_mit_disable(void)
