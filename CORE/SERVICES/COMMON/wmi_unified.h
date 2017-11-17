4fc37fcc6ac84ae00267defdd122ba44c96bc5e2
tinlin <tinlin@codeaurora.org>
2017-11-17 18:36:01 +0800

    qcacld-2.0: CL 3133394 - Update fw common interface files
    Add WMI_PDEV_PARAM_ABG_MODE_TX_CHAIN_NUM. The Location of
    WMI_CTS_CBW is different from the one of firmware.
    
    Change-Id: I65cd4250fb399df8ad6f0de6c4608f6f78d91422
    CRs-fixed: 865207

--- CORE/SERVICES/COMMON/wmi_unified.h
+++ CORE/SERVICES/COMMON/wmi_unified.h
@@ -4369,7 +4369,11 @@ typedef enum {
      *  units are microseconds
      */
     WMI_PDEV_PARAM_ACK_TIMEOUT,
-
+    /** Number of TX chains to use for a/b/g rates.
+     *  bit 0~15  : 11b mode TX chain number.
+     *  bit 16~31 : 11ag mode TX chain number.
+     */
+    WMI_PDEV_PARAM_ABG_MODE_TX_CHAIN_NUM,
 } WMI_PDEV_PARAM;
 
 typedef enum {
@@ -4390,6 +4394,21 @@ typedef enum {
     /** Enable Disable debug */
     WMI_DBGLOG_REPORT_ENABLE
 } WMI_DBG_PARAM;
+
+/** MACRO define to set / get 11b and 11ag mode TX chain number:
+ *  bit 0~15 : 11b mode TX chain number.
+ *  bit 16~31: 11ag mode TX chain number.
+ */
+#define WMI_PDEV_PARAM_11B_TX_CHAIN_NUM_S  0
+#define WMI_PDEV_PARAM_11B_TX_CHAIN_NUM    0x0000FFFF
+#define WMI_PDEV_PARAM_11AG_TX_CHAIN_NUM_S 16
+#define WMI_PDEV_PARAM_11AG_TX_CHAIN_NUM   0xFFFF0000
+
+#define WMI_PDEV_PARAM_GET_11B_TX_CHAIN_NUM(word32) WMI_F_MS(word32, WMI_PDEV_PARAM_11B_TX_CHAIN_NUM)
+#define WMI_PDEV_PARAM_SET_11B_TX_CHAIN_NUM(word32, value)  WMI_F_RMW(word32,value,WMI_PDEV_PARAM_11B_TX_CHAIN_NUM)
+
+#define WMI_PDEV_PARAM_GET_11AG_TX_CHAIN_NUM(word32) WMI_F_MS(word32, WMI_PDEV_PARAM_11AG_TX_CHAIN_NUM)
+#define WMI_PDEV_PARAM_SET_11AG_TX_CHAIN_NUM(word32, value) WMI_F_RMW(word32,value,WMI_PDEV_PARAM_11AG_TX_CHAIN_NUM)
 
 /* param_value for param_id WMI_PDEV_PARAM_CTS_CBW */
 typedef enum {
