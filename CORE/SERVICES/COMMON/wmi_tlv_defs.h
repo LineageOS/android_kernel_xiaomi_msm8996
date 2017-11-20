f12d90ae9ff1c97e76553e44d8eb33a5cc3d45a3
tinlin <tinlin@codeaurora.org>
2017-11-20 18:26:32 +0800

    qcacld-2.0: CL 3174922 - Update fw common interface files
    Add WMI_VDEV_LIMIT_OFFCHAN_CMDID. WMI_PDEV_BSS_CHAN_INFO_REQUEST_CMDID
    & EVENTID of 3094881 is not propagated. Fix them here.
    
    Change-Id: I572ee48d6582063c98a09c2dbd9a33026ab61681
    CRs-fixed: 865207

--- CORE/SERVICES/COMMON/wmi_tlv_defs.h
+++ CORE/SERVICES/COMMON/wmi_tlv_defs.h
@@ -830,6 +830,7 @@ typedef enum {
     WMITLV_TAG_STRUC_wmi_pdev_bss_chan_info_request_fixed_param,
     WMITLV_TAG_STRUC_wmi_pdev_bss_chan_info_event_fixed_param,
     WMITLV_TAG_STRUC_wmi_roam_lca_disallow_config_tlv_param,
+    WMITLV_TAG_STRUC_wmi_vdev_limit_offchan_cmd_fixed_param,
 } WMITLV_TAG_ID;
 
 /*
@@ -1162,6 +1163,9 @@ typedef enum {
     OP(WMI_SET_SCAN_DBS_DUTY_CYCLE_CMDID) \
     OP(WMI_PDEV_DIV_GET_RSSI_ANTID_CMDID) \
     OP(WMI_THERM_THROT_SET_CONF_CMDID) \
+    OP(WMI_OEM_DMA_RING_CFG_REQ_CMDID) \
+    OP(WMI_PDEV_BSS_CHAN_INFO_REQUEST_CMDID) \
+    OP(WMI_VDEV_LIMIT_OFFCHAN_CMDID) \
     /* add new CMD_LIST elements above this line */
 
 /*
@@ -1343,6 +1347,9 @@ typedef enum {
     OP(WMI_OFFCHAN_DATA_TX_COMPLETION_EVENTID) \
     OP(WMI_THERM_THROT_STATS_EVENTID) \
     OP(WMI_PDEV_DIV_RSSI_ANTID_EVENTID) \
+    OP(WMI_OEM_DMA_RING_CFG_RSP_EVENTID) \
+    OP(WMI_OEM_DMA_BUF_RELEASE_EVENTID) \
+    OP(WMI_PDEV_BSS_CHAN_INFO_EVENTID) \
     /* add new EVT_LIST elements above this line */
 
 
@@ -2166,10 +2173,21 @@ WMITLV_CREATE_PARAM_STRUC(WMI_PDEV_GET_TPC_CONFIG_CMDID);
 
 WMITLV_CREATE_PARAM_STRUC(WMI_PDEV_DIV_GET_RSSI_ANTID_CMDID);
 
+#define WMITLV_TABLE_WMI_PDEV_BSS_CHAN_INFO_REQUEST_CMDID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_pdev_bss_chan_info_request_fixed_param,  wmi_pdev_bss_chan_info_request_fixed_param, fixed_param, WMITLV_SIZE_FIX)
+
+WMITLV_CREATE_PARAM_STRUC(WMI_PDEV_BSS_CHAN_INFO_REQUEST_CMDID);
+
 /* VDEV Get Tx power Cmd */
 #define WMITLV_TABLE_WMI_VDEV_GET_TX_POWER_CMDID(id,op,buf,len) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_vdev_get_tx_power_cmd_fixed_param, wmi_vdev_get_tx_power_cmd_fixed_param, fixed_param, WMITLV_SIZE_FIX)
 WMITLV_CREATE_PARAM_STRUC(WMI_VDEV_GET_TX_POWER_CMDID);
+
+/* Limit Offchannel duration Cmd */
+#define WMITLV_TABLE_WMI_VDEV_LIMIT_OFFCHAN_CMDID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_vdev_limit_offchan_cmd_fixed_param, wmi_vdev_limit_offchan_cmd_fixed_param, fixed_param, WMITLV_SIZE_FIX)
+
+WMITLV_CREATE_PARAM_STRUC(WMI_VDEV_LIMIT_OFFCHAN_CMDID);
 
 /* PDEV Set Base Mac Address Cmd */
 #define WMITLV_TABLE_WMI_PDEV_SET_BASE_MACADDR_CMDID(id,op,buf,len) \
@@ -3440,6 +3458,11 @@ WMITLV_CREATE_PARAM_STRUC(WMI_PDEV_TPC_CONFIG_EVENTID);
 #define WMITLV_TABLE_WMI_PDEV_DIV_RSSI_ANTID_EVENTID(id,op,buf,len) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_pdev_div_rssi_antid_event_fixed_param, wmi_pdev_div_rssi_antid_event_fixed_param, fixed_param, WMITLV_SIZE_FIX)
 WMITLV_CREATE_PARAM_STRUC(WMI_PDEV_DIV_RSSI_ANTID_EVENTID);
+
+/* PDEV BSS CHAN info Event */
+#define WMITLV_TABLE_WMI_PDEV_BSS_CHAN_INFO_EVENTID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_pdev_bss_chan_info_event_fixed_param, wmi_pdev_bss_chan_info_event_fixed_param, fixed_param, WMITLV_SIZE_FIX)
+WMITLV_CREATE_PARAM_STRUC(WMI_PDEV_BSS_CHAN_INFO_EVENTID);
 
 /* VDEV Tx Power Event */
 #define WMITLV_TABLE_WMI_VDEV_GET_TX_POWER_EVENTID(id,op,buf,len) \
