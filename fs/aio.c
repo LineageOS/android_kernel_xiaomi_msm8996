0a8f0cd4b2cd64c9544c7518eba34cedcf7fa39d
jonascardoso <jonascard60@gmail.com>
2017-11-23 16:13:57 -0200

    fs: Async I/O latency to a ssd greatly increased, 34% gain (Creds: kleikamp)
    (cherry picked from commit 0a7f121f2151f2f73c0cb2c9a28f1cdce2f8e90d)

--- fs/aio.c
+++ fs/aio.c
@@ -1561,7 +1561,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
-	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1578,7 +1577,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		return -EINVAL;
 	}
 
-	blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1602,8 +1600,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
-
 	percpu_ref_put(&ctx->users);
 	return i ? i : ret;
 }
