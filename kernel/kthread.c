513615b034e48f294ba7ce4e6e74e4a219ce1d2e
Sultanxda <sultanxda@gmail.com>
2017-12-18 00:02:12 -0800

    kthread: Allow kthread_bind() to actually control kthread affinity
    If something is calling kthread_bind(), it's probably got a good reason
    to bind a kthread to a CPU.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -336,6 +336,7 @@ static void __kthread_bind_mask(struct task_struct *p, const struct cpumask *mas
 
 	/* It's safe because the task is inactive. */
 	raw_spin_lock_irqsave(&p->pi_lock, flags);
+	p->kthread_per_cpu = true;
 	do_set_cpus_allowed(p, mask);
 	p->flags |= PF_NO_SETAFFINITY;
 	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
