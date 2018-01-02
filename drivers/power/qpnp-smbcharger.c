f78f19efd7be74da8e8d194b3dc28c1205b382d0
LuK1337 <priv.luk@gmail.com>
2018-01-02 15:27:11 +0000

    qpnp-smbcharger: Bring in select fixes from Xiaomi
    This basically fixes 2 issues:
    
      1. /sys/class/power_supply/battery/current_now not being updated
         unless device is booted with USB cable connected.
      2. Wrong power supply technology being reported (Li-ion instead of Li-poly)
    
    Change-Id: Ie55a56943970313b7d6680f8f6c46bc838d40ab6

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -6176,6 +6176,7 @@ static int smbchg_battery_set_property(struct power_supply *psy,
 		rc = vote(chip->dc_suspend_votable, USER_EN_VOTER,
 				!val->intval, 0);
 		chip->chg_enabled = val->intval;
+		power_supply_changed(chip->usb_psy);
 		schedule_work(&chip->usb_set_online_work);
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
@@ -6309,7 +6310,7 @@ static int smbchg_battery_get_property(struct power_supply *psy,
 		val->intval = get_prop_batt_health(chip);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
-		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
 		break;
 	case POWER_SUPPLY_PROP_FLASH_CURRENT_MAX:
 		val->intval = smbchg_calc_max_flash_current(chip);
