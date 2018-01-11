1e672e8cc03096768f33b046be40450936235023
tinlin <tinlin@codeaurora.org>
2018-01-11 16:24:14 +0800

    qcacld-2.0: Add data_len check to avoid OOB access
    Add data_len check in wma_stats_ext_event_handler()
    to avoid OOB access.
    
    Change-Id: I756ec66fee1cf937f144441a051e973cd561909d
    CRs-Fixed: 2170655

--- CORE/SERVICES/WMA/wma.c
+++ CORE/SERVICES/WMA/wma.c
@@ -8662,7 +8662,8 @@ static int wma_stats_ext_event_handler(void *handle, u_int8_t *event_buf,
 	alloc_len += stats_ext_info->data_len;
 
 	if (stats_ext_info->data_len > (WMA_SVC_MSG_MAX_SIZE -
-	    sizeof(*stats_ext_info))) {
+	    sizeof(*stats_ext_info)) ||
+	    stats_ext_info->data_len > param_buf->num_data) {
 		WMA_LOGE("Excess data_len:%d", stats_ext_info->data_len);
 		VOS_ASSERT(0);
 		return -EINVAL;
