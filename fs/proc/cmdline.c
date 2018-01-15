a950731a47616ac5a95db933e1e015302fabf192
Jonas Cardoso <jonascard60@gmail.com>
2018-01-15 10:48:52 -0200

    cmdline: Safetynet

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,12 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
 
+static char new_command_line[COMMAND_LINE_SIZE];
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -21,9 +23,57 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void remove_flag(char *cmd, const char *flag)
+{
+	char *start_addr, *end_addr;
+
+	/* Ensure all instances of a flag are removed */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memmove(start_addr, end_addr + 1, strlen(end_addr));
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void remove_safetynet_flags(char *cmd)
+{
+	remove_flag(cmd, "androidboot.enable_dm_verity=");
+	remove_flag(cmd, "androidboot.secboot=");
+	remove_flag(cmd, "androidboot.verifiedbootstate=");
+	remove_flag(cmd, "androidboot.veritymode=");
+}
+
 static int __init proc_cmdline_init(void)
 {
+	char *offset_addr, *cmd = new_command_line;
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Remove various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
+	 */
+	remove_safetynet_flags(new_command_line);
+
+   	offset_addr = strstr(cmd, "androidboot.veritymode=");
+        if (offset_addr) {
+               size_t i, len, offset;
+
+                len = strlen(cmd);
+                offset = offset_addr - cmd;
+
+                for (i = 1; i < (len - offset); i++) {
+                        if (cmd[offset + i] == ' ')
+                                break;
+                }
+
+                memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+        }
+ 
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
 fs_initcall(proc_cmdline_init);
+
