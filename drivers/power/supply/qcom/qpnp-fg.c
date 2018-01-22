29171204671147f5b04f9b67a7b4611596107ecd
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-01-22 19:06:38 +0100

    power: qpnp-fg: Increase cutoff voltage
    Change-Id: Ib70256e70a849c713031792aaded208aed52456a

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -249,7 +249,7 @@ static struct fg_mem_setting settings[FG_MEM_SETTING_MAX] = {
 	SETTING(TERM_CURRENT,	 0x40C,   2,      250),
 	SETTING(CHG_TERM_CURRENT, 0x4F8,   2,      250),
 	SETTING(IRQ_VOLT_EMPTY,	 0x458,   3,      3100),
-	SETTING(CUTOFF_VOLTAGE,	 0x40C,   0,      3200),
+	SETTING(CUTOFF_VOLTAGE,	 0x40C,   0,      3400),
 	SETTING(VBAT_EST_DIFF,	 0x000,   0,      30),
 	SETTING(DELTA_SOC,	 0x450,   3,      1),
 	SETTING(BATT_LOW,	 0x458,   0,      4200),
