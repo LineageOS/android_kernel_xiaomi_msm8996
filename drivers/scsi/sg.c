ddcd21e19feb1438d6f856f2ce9942df9abfde84
Nathan Chancellor <natechancellor@gmail.com>
2018-01-29 21:48:14 -0700

    scsi: sg: Fix botched CAF conflict resolution
    CAF completely screwed up the application of commit de93dd11b78f
    ("Prevent potential double frees in sg driver"), failing to add
    two mutex_lock statements and adding an unnecessary seq_printf.
    
    Compare with https://android.googlesource.com/kernel/msm/+/22d8e80738b5ce8784d59b48b0b051a520da4bec
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -924,6 +924,7 @@ sg_ioctl(struct file *filp, unsigned int cmd_in, unsigned long arg)
 			sfp->low_dma = 1;
 			if ((0 == sfp->low_dma) && !sfp->res_in_use) {
 				val = (int) sfp->reserve.bufflen;
+				mutex_lock(&sfp->parentdp->open_rel_lock);
 				sg_remove_scat(sfp, &sfp->reserve);
 				sg_build_reserve(sfp, val);
 				mutex_unlock(&sfp->parentdp->open_rel_lock);
@@ -1006,6 +1007,7 @@ sg_ioctl(struct file *filp, unsigned int cmd_in, unsigned long arg)
 				return -EBUSY;
 			}
 
+			mutex_lock(&sfp->parentdp->open_rel_lock);
 			sg_remove_scat(sfp, &sfp->reserve);
 			sg_build_reserve(sfp, val);
 			mutex_unlock(&sfp->parentdp->open_rel_lock);
@@ -2709,9 +2711,6 @@ static void sg_proc_debug_helper(struct seq_file *s, Sg_device * sdp)
 			seq_puts(s, srp->done ?
 				 ((1 == srp->done) ?  "rcv:" : "fin:")
 				  : "act:");
-			seq_printf(s, srp->done ?
-				   ((1 == srp->done) ?  "rcv:" : "fin:")
-				   : "act:");
 			seq_printf(s, " id=%d blen=%d",
 				   srp->header.pack_id, blen);
 			if (srp->done)
