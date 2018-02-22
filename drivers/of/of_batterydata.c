bb02cae0313b13ebaeccb4e73c0041abde09fa96
yarost12 <yaro330@gmail.com>
2018-02-22 16:07:02 +0300

    of: batterydata: Fix batterydata not being read properly
    * Partial cherry-pick of https://github.com/YaroST12/Z2_PLUS_Kernel_EAS/commit/183c40ac661a93077dbf3eb491f4d955c99a1b18
    
    Signed-off-by: yarost12 <yaro330@gmail.com>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -21,6 +21,8 @@
 #include <linux/batterydata-lib.h>
 #include <linux/power_supply.h>
 
+#define SUPPORT_LENUK_BATTERY_ID_ALGO
+
 static int of_batterydata_read_lut(const struct device_node *np,
 			int max_cols, int max_rows, int *ncols, int *nrows,
 			int *col_legend_data, int *row_legend_data,
@@ -354,6 +356,19 @@ struct device_node *of_batterydata_get_best_profile(
 			if (rc)
 				continue;
 			for (i = 0; i < batt_ids.num; i++) {
+#ifdef SUPPORT_LENUK_BATTERY_ID_ALGO
+				if (((batt_id_kohm >= 1) && (batt_id_kohm < 20) && (batt_ids.kohm[i] == 9))
+						||  ((batt_id_kohm >= 20) && (batt_id_kohm < 80) && (batt_ids.kohm[i] == 50))
+						||  ((batt_id_kohm >= 80) && (batt_id_kohm < 120) && (batt_ids.kohm[i] == 100))) {
+					best_node = node;
+					best_id_kohm = batt_ids.kohm[i];
+					in_range = false;
+					limit = 0;
+					best_delta = 0;
+					delta = 0;
+					break;
+				}
+#else
 				delta = abs(batt_ids.kohm[i] - batt_id_kohm);
 				limit = (batt_ids.kohm[i] * id_range_pct) / 100;
 				in_range = (delta <= limit);
@@ -368,6 +383,7 @@ struct device_node *of_batterydata_get_best_profile(
 					best_delta = delta;
 					best_id_kohm = batt_ids.kohm[i];
 				}
+#endif
 			}
 		}
 		rc = of_property_read_string(node, "qcom,battery-type",	&battery_type);
@@ -384,6 +400,10 @@ struct device_node *of_batterydata_get_best_profile(
 		return best_node;
 	}
 
+#ifdef SUPPORT_LENUK_BATTERY_ID_ALGO
+	pr_info("profile id %d batt id %d",
+		best_id_kohm, batt_id_kohm);
+#else
 	/* check that profile id is in range of the measured batt_id */
 	if (abs(best_id_kohm - batt_id_kohm) >
 			((best_id_kohm * id_range_pct) / 100)) {
@@ -391,6 +411,7 @@ struct device_node *of_batterydata_get_best_profile(
 			best_id_kohm, batt_id_kohm, id_range_pct);
 		return NULL;
 	}
+#endif
 
 	rc = of_property_read_string(best_node, "qcom,battery-type",
 							&battery_type);
@@ -434,15 +455,30 @@ int of_batterydata_read_data(struct device_node *batterydata_container_node,
 		if (rc)
 			continue;
 		for (i = 0; i < batt_ids.num; i++) {
+#ifdef SUPPORT_LENUK_BATTERY_ID_ALGO
+			if (((batt_id_kohm >= 1) && (batt_id_kohm < 20) && (batt_ids.kohm[i] == 9))
+					||  ((batt_id_kohm >= 20) && (batt_id_kohm < 80) && (batt_ids.kohm[i] == 50))
+					||  ((batt_id_kohm >= 80) && (batt_id_kohm < 120) && (batt_ids.kohm[i] == 100))) {
+				best_node = node;
+				best_id_kohm = batt_ids.kohm[i];
+				delta = 0;
+				break;
+			}
+#else
 			delta = abs(batt_ids.kohm[i] - batt_id_kohm);
 			if (delta < best_delta || !best_node) {
 				best_node = node;
 				best_delta = delta;
 				best_id_kohm = batt_ids.kohm[i];
 			}
+#endif
 		}
 	}
 
+#ifdef SUPPORT_LENUK_BATTERY_ID_ALGO
+	pr_info("profile id %d batt id %d",
+		best_id_kohm, batt_id_kohm);
+#endif
 	if (best_node == NULL) {
 		pr_err("No battery data found\n");
 		return -ENODATA;
