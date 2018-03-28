a60552f948a4e5c5d41dca1b371d40e26af891be
Paul Keith <javelinanddart@gmail.com>
2018-03-28 19:52:29 +0200

    fs: sdfat: Add MODULE_ALIAS_FS for supported filesystems
    * This is the proper thing to do for filesystem drivers
    
    Change-Id: I109b201d85e324cc0a72c3fcd09df4a3e1703042
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- sdfat.c
+++ sdfat.c
@@ -5087,6 +5087,7 @@ static struct file_system_type sdfat_fs_type = {
 #endif /* CONFIG_SDFAT_DBG_IOCTL */
 	.fs_flags    = FS_REQUIRES_DEV,
 };
+MODULE_ALIAS_FS("sdfat");
 
 #ifdef CONFIG_SDFAT_USE_FOR_EXFAT
 static struct file_system_type exfat_fs_type = {
@@ -5100,6 +5101,7 @@ static struct file_system_type exfat_fs_type = {
 #endif /* CONFIG_SDFAT_DBG_IOCTL */
 	.fs_flags    = FS_REQUIRES_DEV,
 };
+MODULE_ALIAS_FS("exfat");
 #endif /* CONFIG_SDFAT_USE_FOR_EXFAT */
 
 #ifdef CONFIG_SDFAT_USE_FOR_VFAT
@@ -5114,6 +5116,7 @@ static struct file_system_type vfat_fs_type = {
 #endif /* CONFIG_SDFAT_DBG_IOCTL */
 	.fs_flags    = FS_REQUIRES_DEV,
 };
+MODULE_ALIAS_FS("vfat");
 #endif /* CONFIG_SDFAT_USE_FOR_VFAT */
 
 static int __init init_sdfat_fs(void)
