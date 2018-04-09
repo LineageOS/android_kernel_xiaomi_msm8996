a00352b3b77e1984556e3135f131a479106b4706
Bruno Martins <bgcngm@gmail.com>
2018-04-09 15:43:58 +0200

    power: qpnp-smbcharger: Reduce QC charging current
     * These are the charging current values that Xiaomi sets for MIUI,
       at least based on the latest source code available.
       The fact is that users have been complaining about excessive
       thermal throttling while the device is connected to the charger.
    
    Change-Id: I9b45279794b5dce52a67a380950e560904730a6d

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -439,13 +439,13 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_hvdcp_icl_ma = 1800;
+static int smbchg_default_hvdcp_icl_ma = 1500;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_hvdcp3_icl_ma = 3000;
+static int smbchg_default_hvdcp3_icl_ma = 2700;
 module_param_named(
 	default_hvdcp3_icl_ma, smbchg_default_hvdcp3_icl_ma,
 	int, S_IRUSR | S_IWUSR
