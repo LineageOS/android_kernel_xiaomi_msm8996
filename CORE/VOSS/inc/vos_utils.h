7d6d2e91e6e365025fcfa0cbbc9f893009565ccd
lifeng <lifeng@codeaurora.org>
2018-04-23 16:26:22 +0800

    qcacld-2.0: Remove the dead code
    Remove the dead code in vos_utils.
    
    Change-Id: Ie56bfeb7587c3af865db18ece2493a3ac731fc62
    CRs-Fixed: 2230946

--- CORE/VOSS/inc/vos_utils.h
+++ CORE/VOSS/inc/vos_utils.h
@@ -132,48 +132,6 @@ VOS_STATUS vos_sha1_hmac_str(v_U32_t cryptHandle, /* Handle */
            v_U32_t keyLen, /* length of authentication key */
            v_U8_t digest[VOS_DIGEST_SHA1_SIZE]);/* caller digest to be filled in */
 
-/**
- * vos_md5_hmac_str
- *
- * FUNCTION:
- * Generate the HMAC-MD5 of a string given a key.
- *
- * LOGIC:
- * Standard HMAC processing from RFC 2104. The code is provided in the
- * appendix of the RFC.
- *
- * ASSUMPTIONS:
- * The RFC is correct.
- *
- * @param text text to be hashed
- * @param textLen length of text
- * @param key key to use for HMAC
- * @param keyLen length of key
- * @param digest holds resultant MD5 HMAC (16B)
- *
- * @return VOS_STATUS_SUCCSS if the operation succeeds
- *
- */
-VOS_STATUS vos_md5_hmac_str(v_U32_t cryptHandle, /* Handle */
-           v_U8_t *text, /* pointer to data stream */
-           v_U32_t textLen, /* length of data stream */
-           v_U8_t *key, /* pointer to authentication key */
-           v_U32_t keyLen, /* length of authentication key */
-           v_U8_t digest[VOS_DIGEST_MD5_SIZE]);/* caller digest to be filled in */
-
-
-
-VOS_STATUS vos_encrypt_AES(v_U32_t cryptHandle, /* Handle */
-                           v_U8_t *pText, /* pointer to data stream */
-                           v_U8_t *Encrypted,
-                           v_U8_t *pKey); /* pointer to authentication key */
-
-
-VOS_STATUS vos_decrypt_AES(v_U32_t cryptHandle, /* Handle */
-                           v_U8_t *pText, /* pointer to data stream */
-                           v_U8_t *pDecrypted,
-                           v_U8_t *pKey); /* pointer to authentication key */
-
 v_U32_t vos_chan_to_freq(v_U8_t chan);
 v_U8_t vos_freq_to_chan(v_U32_t freq);
 v_U8_t vos_chan_to_band(v_U32_t chan);
