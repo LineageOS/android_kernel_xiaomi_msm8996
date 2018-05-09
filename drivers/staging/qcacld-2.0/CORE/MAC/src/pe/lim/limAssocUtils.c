8ad195b866eba53fc3a6480bc0add696202aacaa
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 17:03:06 -0700

    qcacld-2.0: Fix misleading indentation warning in limStaSendAddBssPreAssoc
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limAssocUtils.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limAssocUtils.c
@@ -4474,6 +4474,7 @@ tSirRetStatus limStaSendAddBssPreAssoc( tpAniSirGlobal pMac, tANI_U8 updateEntry
 
     if(pMac->lim.gLimProtectionControl != WNI_CFG_FORCE_POLICY_PROTECTION_DISABLE)
         limDecideStaProtectionOnAssoc(pMac, pBeaconStruct, psessionEntry);
+
     vos_mem_copy(pAddBssParams->bssId, bssDescription->bssId,
                      sizeof(tSirMacAddr));
 
