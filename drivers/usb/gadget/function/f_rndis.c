625e142963ef30c27c72f6c8f400dcdf892b1b2b
jonascardoso <jonascard60@gmail.com>
2018-06-16 18:45:29 -0300

    kernel: fix errors detected by Google GCC

--- drivers/usb/gadget/function/f_rndis.c
+++ drivers/usb/gadget/function/f_rndis.c
@@ -27,7 +27,7 @@
 #include "u_ether_configfs.h"
 #include "u_rndis.h"
 #include "rndis.h"
-#include "configfs.h"
+#include "../drivers/usb/gadget/configfs.h"
 
 /*
  * This function is an RNDIS Ethernet port -- a Microsoft protocol that's
