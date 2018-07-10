bfe7842e621210a3f28c66fc4d763f66408b1fc3
Min Liu <minliu@codeaurora.org>
2018-07-10 14:00:29 +0800

    qcacld-2.0: OOB read while processing extscan change results
    Propagation from qcacld-3.0 to qcacld-2.0
    
    In function wma_extscan_change_results_event_handler(), numResults
    in dest_chglist is assigning as total_entries in the event, but the
    memory allocated to dest_chglist is based on the numap variable,
    which may cause out of buffer read in extscan indication callback
    function wlan_hdd_cfg80211_extscan_signif_wifi_change_results_ind().
    Also tSirWifiSignificantChange array parsing in both the functions
    is not efficient which may lead to accessing unallocated memory.
    To address out of buffer read, assign numap to numResults in
    dest_chglist and to address accessing of unallocated memory,
    parse tSirWifiSignificantChange array with efficient logic.
    
    Change-Id: I469405d68cf075e58aa3a17e884032882a595b18
    CRs-Fixed: 2275630

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -31866,7 +31866,9 @@ wlan_hdd_cfg80211_extscan_signif_wifi_change_results_ind(
         for (j = 0; j < ap_info->numOfRssi; j++)
             hddLog(LOG1, "Rssi %d", *rssi++);
 
-        ap_info += ap_info->numOfRssi * sizeof(*rssi);
+        ap_info = (tSirWifiSignificantChange *)((char *)ap_info +
+                    ap_info->numOfRssi * sizeof(*rssi) +
+                    sizeof(*ap_info));
     }
 
     if (nla_put_u32(skb, QCA_WLAN_VENDOR_ATTR_EXTSCAN_RESULTS_REQUEST_ID,
@@ -31909,7 +31911,9 @@ wlan_hdd_cfg80211_extscan_signif_wifi_change_results_ind(
 
               nla_nest_end(skb, ap);
 
-            ap_info += ap_info->numOfRssi * sizeof(*rssi);
+            ap_info = (tSirWifiSignificantChange *)((char *)ap_info +
+                      ap_info->numOfRssi * sizeof(*rssi) +
+                      sizeof(*ap_info));
         }
         nla_nest_end(skb, aps);
 
