f3a67ef420b456b67cb7065acedcb173f9edf537
tinlin <tinlin@codeaurora.org>
2018-07-19 15:35:04 +0530

    qcacld-2.0: Security enhancement fixes for RSNE frame parser
    Adds logic in frame parser to use default RSN IE fields if optional
    fields are not present in IE. So if RSN IE doesn't contain AKM,
    group and unicast cipher use the default AKM(AKM_IEEE8021X) and
    cipher (CCMP). Also adds sanity checks for RSNE in the frame parser.
    
    Change-Id: I120a29c21407f82aab2c6fcdcaf1095518cb71b5
    CRs-Fixed: 2283012

--- CORE/SME/inc/sme_Api.h
+++ CORE/SME/inc/sme_Api.h
@@ -4960,4 +4960,17 @@ eHalStatus sme_thermal_throttle_set_conf_cmd(tHalHandle hHal, bool enable,
                                              tANI_U32 prio);
 eHalStatus sme_thermal_throttle_mgmt_cmd(tHalHandle hHal, tANI_U16 lower_thresh_deg,
                                          tANI_U16 higher_thresh_deg);
+/**
+ * sme_unpack_rsn_ie: wrapper to unpack RSN IE and update def RSN params
+ * if optional fields are not present.
+ * @hal: handle returned by mac_open
+ * @buf: rsn ie buffer pointer
+ * @buf_len: rsn ie buffer length
+ * @rsn_ie: outframe rsn ie structure
+ * @append_ie: flag to indicate if the rsn_ie need to be appended from buf
+ *
+ * Return: parse status
+ */
+uint32_t sme_unpack_rsn_ie(tHalHandle hal, uint8_t *buf,
+                        uint8_t buf_len, tDot11fIERSN *rsn_ie);
 #endif //#if !defined( __SME_API_H )
