36fe4c100ee9638490c5ffa48b16dfe2ab7f2888
JonasCardoso <jonascard60@gmail.com>
2018-07-20 19:29:42 -0300

    arm64: defconfigs: force wlan to load

--- kernel/module.c
+++ kernel/module.c
@@ -1145,6 +1145,10 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+	/* Force wlan to load */
+	if (!strncmp("wlan", mod->name, 4))
+		return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
