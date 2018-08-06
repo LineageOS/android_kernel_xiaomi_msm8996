1f5a312d8197435d74070fad799149868eebeadb
Kai Liu <kaliu@codeaurora.org>
2018-08-06 09:01:21 +0800

    qcacld-2.0: Add mutex to keep wmi sent serialized
    Timer wma_fw_time_sync_timer will send wmi periodically, there
    is a chance that wmi is sent after driver issue suspend cmd,
    which will cause fw assert.
    
    Current flag is_target_suspended can't prevent all race condition issue,
    add wmi_mutex to let wmi send serialized.
    
    Change-Id: I780f5dd6e89859660eb50a9b7443f2cdc46236c0
    CRs-Fixed: 2288813

--- CORE/SERVICES/WMI/wmi_unified.c
+++ CORE/SERVICES/WMI/wmi_unified.c
@@ -928,6 +928,7 @@ int wmi_unified_cmd_send(wmi_unified_t wmi_handle, wmi_buf_t buf, int len,
 	void *vos_context;
 	struct ol_softc *scn;
 	A_UINT16 htc_tag = 0;
+	int ret;
 
 	if (vos_is_shutdown_in_progress(VOS_MODULE_ID_WDA, NULL)) {
 		adf_os_print("\nERROR: %s: shutdown is in progress so could not send WMI command: %d\n",
@@ -935,6 +936,9 @@ int wmi_unified_cmd_send(wmi_unified_t wmi_handle, wmi_buf_t buf, int len,
 		return -EBUSY;
 	}
 
+	vos_context = vos_get_global_context(VOS_MODULE_ID_WDA, NULL);
+	scn = vos_get_context(VOS_MODULE_ID_HIF, vos_context);
+	adf_os_mutex_acquire(scn->adf_dev, &wmi_handle->wmi_mutex);
 	if (wmi_get_runtime_pm_inprogress(wmi_handle))
 		goto skip_suspend_check;
 
@@ -944,7 +948,8 @@ int wmi_unified_cmd_send(wmi_unified_t wmi_handle, wmi_buf_t buf, int len,
 		adf_os_print("\nERROR: %s: Target is suspended  could not send WMI command: %d\n",
 				__func__, cmd_id);
 		VOS_ASSERT(0);
-		return -EBUSY;
+		ret = -EBUSY;
+		goto error;
 	} else
 		goto dont_tag;
 
@@ -961,22 +966,22 @@ dont_tag:
 		{
 			adf_os_print("\nERROR: %s: Invalid WMI Parameter Buffer for Cmd:%d\n",
 				     __func__, cmd_id);
-			return -1;
+			ret = -EINVAL;
+			goto error;
 		}
 	}
 
 	if (adf_nbuf_push_head(buf, sizeof(WMI_CMD_HDR)) == NULL) {
 		pr_err("%s, Failed to send cmd %x, no memory\n",
 		       __func__, cmd_id);
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto error;
 	}
 
 	WMI_SET_FIELD(adf_nbuf_data(buf), WMI_CMD_HDR, COMMANDID, cmd_id);
 
 	adf_os_atomic_inc(&wmi_handle->pending_cmds);
 	if (adf_os_atomic_read(&wmi_handle->pending_cmds) >= WMI_MAX_CMDS) {
-		vos_context = vos_get_global_context(VOS_MODULE_ID_WDA, NULL);
-		scn = vos_get_context(VOS_MODULE_ID_HIF, vos_context);
 		pr_err("\n%s: hostcredits = %d\n", __func__,
 		       wmi_get_host_credits(wmi_handle));
 		HTC_dump_counter_info(wmi_handle->htc_handle);
@@ -989,12 +994,15 @@ dont_tag:
 			if (vos_is_logp_in_progress(VOS_MODULE_ID_VOSS, NULL)) {
 				pr_err("%s- %d: SSR is in progress!!!!\n",
 					 __func__, __LINE__);
-				return -EBUSY;
+				ret = -EBUSY;
+				goto error;
 			}
 			vos_trigger_recovery(true);
 		} else
 			VOS_BUG(0);
-		return -EBUSY;
+
+		ret = -EBUSY;
+		goto error;
 	}
 
 	pkt = adf_os_mem_alloc(NULL, sizeof(*pkt));
@@ -1002,7 +1010,8 @@ dont_tag:
 		adf_os_atomic_dec(&wmi_handle->pending_cmds);
 		pr_err("%s, Failed to alloc htc packet %x, no memory\n",
 		       __func__, cmd_id);
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto error;
 	}
 
 	SET_HTC_PACKET_INFO_TX(pkt,
@@ -1032,8 +1041,11 @@ dont_tag:
 		pr_err("%s %d, HTCSendPkt failed\n", __func__, __LINE__);
 	}
 
-
+	adf_os_mutex_release(scn->adf_dev, &wmi_handle->wmi_mutex);
 	return ((status == A_OK) ? EOK : -1);
+error:
+	adf_os_mutex_release(scn->adf_dev, &wmi_handle->wmi_mutex);
+	return ret;
 }
 
 
