ce20db87e391285da5deec5d098d46e711688493
Sultan Alsawaf <sultan@kerneltoast.com>
2018-08-11 18:27:35 -0700

    cpu_input_boost: Destroy workqueue on error path in init
    The workqueue was not getting destroyed upon error in init; fix it.
    
    Also simplify this portion of init and remove the superfluous OOM error
    messages.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -319,36 +319,19 @@ static struct input_handler cpu_input_boost_input_handler = {
 	.id_table	= cpu_input_boost_ids
 };
 
-static struct boost_drv *alloc_boost_drv(void)
-{
-	struct boost_drv *b;
-
-	b = kzalloc(sizeof(*b), GFP_KERNEL);
-	if (!b)
-		return NULL;
-
-	b->wq = alloc_workqueue("cpu_input_boost_wq", WQ_HIGHPRI, 0);
-	if (!b->wq) {
-		pr_err("Failed to allocate workqueue\n");
-		goto free_b;
-	}
-
-	return b;
-
-free_b:
-	kfree(b);
-	return NULL;
-}
-
 static int __init cpu_input_boost_init(void)
 {
 	struct boost_drv *b;
 	int ret;
 
-	b = alloc_boost_drv();
-	if (!b) {
-		pr_err("Failed to allocate boost_drv struct\n");
+	b = kzalloc(sizeof(*b), GFP_KERNEL);
+	if (!b)
 		return -ENOMEM;
+
+	b->wq = alloc_workqueue("cpu_input_boost_wq", WQ_HIGHPRI, 0);
+	if (!b->wq) {
+		ret = -ENOMEM;
+		goto free_b;
 	}
 
 	spin_lock_init(&b->lock);
@@ -362,7 +345,7 @@ static int __init cpu_input_boost_init(void)
 	ret = cpufreq_register_notifier(&b->cpu_notif, CPUFREQ_POLICY_NOTIFIER);
 	if (ret) {
 		pr_err("Failed to register cpufreq notifier, err: %d\n", ret);
-		goto free_b;
+		goto destroy_wq;
 	}
 
 	cpu_input_boost_input_handler.private = b;
@@ -389,6 +372,8 @@ unregister_handler:
 	input_unregister_handler(&cpu_input_boost_input_handler);
 unregister_cpu_notif:
 	cpufreq_unregister_notifier(&b->cpu_notif, CPUFREQ_POLICY_NOTIFIER);
+destroy_wq:
+	destroy_workqueue(b->wq);
 free_b:
 	kfree(b);
 	return ret;
