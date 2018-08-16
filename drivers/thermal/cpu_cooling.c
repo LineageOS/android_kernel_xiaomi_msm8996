1980ef0aa23eddddf75c26d5a2591c279adbbd9a
JonasCardoso <jonascard60@gmail.com>
2018-08-15 21:47:36 -0300

    Revert "thermal: cpu_cooling: Avoid accessing potentially freed structures"
    This reverts commit 77fc8e1fba4c7bb446f298c246c61aa241ca8848.

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -188,10 +188,8 @@ unsigned long cpufreq_cooling_get_level(unsigned int cpu, unsigned int freq)
 	mutex_lock(&cooling_cpufreq_lock);
 	list_for_each_entry(cpufreq_dev, &cpufreq_dev_list, node) {
 		if (cpumask_test_cpu(cpu, &cpufreq_dev->allowed_cpus)) {
-			unsigned long level = get_level(cpufreq_dev, freq);
-
 			mutex_unlock(&cooling_cpufreq_lock);
-			return level;
+			return get_level(cpufreq_dev, freq);
 		}
 	}
 	mutex_unlock(&cooling_cpufreq_lock);
