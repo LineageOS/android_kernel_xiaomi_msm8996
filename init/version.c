f7d045b4bd87a7149adbc714a6238de00a88f0d4
JonasCardoso <jonascard60@gmail.com>
2018-08-25 15:22:53 -0300

    kernel: updated version

--- init/version.c
+++ init/version.c
@@ -41,10 +41,10 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (" "JonasCardoso" "@"
-	"FloppyKernel-Miui-O" ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
+	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" "JonasCardoso" "@" "FloppyKernel-Miui-O" ")"
-" (" LINUX_COMPILER ") %s\n";
+	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (" LINUX_COMPILER ") %s\n";
