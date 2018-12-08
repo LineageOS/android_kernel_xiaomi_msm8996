7989c7e7205bec9728608ed6eabbfdde21ec1335
Jonas Cardoso <jonascard60@gmail.com>
2018-12-08 15:44:55 -0200

    zip-creator: set animation speed to 0.8x

---
+++ zip-creator/base/ramdisk/init.animation.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+# CUSTOM ANIMATION SPEED
+
+# Set speed to 0.8x
+settings put global window_animation_scale 0.8
+settings put global transition_animation_scale 0.8
+settings put global animator_duration_scale 0.8
+
