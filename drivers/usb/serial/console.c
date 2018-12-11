0b77ec583772227c4df20266028ccdda79cfc4b1
Dan Aloni <daloni@magicleap.com>
2018-12-11 13:29:52 +0200

    drivers/usb/serial/console.c: remove superfluous serial->port condition
    This is because serial->port is an array. Detected under Clang.
    
    Change-Id: Ibd5f14eeddfad0b99c29fa3b108ff86f29d69908
    Signed-off-by: Dan Aloni <daloni@magicleap.com>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- drivers/usb/serial/console.c
+++ drivers/usb/serial/console.c
@@ -269,7 +269,7 @@ static struct console usbcons = {
 
 void usb_serial_console_disconnect(struct usb_serial *serial)
 {
-	if (serial && serial->port && serial->port[0]
+	if (serial && serial->port[0]
 				&& serial->port[0] == usbcons_info.port) {
 		usb_serial_console_exit();
 		usb_serial_put(serial);
