51099fb4207b67ba4c3ba6901b99a8247971fcb7
Sultan Alsawaf <sultan@kerneltoast.com>
2018-12-15 02:54:35 +0000

    msm: mdss: Boost CPU when a new frame is ready to be committed
    In order to reduce jank, request a CPU boost whenever a new frame is
    ready to be rendered to the display. The CPU boost should be sufficient
    enough to render 60 FPS without any dropped frames while there is no
    significant external source of CPU load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -48,6 +48,7 @@
 #include <linux/file.h>
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
+#include <linux/cpu_input_boost.h>
 #include "mdss_fb.h"
 #include "mdss_mdp_splash_logo.h"
 #define CREATE_TRACE_POINTS
@@ -4949,6 +4950,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+		cpu_input_boost_kick();
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
