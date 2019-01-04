3eb96cfcde6f84577c78066bf1fa3b76aa40193a
Jonas Cardoso <jonascard60@gmail.com>
2019-01-04 19:53:43 -0200

    zip-creator: disabled set profile at boot

--- zip-creator/base/ramdisk/init.spectrum.sh
+++ zip-creator/base/ramdisk/init.spectrum.sh
@@ -4,5 +4,5 @@
 
 # If there is not a persist value, we need to set one
 if [ ! -f /data/property/persist.spectrum.profile ]; then
-    setprop persist.spectrum.profile 4
+    #setprop persist.spectrum.profile 4
 fi
