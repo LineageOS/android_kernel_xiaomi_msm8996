68adf96c7d0e53ca52f5bb55c643a78ad1d82196
Sultan Alsawaf <sultan@kerneltoast.com>
2019-01-13 19:39:55 -0800

    devfreq_boost: Remove unused devfreq_get_boost_dev function
    This function isn't used anywhere. Remove it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2018 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 
 #define pr_fmt(fmt) "devfreq_boost: " fmt
@@ -8,6 +8,21 @@
 #include <linux/devfreq_boost.h>
 #include <linux/fb.h>
 #include <linux/input.h>
+
+struct boost_dev {
+	struct workqueue_struct *wq;
+	struct devfreq *df;
+	struct work_struct input_boost;
+	struct delayed_work input_unboost;
+	struct work_struct max_boost;
+	struct delayed_work max_unboost;
+	unsigned long abs_min_freq;
+	unsigned long boost_freq;
+	unsigned long max_boost_expires;
+	unsigned long max_boost_jiffies;
+	bool disable;
+	spinlock_t lock;
+};
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
@@ -88,16 +103,6 @@ void devfreq_register_boost_device(enum df_device device, struct devfreq *df)
 	spin_lock_irqsave(&b->lock, flags);
 	b->df = df;
 	spin_unlock_irqrestore(&b->lock, flags);
-}
-
-struct boost_dev *devfreq_get_boost_dev(enum df_device device)
-{
-	struct df_boost_drv *d = df_boost_drv_g;
-
-	if (!d)
-		return NULL;
-
-	return d->devices + device;
 }
 
 static unsigned long devfreq_abs_min_freq(struct boost_dev *b)
--- include/linux/devfreq_boost.h
+++ include/linux/devfreq_boost.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2018 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 #ifndef _DEVFREQ_BOOST_H_
 #define _DEVFREQ_BOOST_H_
@@ -12,26 +12,10 @@ enum df_device {
 	DEVFREQ_MAX
 };
 
-struct boost_dev {
-	struct workqueue_struct *wq;
-	struct devfreq *df;
-	struct work_struct input_boost;
-	struct delayed_work input_unboost;
-	struct work_struct max_boost;
-	struct delayed_work max_unboost;
-	unsigned long abs_min_freq;
-	unsigned long boost_freq;
-	unsigned long max_boost_expires;
-	unsigned long max_boost_jiffies;
-	bool disable;
-	spinlock_t lock;
-};
-
 #ifdef CONFIG_DEVFREQ_BOOST
 void devfreq_boost_kick(enum df_device device);
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms);
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df);
-struct boost_dev *devfreq_get_boost_dev(enum df_device device);
 #else
 static inline
 void devfreq_boost_kick(enum df_device device)
@@ -44,11 +28,6 @@ void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
 static inline
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df)
 {
-}
-static inline
-struct boost_dev *devfreq_get_boost_dev(enum df_device device)
-{
-	return NULL;
 }
 #endif
 
