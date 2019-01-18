2dc96e8f32538006c44dabfaa43dadbb4728e1a1
Will Huang <wilhuang@codeaurora.org>
2019-01-18 17:36:57 +0800

    qcacld-2.0: Add reset resume rather than cold reset target
    Re-direct .reset_resume to .resume routine, while target support
    reset_resume now. The behavior can be configured by
    CONFIG_USB_RESET_RESUME_PERSISTENCE, and by default is cold reset
    as original.
    
    Change-Id: Ia01b2aebd807d4f466e645b61393c4d52f9fd118
    CRs-Fixed: 2366232

--- CORE/SERVICES/HIF/USB/if_usb.c
+++ CORE/SERVICES/HIF/USB/if_usb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2016, 2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2016, 2018-2019 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -434,6 +434,7 @@ static int hif_usb_resume(struct usb_interface *interface)
 	return 0;
 }
 
+#ifndef USB_RESET_RESUME_PERSISTENCE
 static int hif_usb_reset_resume(struct usb_interface *intf)
 {
 	HIF_DEVICE_USB *device = usb_get_intfdata(intf);
@@ -444,6 +445,14 @@ static int hif_usb_reset_resume(struct usb_interface *intf)
 	printk("Exit:%s,Line:%d \n\r", __func__,__LINE__);
 	return 0;
 }
+#else
+static int hif_usb_reset_resume(struct usb_interface *intf)
+{
+	printk("Enter:%s,Line:%d \n\r", __func__,__LINE__);
+	hif_usb_resume(intf);
+	printk("Exit:%s,Line:%d \n\r", __func__,__LINE__);
+}
+#endif
 
 static struct usb_device_id hif_usb_id_table[] = {
 	{USB_DEVICE_AND_INTERFACE_INFO(VENDOR_ATHR, 0x9378, 0xFF, 0xFF, 0xFF)},
