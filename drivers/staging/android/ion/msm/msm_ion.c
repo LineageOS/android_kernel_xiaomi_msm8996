cc6d15258c1b97853b61955ee0dce1e5ba8e9c5b
Vijayanand Jitta <vjitta@codeaurora.org>
2019-03-06 10:48:07 +0530

    ion: Fix typecasting of msm_ion_heap_types
    msm_ion_heap_types type variable is assigned to a ion_heap_type
    variable, typecast this properly to ion_heap_type before assigning
    also fix is_cp_flag_present function by using bitwise operators
    instead of logical operators and consider ION_FLAG_SECURE as well
    while allocating through system secure heap.
    
    Change-Id: I7f65c5d5d92e3007a1f803e7438ccbfb672907e6
    Signed-off-by: Vijayanand Jitta <vjitta@codeaurora.org>
    Link: https://source.codeaurora.org/quic/la/kernel/msm-4.9/commit/?id=ad7d1e333f74f1a71f3ed51882221e61ab67dc60
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -760,13 +760,15 @@ long msm_ion_custom_ioctl(struct ion_client *client,
 		int ret;
 
 		ret = ion_walk_heaps(client, data.prefetch_data.heap_id,
-			ION_HEAP_TYPE_SECURE_DMA,
-			(void *)data.prefetch_data.len,
-			ion_secure_cma_prefetch);
+				     (enum ion_heap_type)
+				     ION_HEAP_TYPE_SECURE_DMA,
+				     (void *)data.prefetch_data.len,
+				     ion_secure_cma_prefetch);
 		if (ret)
 			return ret;
 
 		ret = ion_walk_heaps(client, data.prefetch_data.heap_id,
+			(enum ion_heap_type)
 			ION_HEAP_TYPE_SYSTEM_SECURE,
 			(void *)&data.prefetch_data,
 			ion_system_secure_heap_prefetch);
@@ -779,6 +781,7 @@ long msm_ion_custom_ioctl(struct ion_client *client,
 		int ret;
 
 		ret = ion_walk_heaps(client, data.prefetch_data.heap_id,
+			(enum ion_heap_type)
 			ION_HEAP_TYPE_SECURE_DMA,
 			(void *)data.prefetch_data.len,
 			ion_secure_cma_drain_pool);
