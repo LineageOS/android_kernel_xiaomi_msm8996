baab6e33b07b41045b9a71d5318b7b6a16e2d5b3
Christian Brauner <christian@brauner.io>
2019-05-24 12:44:59 +0200

    BACKPORT: arch: wire-up pidfd_open()
    This wires up the pidfd_open() syscall into all arches at once.
    
    Signed-off-by: Christian Brauner <christian@brauner.io>
    Reviewed-by: David Howells <dhowells@redhat.com>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Jann Horn <jannh@google.com>
    Cc: Andy Lutomirsky <luto@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Aleksa Sarai <cyphar@cyphar.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: linux-api@vger.kernel.org
    Cc: linux-alpha@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-ia64@vger.kernel.org
    Cc: linux-m68k@lists.linux-m68k.org
    Cc: linux-mips@vger.kernel.org
    Cc: linux-parisc@vger.kernel.org
    Cc: linuxppc-dev@lists.ozlabs.org
    Cc: linux-s390@vger.kernel.org
    Cc: linux-sh@vger.kernel.org
    Cc: sparclinux@vger.kernel.org
    Cc: linux-xtensa@linux-xtensa.org
    Cc: linux-arch@vger.kernel.org
    Cc: x86@kernel.org
    
    (cherry picked from commit 7615d9e1780e26e0178c93c55b73309a5dc093d7)
    
    Conflicts:
    
            arch/alpha/kernel/syscalls/syscall.tbl
            arch/arm/tools/syscall.tbl
            arch/ia64/kernel/syscalls/syscall.tbl
            arch/m68k/kernel/syscalls/syscall.tbl
            arch/microblaze/kernel/syscalls/syscall.tbl
            arch/mips/kernel/syscalls/syscall_n32.tbl
            arch/mips/kernel/syscalls/syscall_n64.tbl
            arch/mips/kernel/syscalls/syscall_o32.tbl
            arch/parisc/kernel/syscalls/syscall.tbl
            arch/powerpc/kernel/syscalls/syscall.tbl
            arch/s390/kernel/syscalls/syscall.tbl
            arch/sh/kernel/syscalls/syscall.tbl
            arch/sparc/kernel/syscalls/syscall.tbl
            arch/xtensa/kernel/syscalls/syscall.tbl
            arch/x86/entry/syscalls/syscall_32.tbl
            arch/x86/entry/syscalls/syscall_64.tbl
    
    (1. Skipped syscall.tbl modifications for missing architectures.
     2. Removed __ia32_sys_pidfd_open in arch/x86/entry/syscalls/syscall_32.tbl.
     3. Replaced __x64_sys_pidfd_open with sys_pidfd_open in arch/x86/entry/syscalls/syscall_64.tbl.)
    
    Bug: 135608568
    Test: test program using syscall(__NR_sys_pidfd_open,..) and poll()
    Change-Id: I294aa33dea5ed2662e077340281d7aa0452f7471
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -717,9 +717,11 @@ __SYSCALL(__NR_membarrier, sys_membarrier)
 __SYSCALL(__NR_mlock2, sys_mlock2)
 #define __NR_pidfd_send_signal 424
 __SYSCALL(__NR_pidfd_send_signal, sys_pidfd_send_signal)
+#define __NR_pidfd_open 434
+__SYSCALL(__NR_pidfd_open, sys_pidfd_open)
 
 #undef __NR_syscalls
-#define __NR_syscalls 425
+#define __NR_syscalls 435
 
 /*
  * All syscalls below here should go away really,
