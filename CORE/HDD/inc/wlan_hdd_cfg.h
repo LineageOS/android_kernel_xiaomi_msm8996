75ea33d185374a4860271865321607b6ec3c7713
Li Feng <lifeng@codeaurora.org>
2019-07-09 11:11:45 +0800

    qcacld-2.0: Adjust CFG_SET_TSF_GPIO_PIN_MAX value to 255
    Adjust CFG_SET_TSF_GPIO_PIN_MAX value to 255 to eliminate range 
    checking error.
    
    Change-Id: I5bd63b8414141b23b56b5fbba9dacf39571286f9
    CRs-Fixed: 2482770

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -4224,7 +4224,7 @@ FG_BTC_BT_INTERVAL_PAGE_P2P_STA_DEFAULT
 /* GPIO pin to toogle when capture tsf */
 #define CFG_SET_TSF_GPIO_PIN_NAME                  "gtsf_gpio_pin"
 #define CFG_SET_TSF_GPIO_PIN_MIN                   (0)
-#define CFG_SET_TSF_GPIO_PIN_MAX                   (254)
+#define CFG_SET_TSF_GPIO_PIN_MAX                   (255)
 #define TSF_GPIO_PIN_INVALID                       (255)
 #define CFG_SET_TSF_GPIO_PIN_DEFAULT               (TSF_GPIO_PIN_INVALID)
 
