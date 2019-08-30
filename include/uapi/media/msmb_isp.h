e5fe32061e1b17f0fe25b396737b6d7a8258352a
xNombre <kartapolska@gmail.com>
2019-08-30 23:29:30 +0200

    msm: camera_v2: fix for older blobs
    It took me a few days to notice that size of this structure is too small.
    It was breaking camera because ISP command wasn't recognized.
    
    It's a dummy add of commit 3d5e5360d2aa9 "msm: camera: isp: Handle standby mode for B+M"
    Enum is at size of int32.
    
    There's no need to port it as standby mode seems to be implemented in other way.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- include/uapi/media/msmb_isp.h
+++ include/uapi/media/msmb_isp.h
@@ -337,6 +337,7 @@ struct msm_vfe_axi_stream_cfg_cmd {
 	uint32_t stream_handle[VFE_AXI_SRC_MAX];
 	enum msm_vfe_axi_stream_cmd cmd;
 	uint8_t sync_frame_id_src;
+	int32_t dummy;
 };
 
 enum msm_vfe_axi_stream_update_type {
