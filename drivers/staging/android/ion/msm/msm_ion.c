3ad6c4c9c9644b93b57098e780b07ab4011d9e86
Michael Bestas <mkbestas@lineageos.org>
2020-06-03 21:16:10 +0300

    Revert "ion: Fix typecasting of msm_ion_heap_types"
    * We'll take the upstream fix
    
    This reverts commit 547b4a3bcc13f1167e81d8b8318da8211623ae04.
    
    Change-Id: I0c1eb338dc1714b48fff3bc3a9aefafd64e293ee

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -809,15 +809,13 @@ long msm_ion_custom_ioctl(struct ion_client *client,
 		int ret;
 
 		ret = ion_walk_heaps(client, data.prefetch_data.heap_id,
-				     (enum ion_heap_type)
-				     ION_HEAP_TYPE_SECURE_DMA,
-				     (void *)data.prefetch_data.len,
-				     ion_secure_cma_prefetch);
+			ION_HEAP_TYPE_SECURE_DMA,
+			(void *)data.prefetch_data.len,
+			ion_secure_cma_prefetch);
 		if (ret)
 			return ret;
 
 		ret = ion_walk_heaps(client, data.prefetch_data.heap_id,
-				     (enum ion_heap_type)
 				     ION_HEAP_TYPE_SYSTEM_SECURE,
 				     (void *)&data.prefetch_data,
 				     ion_system_secure_heap_prefetch);
@@ -830,7 +828,6 @@ long msm_ion_custom_ioctl(struct ion_client *client,
 		int ret;
 
 		ret = ion_walk_heaps(client, data.prefetch_data.heap_id,
-			(enum ion_heap_type)
 			ION_HEAP_TYPE_SECURE_DMA,
 			(void *)data.prefetch_data.len,
 			ion_secure_cma_drain_pool);
@@ -839,7 +836,6 @@ long msm_ion_custom_ioctl(struct ion_client *client,
 			return ret;
 
 		ret = ion_walk_heaps(client, data.prefetch_data.heap_id,
-				     (enum ion_heap_type)
 				     ION_HEAP_TYPE_SYSTEM_SECURE,
 				     (void *)&data.prefetch_data,
 				     ion_system_secure_heap_drain);
