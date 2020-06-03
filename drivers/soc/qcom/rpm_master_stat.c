f353f31e32e56e6d8daa855ae6de8efb15ec9237
Michael Bestas <mkbestas@lineageos.org>
2020-06-03 21:11:44 +0300

    Revert "soc: qcom: rpm_master_stat: Fix strlcpy usage"
    * We'll take the upstream fix
    
    This reverts commit 15457b9a920613a525640e57305f8f04606ac5cb.
    
    Change-Id: I1f67bd6eb642a8cf8d376aa87a82796daad384ab

--- drivers/soc/qcom/rpm_master_stat.c
+++ drivers/soc/qcom/rpm_master_stat.c
@@ -396,8 +396,8 @@ static struct msm_rpm_master_stats_platform_data
 		if (!pdata->masters[i])
 			goto err;
 
-		strscpy(pdata->masters[i], master_name,
-					sizeof(pdata->masters[i]));
+		strlcpy(pdata->masters[i], master_name,
+					strlen(master_name) + 1);
 	}
 	return pdata;
 err:
