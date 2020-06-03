d544bd67ddb903305bc5b3f297f6218e2d42f013
Michael Bestas <mkbestas@lineageos.org>
2020-06-03 21:17:54 +0300

    Revert "tty: msm_smd_tty: Remove useless NULL checks"
    * We'll take the upstream fix
    
    This reverts commit 245e308390b6ed1ca73ba653983923d45a133130.
    
    Change-Id: Ia068faf467f697345ff20fe390f952cc4f11699c

--- drivers/tty/serial/msm_smd_tty.c
+++ drivers/tty/serial/msm_smd_tty.c
@@ -368,6 +368,9 @@ static int smd_tty_dummy_probe(struct platform_device *pdev)
 	int n;
 
 	for (n = 0; n < MAX_SMD_TTYS; ++n) {
+		if (!smd_tty[n].dev_name)
+			continue;
+
 		if (pdev->id == smd_tty[n].edge &&
 			!strcmp(pdev->name, smd_tty[n].dev_name)) {
 			complete_all(&smd_tty[n].ch_allocated);
@@ -499,7 +502,7 @@ static int smd_tty_port_activate(struct tty_port *tport,
 	struct smd_tty_info *info;
 	const char *peripheral = NULL;
 
-	if (n >= MAX_SMD_TTYS)
+	if (n >= MAX_SMD_TTYS || !smd_tty[n].ch_name)
 		return -ENODEV;
 
 	info = smd_tty + n;
