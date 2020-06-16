ca7f03f0d99599e20e83b1847e1b629c56a80d81
Sourav Mohapatra <mohapatr@codeaurora.org>
2020-06-16 13:25:14 +0530

    qcacld-3.0: Wait for driver recovery while WiFi ON/OFF
    Currently the driver is not synchronized properly between SSR and wifi
    ON/OFF. This causes a potential deadlock.
    
    To synchronize, add a wait in wlan_hdd_state_ctrl_param_write
    which is called during WiFi ON/OFF. This wait will ensure that the
    driver recovery is complete before proceeding with ON/OFF.
    
    Change-Id: Ia1c4f8d7076d77c591276ee380b55a747cf606bd
    CRs-Fixed: 2701513

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -12804,7 +12804,7 @@ static ssize_t wlan_hdd_state_ctrl_param_write(struct file *filp,
 		goto exit;
 	}
 
-	if (!cds_is_driver_loaded()) {
+	if (!cds_is_driver_loaded() || cds_is_driver_recovering()) {
 		init_completion(&wlan_start_comp);
 		rc = wait_for_completion_timeout(&wlan_start_comp,
 				msecs_to_jiffies(HDD_WLAN_START_WAIT_TIME));
