dfb161642804efbcfdd674f20150e0532b7dae01
Narender Ankam <nankam@codeaurora.org>
2020-09-17 13:37:00 +0530

    msm: mdss: hdmi: parse physical screen dimensions from EDID
    Parse Basic Display Parameters/Features of EDID and store
    Horizontal and Vertical Screen size of downstream HDMI sink.
    
    Change-Id: Ic538a881f877f27a66983a34d6869bf9663567e9
    Signed-off-by: Narender Ankam <nankam@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_hdmi_tx.c
+++ drivers/video/fbdev/msm/mdss_hdmi_tx.c
@@ -4125,6 +4125,7 @@ sysfs_err:
 static int hdmi_tx_evt_handle_check_param(struct hdmi_tx_ctrl *hdmi_ctrl)
 {
 	struct mdss_panel_info *pinfo = &hdmi_ctrl->panel_data.panel_info;
+	void *data = NULL;
 	int new_vic = -1;
 	int rc = 0;
 
@@ -4135,6 +4136,10 @@ static int hdmi_tx_evt_handle_check_param(struct hdmi_tx_ctrl *hdmi_ctrl)
 		DEV_ERR("%s: invalid or not supported vic\n", __func__);
 		goto end;
 	}
+
+	data = hdmi_tx_get_fd(HDMI_TX_FEAT_EDID);
+	pinfo->physical_width = hdmi_edid_get_phys_width(data);
+	pinfo->physical_height = hdmi_edid_get_phys_height(data);
 
 	/*
 	 * return value of 1 lets mdss know that panel
