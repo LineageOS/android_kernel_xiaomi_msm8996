c2e2adf298499edea91a72c0912a3285ac07c4cc
Jyoti Kumari <jyotkuma@codeaurora.org>
2021-03-02 17:30:00 +0530

    qcacld-3.0: wma_send_peer_assoc() sets incorrect peer state
    wma_send_peer_assoc() calls wma_unified_peer_state_update() which
    always sets the peer state as AUTH irrespective of peer state.
    
    Remove wma_unified_peer_state_update() from wma_send_peer_assoc()
    to handle peer state.
    
    Change-Id: I4a887acbc8018653c34b927636cce7cc05323838
    CRs-Fixed: 2888808

--- core/wma/src/wma_mgmt.c
+++ core/wma/src/wma_mgmt.c
@@ -918,45 +918,6 @@ static inline uint8_t wma_parse_mpdudensity(uint8_t mpdudensity)
 		return 0;
 }
 
-#if defined(CONFIG_HL_SUPPORT) && defined(FEATURE_WLAN_TDLS)
-
-/**
- * wma_unified_peer_state_update() - update peer state
- * @pdev: pdev handle
- * @sta_mac: pointer to sta mac addr
- * @bss_addr: bss address
- * @sta_type: sta entry type
- *
- *
- * Return: None
- */
-static void
-wma_unified_peer_state_update(
-	struct ol_txrx_pdev_t *pdev,
-	uint8_t *sta_mac,
-	uint8_t *bss_addr,
-	uint8_t sta_type)
-{
-	if (STA_ENTRY_TDLS_PEER == sta_type)
-		ol_txrx_peer_state_update(pdev, sta_mac,
-					  OL_TXRX_PEER_STATE_AUTH);
-	else
-		ol_txrx_peer_state_update(pdev, bss_addr,
-					  OL_TXRX_PEER_STATE_AUTH);
-}
-#else
-
-static inline void
-wma_unified_peer_state_update(
-	struct ol_txrx_pdev_t *pdev,
-	uint8_t *sta_mac,
-	uint8_t *bss_addr,
-	uint8_t sta_type)
-{
-	ol_txrx_peer_state_update(pdev, bss_addr, OL_TXRX_PEER_STATE_AUTH);
-}
-#endif
-
 #define CFG_CTRL_MASK              0xFF00
 #define CFG_DATA_MASK              0x00FF
 
@@ -1246,9 +1207,6 @@ QDF_STATUS wma_send_peer_assoc(tp_wma_handle wma,
 	}
 	if (params->wpa_rsn >> 1)
 		cmd->peer_flags |= WMI_PEER_NEED_GTK_2_WAY;
-
-	wma_unified_peer_state_update(pdev, params->staMac,
-				      params->bssId, params->staType);
 
 #ifdef FEATURE_WLAN_WAPI
 	if (params->encryptType == eSIR_ED_WPI) {
