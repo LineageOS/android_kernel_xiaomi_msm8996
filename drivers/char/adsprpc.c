de77a21b93eb19e5f3b4bd45614dd81f0fff7019
Swathi K <quic_c_kataka@quicinc.com>
2022-02-01 03:21:12 +0530

    msm: adsprpc: Handle UAF in fastrpc debugfs read
    Use lock to protect maps among multiple
    threads to avoid race condition.
    
    Change-Id: I0c216d23316d41a91c2292ef7469e6ca06d771ea
    Signed-off-by: Swathi K <quic_c_kataka@quicinc.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2012-2022, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
