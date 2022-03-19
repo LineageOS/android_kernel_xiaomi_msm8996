b04baa6223d275019e028c83e815fccb6616b06e
Han Wang <416810799@qq.com>
2022-03-19 08:17:00 +0100

    input: Drop INPUT_PROP_NO_DUMMY_RELEASE bit
     * INPUT_PROP_NO_DUMMY_RELEASE definition in this kernel collides with
       INPUT_PROP_ACCELEROMETER definition in bionic and upstream kernel. As
       a result, Android recognizes normal input devices like accelerometers
       and causes strange behaviors. There are no references to this bit in
       userspace and it is not in 4.9+ kernels, so let's drop this CAF jank.
    
    Change-Id: Id9b4ec8d31470e663f533249c4bc4b9e94fd38be

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -181,7 +181,6 @@ struct input_mask {
 #define INPUT_PROP_SEMI_MT		0x03	/* touch rectangle only */
 #define INPUT_PROP_TOPBUTTONPAD		0x04	/* softbuttons at top of pad */
 #define INPUT_PROP_POINTING_STICK	0x05	/* is a pointing stick */
-#define INPUT_PROP_NO_DUMMY_RELEASE	0x06	/* no dummy event */
 
 #define INPUT_PROP_MAX			0x1f
 #define INPUT_PROP_CNT			(INPUT_PROP_MAX + 1)
