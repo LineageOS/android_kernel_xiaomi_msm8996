86cd107cd467cbba7f260dfdddb189dc07366a96
Georg Veichtlbauer <georg@vware.at>
2023-07-26 17:48:35 +0200

    sched: Enable min capacity capping
    Change-Id: Icd8f2cde6cac1b7bb07e54b8e6989c65eea4e4a5

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -85,7 +85,7 @@ SCHED_FEAT(ENERGY_AWARE, false)
  * If enabled, this can be used to inform the scheduler about capacity
  * restrictions.
  */
-SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
+SCHED_FEAT(MIN_CAPACITY_CAPPING, true)
 
 /*
  * Enforce the priority of candidates selected by find_best_target()
