8a021c2fe1db1b3a376a563d0156e9488d9f5aff
LuK1337 <priv.luk@gmail.com>
2023-10-27 18:44:20 +0200

    writeback: Fix cgroup_path() return value check
    cgroup_path() returns length, not char *.
    
    Change-Id: I8bdfcc0fc58789aa23f730866f27fbb932b24be1

--- include/trace/events/writeback.h
+++ include/trace/events/writeback.h
@@ -142,10 +142,10 @@ static inline size_t __trace_wb_cgroup_size(struct bdi_writeback *wb)
 static inline void __trace_wb_assign_cgroup(char *buf, struct bdi_writeback *wb)
 {
 	struct cgroup *cgrp = wb->memcg_css->cgroup;
-	char *path;
+	int len;
 
-	path = cgroup_path(cgrp, buf, kernfs_path_len(cgrp->kn) + 1);
-	WARN_ON_ONCE(path != buf);
+	len = cgroup_path(cgrp, buf, kernfs_path_len(cgrp->kn) + 1);
+	WARN_ON_ONCE(len < 0);
 }
 
 static inline size_t __trace_wbc_cgroup_size(struct writeback_control *wbc)
